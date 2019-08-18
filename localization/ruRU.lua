bd8facb80d106aca98c5b94ed4e54016428a55f7
Zarillion <zarillion@zarillion.com>
2019-08-17 21:31:48 -0500

    Remove default flag from ruRU localization [#11]

--- localization/ruRU.lua
+++ localization/ruRU.lua
@@ -1,5 +1,5 @@
 local ADDON_NAME, _ = ...
-local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "ruRU", true, true)
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "ruRU", false, true)
 if not L then return end
 
 -------------------------------------------------------------------------------
