8737a3703fe565b0342fc3d4b67b1f9c6f34d22c
Zarillion <zarillion@zarillion.com>
2019-12-12 01:37:19 -0600

    Add mount/pet/toy rewards

--- uldum.lua
+++ uldum.lua
@@ -65,22 +65,42 @@ options.petUldum = {
 -------------------------------------------------------------------------------
 
 nodes[66817436] = Rare({id=158557, quest=57669, note=nil}) -- Actiss the Deceiver
+-- nodes[] = Rare({id=157593, quest=nil, note=nil, rewards={
+--     Pet({id=2851, item=174478}) -- Wicked Lurker
+-- }}) -- Amalgamation of Flesh
 nodes[32426443] = Rare({id=155703, quest=56834, note=nil}) -- Anq'uri the Titanic
 nodes[33595682] = Rare({id=154576, quest=58614, note=nil}) -- Aqir Titanus
 nodes[45605777] = Rare({id=162171, quest=nil, note=L["dunewalker"]}) -- Captain Dunewalker
+nodes[30854971] = Rare({id=162147, quest=nil, note=nil, rewards={
+    Mount({id=1319, item=174769}) -- Malevolent Drone
+}}) -- Corpse Eater
 nodes[49363822] = Rare({id=158594, quest=57672, note=nil}) -- Doomsayer Vathiris
 nodes[48657067] = Rare({id=158491, quest=nil, note=L["falconer"]}) -- Falconer Amenophis
 nodes[54694317] = Rare({id=158597, quest=57675, note=nil}) -- High Executor Yothrim
 nodes[60033950] = Rare({id=160623, quest=58206, note=nil}) -- Hungering Miasma
+nodes[21405794] = Rare({id=155531, quest=56823, note=nil}) -- Infested Wastewander Captain
 nodes[71237375] = Rare({id=156655, quest=nil, note=nil}) -- Korzaran the Slaughterer
-nodes[34681890] = Rare({id=154604, quest=nil, note=L["ajqirai"]}) -- Lord Aj'qirai
+nodes[34681890] = Rare({id=154604, quest=nil, note=L["ajqirai"], rewards={
+    Pet({id=2847, item=174475}) -- Rotbreath
+}}) -- Lord Aj'qirai
+nodes[68593204] = Rare({id=157146, quest=nil, note=nil, rewards={
+    Mount({id=1317, item=174753}) -- Waste Marauder
+}}) -- Rotfeaster
 nodes[35071729] = Rare({id=162196, quest=nil, note=nil}) -- Obsidian Annihilator
 nodes[58175712] = Rare({id=156299, quest=57430, note=L["rkhuzj"]}) -- R'khuzj the Unfathomable
 nodes[57003794] = Rare({id=161033, quest=58333, note=nil}) -- Shadowmaw
 nodes[58558282] = Rare({id=156654, quest=nil, note=nil}) -- Shol'thoss the Doomspeaker
 nodes[61297484] = Rare({id=160532, quest=58169, note=nil}) -- Shoth the Darkened
+nodes[21236105] = Rare({id=162140, quest=nil, note=nil, rewards={
+    Pet({id=2848, item=174476}) -- Aqir Tunneler
+}}) -- Skikx'traz
 nodes[78986389] = Rare({id=151878, quest=58613, note=nil}) -- Sun King Nahkotep
-nodes[49328235] = Rare({id=158636, quest=nil, note=L["gexec"]}) -- The Grand Executor
+nodes[49328235] = Rare({id=158636, quest=nil, note=L["gexec"], rewards={
+    Toy({item=169303}) -- Hell-Bent Bracers
+}}) -- The Grand Executor
+-- nodes[] = Rare({id=157473, quest=nil, note=nil, rewards={
+--     Toy({item=174874}) -- Budget K'thir Disguise
+-- }}) -- Yiphrim the Will Ravager
 -- nodes[] = Rare({id=, quest=, note=nil}) --
 
 -------------------------------------------------------------------------------
--- vale.lua
+++ vale.lua
@@ -51,6 +51,9 @@ options.groupVale = {
 -------------------------------------------------------------------------------
 
 nodes[20007460] = Rare({id=160825, quest=58300, note=nil}) -- Amber-Shaper Esh'ri
+nodes[34156805] = Rare({id=157466, quest=nil, note=nil, rewards={
+    Mount({id=1328, item=174840}) -- Xinlao
+}}) -- Anh-De the Loyal
 nodes[06487070] = Rare({id=160878, quest=58307, note=nil}) -- Buh'gzaki the Blasphemous
 nodes[06406433] = Rare({id=160893, quest=58308, note=nil}) -- Captain Vor'lek
 nodes[26506657] = Rare({id=160872, quest=58304, note=nil}) -- Destroyer Krox'tazar
@@ -59,22 +62,36 @@ nodes[10474089] = Rare({id=160876, quest=58306, note=nil}) -- Enraged Amber Elem
 nodes[28895272] = Rare({id=160810, quest=58299, note=nil}) -- Harbinger Il'koxik
 nodes[12835129] = Rare({id=160868, quest=58303, note=nil}) -- Harrier Nir'verash
 nodes[21256265] = Rare({id=160826, quest=58301, note=nil}) -- Hive-Guard Naz'ruzek
+nodes[10003200] = Rare({id=157160, quest=nil, note=nil, reward={
+    Mount({id=1327, item=174841}) -- Ren's Stalwart Hound
+}}) -- Houndlord Ren
 nodes[18426557] = Rare({id=160930, quest=58312, note=nil}) -- Infused Amber Ooze
 nodes[17201159] = Rare({id=160968, quest=58295, note=L["jadec"]}) -- Jade Colossus
 nodes[17850918] = Rare({id=160920, quest=58310, note=nil}) -- Kal'tik the Blight
 nodes[25673816] = Rare({id=160867, quest=58302, note=nil}) -- Kzit'kovok
 nodes[14813374] = Rare({id=160922, quest=58311, note=nil}) -- Needler Zhesalla
+nodes[29132207] = Rare({id=156424, quest=nil, note=nil, rewards={
+    Toy({item=174873}) -- Trans-mogu-rifier
+}}) -- Tashara
+nodes[51884116] = Rare({id=157176, quest=nil, note=nil, rewards={
+    Pet({id=2845, item=174473}) -- K'uddly
+}}) -- The Forgotten
 nodes[09586736] = Rare({id=157468, quest=57364, note=nil}) -- Tisiphon
+nodes[54266272] = Rare({id=154495, quest=nil, note=nil, rewards={
+    Pet({id=2846, item=174474}) -- Corrupted Tentacle
+}}) -- Will of N'Zoth
 -- nodes[] = Rare({id=, quest=, note=nil}) --
 
 -------------------------------------------------------------------------------
 ---------------------------------- TREASURES ----------------------------------
 -------------------------------------------------------------------------------
 
-nodes[11552553] = Treasure({quest=58224, label=L["ambered_cache"]})
+--nodes[] = Treasure({quest=58224, label=L["ambered_cache"]})
 nodes[21051415] = Treasure({quest=58225, label=L["ambered_cache"]})
 nodes[09302831] = Treasure({quest=58226, label=L["ambered_cache"]})
+nodes[07693682] = Treasure({quest=58226, label=L["ambered_cache"]})
 nodes[26704680] = Treasure({quest=58227, label=L["ambered_cache"]})
+nodes[31724184] = Treasure({quest=58227, label=L["ambered_cache"]})
 nodes[15406394] = Treasure({quest=58228, label=L["ambered_cache"]})
 
 -------------------------------------------------------------------------------
@@ -82,6 +99,7 @@ nodes[15406394] = Treasure({quest=58228, label=L["ambered_cache"]})
 -------------------------------------------------------------------------------
 
 nodes[28553494] = PetBattle({id=162470}) -- Baruk Stone Defender
+nodes[07333190] = PetBattle({id=162471}) -- Vil'thik Hatchling
 
 -------------------------------------------------------------------------------
 
