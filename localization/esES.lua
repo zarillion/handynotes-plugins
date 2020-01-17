1da88b2ad5c428020623bc2e922240cc2b13d8dd
Zarillion <zarillion@zarillion.com>
2020-01-17 13:51:22 -0600

    Add esES translation (thanks krovikan74!)

---
+++ localization/esES.lua
@@ -0,0 +1,183 @@
+local ADDON_NAME, _ = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "esES", false, true)
+if not L then return end
+
+-------------------------------------------------------------------------------
+------------------------------------ ULDUM ------------------------------------
+-------------------------------------------------------------------------------
+
+L["uldum"] = "Uldum"
+L["uldum_intro_note"] = "Completa la cadena de misiones introductorias para desbloquear los monstruos raros, tesoros y misiones de asalto en Uldum."
+
+L["aqir_flayer"] = "Comparte lugar de aparición con Trabajador Colmen'Ashi y Segador aqir."
+L["aqir_titanus"] = "Comparte lugar de aparición con Goliat aqir."
+L["aqir_warcaster"] = "Comparte lugar de aparición con Taumaturgo del Vacío aqir."
+L["atekhramun"] = "Aplasta los Miniescorpión Escama Venenosa hasta que aparezca."
+L["chamber_of_the_moon"] = "Subterráneo en la Cámara de la Luna."
+L["chamber_of_the_stars"] = "Subterráneo en la Cámara de las Estrellas."
+L["chamber_of_the_sun"] = "Subterráneo en la Cámara del Sol."
+L["friendly_alpaca"] = "Alimenta a la alpaca con Verduras Gersahl siete veces para aprenderla como montura. Aparece durante 10 minutos en una localización, después tiene un largo tiempo de reaparición."
+L["gersahl"] = "Arbusto Gershal"
+L["gersahl_note"] = "Alimenta a una Alpaca Amistosa siete veces para una montura. No requiere Herboristería."
+L["hmiasma"] = "Feed it the surrounding oozes until it activates."
+L["left_eye"] = "Te da la mitad izquierda del juguete Ojos que todo lo ven."
+L["platform"] = "Aparece encima de la plataforma flotante."
+L["reshef"] = "Comparte lugar de aparición con Guardia superior distorsionado por el Vacío."
+L["spirit_cave"] = "Entrada de la cueva para el Espíritu del Ritualista Oscuro Zakahn."
+L["uatka"] = "Junto con otros dos jugadores más, haced click al mismo tiempo al Dispositivo Misterioso. Requiere que cada uno tenga un Amuleto Fúlgido de un Relicario Amathet."
+L["wastewander"] = "Comparte lugar de aparición con Huésped Vagayermos."
+
+L["amathet_cache"] = "Alijo Amathet"
+L["black_empire_cache"] = "Alijo del Imperio Negro"
+L["black_empire_coffer"] = "Black Empire Coffer"
+L["infested_cache"] = "Infested Cache"
+L["infested_strongbox"] = "Infested Strongbox"
+L["amathet_reliquary"] = "Relicario Amathet"
+
+L["cursed_relic"] = "Requiere una Reliquia Maldita"
+L["tolvir_relic"] = "Requires una Reliquia de los Tol'vir"
+
+-------------------------------------------------------------------------------
+------------------------------------ VALE -------------------------------------
+-------------------------------------------------------------------------------
+
+L["vale"] = "Valle de la Flor Eterna"
+L["vale_intro_note"] = "Completa la cadena de misiones introductorias para desbloquear los monstruos raros, tesoros y misiones de asalto en Valle de la Flor Eterna."
+
+L["big_blossom_mine"] = "Está dentro de la Mina de la Gran Flor. La entrada está al noreste."
+L["guolai"] = "Está dentro de las Salas de Guo-Lai."
+L["jadec"] = "Está dentro de las Salas de Guo-Lai. Ve a la izquierda al pie de las escaleras."
+L["pools_of_power"] = "Está dentro de las Pozas de Poder. La entrada está por La Pagoda Dorada."
+L["right_eye"] = "Te da la mitad derecha del juguete Ojos que todo lo ven."
+L["tisiphon"] = "Haz click en la Caña de pescar de la suerte de Danielle."
+
+L["ambered_cache"] = "Alijo ámbar"
+L["ambered_coffer"] = "Cofre ámbar"
+L["mogu_plunder"] = "Saqueo mogu"
+L["mogu_strongbox"] = "Caja fuerte mogu"
+
+-------------------------------------------------------------------------------
+------------------------------------ GEAR -------------------------------------
+-------------------------------------------------------------------------------
+
+L["cloth"] = "Tela"
+L["leather"] = "Cuero"
+L["mail"] = "Malla"
+L["plate"] = "Placas"
+
+L["1h_mace"] = "1m Maza"
+L["1h_sword"] = "1m Espada"
+L["1h_axe"] = "1m Hacha"
+L["2h_mace"] = "2m Maza"
+L["2h_axe"] = "2m Hacha"
+L["2h_sword"] = "2m Espada"
+L["shield"] = "Escudo"
+L["dagger"] = "Daga"
+L["staff"] = "Bastón"
+L["fist"] = "Puño"
+L["polearm"] = "Arma de asta"
+L["bow"] = "Arco"
+L["gun"] = "Arma de fuego"
+L["wand"] = "Varita"
+L["crossbow"] = "Ballesta"
+L["offhand"] = "Mano Izquierda"
+L["warglaives"] = "Glaive"
+
+L["ring"] = "Anillo"
+L["amulet"] = "Amuleto"
+L["cloak"] = "Capa"
+L["trinket"] = "Abalorio"
+
+-------------------------------------------------------------------------------
+---------------------------------- TOOLTIPS -----------------------------------
+-------------------------------------------------------------------------------
+
+L["retrieving"] = "Recuperando el enlace del elemento ..."
+L["in_cave"] = "En una cueva."
+L["in_small_cave"] = "En una cueva pequeña."
+L["in_water_cave"] = "En una cueva submarina."
+L["weekly"] = "Semanal"
+L["normal"] = "Normal"
+L["hard"] = "Difícil"
+L["mount"] = "Montura"
+L["pet"] = "Mascota"
+L["toy"] = "Juguete"
+L["completed"] = "Terminado"
+L["incomplete"] = "Incompleto"
+L["known"] = "Conocido"
+L["missing"] = "Desconocido"
+L["unobtainable"] = "No se puede obtener"
+L["unlearnable"] = "No se puede aprender"
+
+-------------------------------------------------------------------------------
+--------------------------------- CONTEXT MENU --------------------------------
+-------------------------------------------------------------------------------
+
+L["context_menu_title"] = "HandyNotes Visions of N'Zoth"
+L["context_menu_add_tomtom"] = "Agregar a TomTom"
+L["context_menu_hide_node"] = "Ocultar este nodo"
+L["context_menu_restore_hidden_nodes"] = "Restaurar todos los nodos ocultos"
+
+-------------------------------------------------------------------------------
+----------------------------------- OPTIONS -----------------------------------
+-------------------------------------------------------------------------------
+
+L["options_title"] = "Visions of N'Zoth"
+
+------------------------------------ ICONS ------------------------------------
+
+L["options_icon_settings"] = "Configuración de Iconos"
+L["options_icons_treasures"] = "Iconos de Tesoro"
+L["options_icons_rares"] = "Iconos de los Raros"
+L["options_icons_caves"] = "Iconos de Cueva"
+L["options_icons_pet_battles"] = "Iconos de duelo de mascotas"
+L["options_icons_other"] = "Otros Iconos"
+L["options_scale"] = "Escala"
+L["options_scale_desc"] = "1 = 100%"
+L["options_opacity"] = "Opacidad"
+L["options_opacity_desc"] = "0 = transparente, 1 = opaco"
+
+---------------------------------- VISIBILITY ---------------------------------
+
+L["options_visibility_settings"] = "Visibilidad"
+L["options_general_settings"] = "General"
+L["options_toggle_looted_rares"] = "Mostrar siempre todos los raros"
+L["options_toggle_looted_rares_desc"] = "Mostrar cada raro estén despojados o no"
+L["options_toggle_looted_treasures"] = "Tesoros ya despojados"
+L["options_toggle_looted_treasures_desc"] = "Mostrar todos los tesoros estén despojados o no"
+L["options_toggle_hide_done_rare"] = "Ocultar el raro si todo el botín se conoce"
+L["options_toggle_hide_done_rare_desc"] = "Ocultar todos los raros a los que se les conozca el botín."
+L["options_toggle_hide_minimap"] = "Ocultar todos los iconos sobre el minimapa"
+L["options_toggle_hide_minimap_desc"] = "Oculta todos los iconos de este addon sobre el minimapa y los muestra solamente sobre el mapa principal."
+
+L["options_toggle_battle_pets_desc"] = "Mostrar las localizaciones de los instructores de mascotas y NPCs."
+L["options_toggle_battle_pets"] = "Mascotas de combate"
+L["options_toggle_caves_desc"] = "Mostrar entradas de cuevas para otros nodos."
+L["options_toggle_caves"] = "Cuevas"
+L["options_toggle_chests_desc"] = "Mostrar la localización de los cofres (despojables diariamente)."
+L["options_toggle_chests"] = "Cofres"
+L["options_toggle_misc"] = "Miscelánea"
+L["options_toggle_npcs"] = "NPCs"
+L["options_toggle_rares_desc"] = "Mostrar las localizaciones de los NPCs raros."
+L["options_toggle_rares"] = "Raros"
+L["options_toggle_supplies_desc"] = "Mostrar todas las localizaciones posibles para los cajones de suministros de guerra."
+L["options_toggle_supplies"] = "Localizaciones de Suministros de guerra"
+L["options_toggle_treasures"] = "Tesoros"
+
+---------------------------------- TOOLTIP ---------------------------------
+
+L["options_tooltip_settings"] = "Tooltip"
+L["options_tooltip_settings_desc"] = "Tooltip"
+L["options_toggle_show_loot"] = "Mostrar el botín"
+L["options_toggle_show_loot_desc"] = "Agregar la información de botín al toolip"
+L["options_toggle_show_notes"] = "Mostrar las Notas"
+L["options_toggle_show_notes_desc"] = "Agregar las notas útiles al tooltip cuando esté disponible"
+
+--------------------------------- DEVELOPMENT ---------------------------------
+
+L["options_dev_settings"] = "Desarrollo"
+L["options_dev_settings_desc"] = "Opciones de Desarrollo"
+L["options_toggle_show_debug"] = "Debug"
+L["options_toggle_show_debug_desc"] = "Mostrar las cosas de debug"
+L["options_toggle_force_nodes"] = "Forzar Nodos"
+L["options_toggle_force_nodes_desc"] = "Forzar el mostrar todos los nodos"
