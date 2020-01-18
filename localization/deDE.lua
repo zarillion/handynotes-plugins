015585fe649e692796709274cc5f4bcaef87efde
Zarillion <zarillion@zarillion.com>
2020-01-17 23:57:48 -0600

    Add WIP german translation

---
+++ localization/deDE.lua
@@ -0,0 +1,184 @@
+local ADDON_NAME, _ = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "deDE", false, true)
+if not L then return end
+--German translation by Dathwada EU-Eredar
+-------------------------------------------------------------------------------
+------------------------------------ ULDUM ------------------------------------
+-------------------------------------------------------------------------------
+
+L["uldum"] = "Uldum"
+L["uldum_intro_note"] = "Schließe die Einführungsquestreihe ab, um in Uldum seltene Gegner, Schätze und Angriffsquests freizuschalten."
+
+L["aqir_flayer"] = "Teilt sich den spawn mit \"Arbeiter der Aqir\" und \"Schnitter der Aqir\"."
+L["aqir_titanus"] = "Teilt sich den spawn mit \"Goliath der Aqir\"."
+L["aqir_warcaster"] = "Teilt sich den spawn mit \n\"Leerenwirker der Aqir\"."
+L["atekhramun"] = "Zertrete die \"Giftschuppenskorplinge\" in der Nähe bis er erscheint."
+L["chamber_of_the_moon"] = "Im Untergrund in der Kammer des Monds."
+L["chamber_of_the_stars"] = "Im Untergrund in der Kammer der Sterne."
+L["chamber_of_the_sun"] = "Im Untergrund in der Kammer der Sonne."
+L["friendly_alpaca"] = "Füttere das Alpaka sieben Mal mit Gersahlblättern, um es als Reittier zu erlernen. Erscheint 10 Minuten lang an einem Ort, danach ein längere Zeit nicht."
+L["gersahl"] = "Gersahlstrauch"
+L["gersahl_note"] = "Füttere sieben Mal ein freundliches Alpaka damit um es als Reittier zu erlernen. \nBenötigt KEIN Kräuterkunde."
+L["hmiasma"] = "Feed it the surrounding oozes until it activates." --work in progress
+L["left_eye"] = "Lässt die linke Hälfte des Allsehende Augen Spielzeugs fallen."
+L["platform"] = "Erscheint auf der schwebenden Plattform."
+L["reshef"] = "Teilt sich den spawn mit Voidwarped High Guards." --work in progress
+L["spirit_cave"] = "Höhleneingang für den Geist des dunklen Ritualisten Zakahn."
+L["tomb_widow"] = "Wenn die weißen Eiersäcke an den Säulen vorhanden sind, töte die unsichtbaren \"Gruftspinnen\", um sie zu beschwören."
+L["uatka"] = "Klick zusammen mit zwei anderen Spielern auf jeweils ein  Mysteriöses Gerät. \nBenötigt ein \"Sonnenberührtes Amulett\" aus einem \"Reliquiar der Amathet\"."
+L["wastewander"] = "Teilt sich den spawn mit \"Befallener Wüstenwanderer\"."
+
+L["amathet_cache"] = "Truhe der Amathet"
+L["black_empire_cache"] = "Truhe des Schwarzen Imperiums"
+L["black_empire_coffer"] = "Kiste des Schwarzen Imperiums"
+L["infested_cache"] = "Befallene Truhe" --check for correctness
+L["infested_strongbox"] = "Befallene Schatulle" --check for correctness
+L["amathet_reliquary"] = "Reliquiar der Amathet"
+
+L["cursed_relic"] = "Benötigt Verfluchtes Relikt"
+L["tolvir_relic"] = "Benötigt Tol'virrelikt"
+
+-------------------------------------------------------------------------------
+------------------------------------ VALE -------------------------------------
+-------------------------------------------------------------------------------
+
+L["vale"] = "Tal der Ewigen Blüten"
+L["vale_intro_note"] = "Schließe die Einführungsquestreihe ab, um im Tal der Ewigen Blüten seltene Gegner, Schätze und Angriffsquests freizuschalten."
+
+L["big_blossom_mine"] = "In der Großblütenmine. \nEingang im Nordosten (70.0 58.4)."
+L["guolai"] = "In den Guo-Lai Hallen."
+L["jadec"] = "In den Guo-Lai Hallen. Geh am ende der Treppe nach links."
+L["pools_of_power"] = "Im Becken der Macht. \nEingang bei der Golden Pagode (57.4 38.0)."
+L["right_eye"] = "Lässt die rechte Hälfte des Allsehende Augen Spielzeugs fallen."
+L["tisiphon"] = "Klick auf Danielles Glücksangelrute."
+
+L["ambered_cache"] = "Bernsteinerne Truhe" --check for correctness
+L["ambered_coffer"] = "Bernsteinerne Kiste" --check for correctness
+L["mogu_plunder"] = "Beute der Mogu" --check for correctness
+L["mogu_strongbox"] = "Schatulle der Mogu" --check for correctness
+
+-------------------------------------------------------------------------------
+------------------------------------ GEAR -------------------------------------
+-------------------------------------------------------------------------------
+
+L["cloth"] = "Stoff"
+L["leather"] = "Leder"
+L["mail"] = "Kette"
+L["plate"] = "Platte"
+
+L["1h_mace"] = "Einhandstreikolben"
+L["1h_sword"] = "Einhandschwert"
+L["1h_axe"] = "Einhandaxt"
+L["2h_mace"] = "Zweihandstreitkolben"
+L["2h_axe"] = "Zweihandaxt"
+L["2h_sword"] = "Zweihandschwert"
+L["shield"] = "Schild"
+L["dagger"] = "Dolch"
+L["staff"] = "Stab"
+L["fist"] = "Faustwaffe"
+L["polearm"] = "Stangenwaffe"
+L["bow"] = "Bogen"
+L["gun"] = "Schusswaffe"
+L["wand"] = "Zauberstab"
+L["crossbow"] = "Armbrust"
+L["offhand"] = "Nebenhand"
+L["warglaive"] = "Kriegsgleve"
+
+L["ring"] = "Finger"
+L["amulet"] = "Hals"
+L["cloak"] = "Umhang"
+L["trinket"] = "Schmuck"
+
+-------------------------------------------------------------------------------
+---------------------------------- TOOLTIPS -----------------------------------
+-------------------------------------------------------------------------------
+
+L["retrieving"] = "Gegenstandslink wird abgerufen ..."
+L["in_cave"] = "In einer Höhle."
+L["in_small_cave"] = "In einer kleinen Höhle."
+L["in_water_cave"] = "In einer Unterwasserhöhle."
+L["weekly"] = "Wöchentlich"
+L["normal"] = "Normal"
+L["hard"] = "schwer"
+L["mount"] = "Reittier"
+L["pet"] = "Haustier"
+L["toy"] = "Spielzeug"
+L["completed"] = "Abgeschlossen"
+L["incomplete"] = "Unvollständig"
+L["known"] = "Bekannt"
+L["missing"] = "Fehlt"
+L["unobtainable"] = "Unerreichbar"
+L["unlearnable"] = "Unlernbar"
+
+-------------------------------------------------------------------------------
+--------------------------------- CONTEXT MENU --------------------------------
+-------------------------------------------------------------------------------
+
+L["context_menu_title"] = "HandyNotes Visions of N'Zoth"
+L["context_menu_add_tomtom"] = "Zu TomTom hinzufügen"
+L["context_menu_hide_node"] = "Verstecke diesen Punkt"
+L["context_menu_restore_hidden_nodes"] = "Versteckte Punkte wiederherstellen"
+
+-------------------------------------------------------------------------------
+----------------------------------- OPTIONS -----------------------------------
+-------------------------------------------------------------------------------
+
+L["options_title"] = "Visions of N'Zoth"
+
+------------------------------------ ICONS ------------------------------------
+
+L["options_icon_settings"] = "Symboleinstellungen"
+L["options_icons_treasures"] = "Schatzsymbole"
+L["options_icons_rares"] = "Seltene Gegner Symbole"
+L["options_icons_caves"] = "Höhlensymbole"
+L["options_icons_pet_battles"] = "Haustierkampfsymbole"
+L["options_icons_other"] = "Andere Symbole"
+L["options_scale"] = "Symbolgröße"
+L["options_scale_desc"] = "1 = 100%"
+L["options_opacity"] = "Symboltransparenz"
+L["options_opacity_desc"] = "0 = transparent, 1 = undurchsichtig"
+
+---------------------------------- VISIBILITY ---------------------------------
+
+L["options_visibility_settings"] = "Sichtbarkeit"
+L["options_general_settings"] = "Allgemeines"
+L["options_toggle_looted_rares"] = "Zeige immer alle seltenen Gegner an"
+L["options_toggle_looted_rares_desc"] = "Zeigt alle seltenen Gegner unabhängig vom Lootstatus"
+L["options_toggle_looted_treasures"] = "Bereits geplünderte Schätze anzeigen"
+L["options_toggle_looted_treasures_desc"] = "Zeigt alle Schätze unabhängig vom Lootstatus"
+L["options_toggle_hide_done_rare"] = "Blende seltene Gegner aus deren Beute bekannt ist."
+L["options_toggle_hide_done_rare_desc"] = "Blendet alle seltenen Gegner deren Beute bereits bekannt ist aus."
+L["options_toggle_hide_minimap"] = "Blende alle Symbole auf der Minikarte aus"
+L["options_toggle_hide_minimap_desc"] = "Blendet alle Symbole dieses Addons auf der Minikarte aus und zeigt sie nur auf der Hauptkarte an."
+
+L["options_toggle_battle_pets_desc"] = "Zeigt Standorte von Kampfhaustiertrainern und NPCs an."
+L["options_toggle_battle_pets"] = "Kampfhaustiere"
+L["options_toggle_caves_desc"] = "Zeigt Höhleneingänge für andere Symbole an"
+L["options_toggle_caves"] = "Höhlen"
+L["options_toggle_chests_desc"] = "Zeigt die Positionen von Truhen an (täglich plünderbar)."
+L["options_toggle_chests"] = "Truhen"
+L["options_toggle_misc"] = "Sonstiges"
+L["options_toggle_npcs"] = "NPCs"
+L["options_toggle_rares_desc"] = "Zeigt die Positionen von seltenen Gegnern an."
+L["options_toggle_rares"] = "Seltene Gegner"
+L["options_toggle_supplies_desc"] = "Zeigt alle möglichen Standorte für Kisten mit Kriegsvorräten an."
+L["options_toggle_supplies"] = "Kisten mit Kriegsvorräten"
+L["options_toggle_treasures"] = "Schätze"
+
+---------------------------------- TOOLTIP ---------------------------------
+
+L["options_tooltip_settings"] = "Tooltip"
+L["options_tooltip_settings_desc"] = "Tooltip"
+L["options_toggle_show_loot"] = "Zeige Beute"
+L["options_toggle_show_loot_desc"] = "Zeigt Beuteinformationen im Tooltip an."
+L["options_toggle_show_notes"] = "Zeige Notizen"
+L["options_toggle_show_notes_desc"] = "Zeigt hilfreiche Notizen an sofern diese verfügbar sind."
+
+--------------------------------- DEVELOPMENT ---------------------------------
+
+L["options_dev_settings"] = "Entwicklung"
+L["options_dev_settings_desc"] = "Entwicklungseinstellungen"
+L["options_toggle_show_debug"] = "Debug"
+L["options_toggle_show_debug_desc"] = "Zeige Debugger sachen"
+L["options_toggle_force_nodes"] = "Erzwinge Punkte"
+L["options_toggle_force_nodes_desc"] = "Anzeige aller Knoten erzwingen"
