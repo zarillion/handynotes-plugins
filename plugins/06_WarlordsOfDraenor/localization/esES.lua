dbb6e3efd163fbcc851deceef074bf2217c15372
Krovikan <krovikan@gmail.com>
2020-08-30 00:46:17 +0200

    Add new file to WoD esEs.lua
    Done the few lines.

---
+++ plugins/06_WarlordsOfDraenor/localization/esES.lua
@@ -0,0 +1,20 @@
+--[[
+	Spanish Localization by (EU) Krovikan-Minahonda
+--]]
+
+local ADDON_NAME, _ = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "esES", false, true)
+if not L then return end
+
+-------------------------------------------------------------------------------
+----------------------------------- GENERAL -----------------------------------
+-------------------------------------------------------------------------------
+
+L["context_menu_title"] = "HandyNotes Warlords of Draenor"
+L["options_title"] = "Warlords of Draenor"
+
+-------------------------------------------------------------------------------
+----------------------------------- VOIDTALON ---------------------------------
+-------------------------------------------------------------------------------
+
+L["edge_of_reality"] = "Acantilado de la Realidad"
