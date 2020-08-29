6f1689f505789b6854a9f74a00030d34a1078fc7
Dathwada <57680823+Dathwada@users.noreply.github.com>
2020-08-29 23:25:02 +0200

    Create deDE.lua
    not yet finished, i need the official blizzard translation for 100% accuracy

---
+++ plugins/09_Shadowlands/localization/deDE.lua
@@ -0,0 +1,145 @@
+local ADDON_NAME, ns = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "deDE", false, true)
+if not L then return end
+
+-------------------------------------------------------------------------------
+----------------------------------- GENERAL -----------------------------------
+-------------------------------------------------------------------------------
+
+L["context_menu_title"] = "HandyNotes Shadowlands"
+L["options_title"] = "Shadowlands"
+L["covenant_required"] = "Benötigt ein Mitglied der %s"
+
+-------------------------------------------------------------------------------
+--------------------------------- ARDENWEALD ----------------------------------
+-------------------------------------------------------------------------------
+
+L["deifir_note"] = "Reite auf diesem seltenen Gegner und benutze die Fähigkeiten {spell:319566} und {spell:319575} um ihn zu verlangsamen und zu betäuben."
+L["faeflayer_note"] = "In einer kleinen Höhle, versteckt hinter einem Wasserfall."
+L["lehgo_note"] = "Zerstöre Quivering Gorm Eggs und töte {npc:171827} bis er erscheint. In einer Höhle (Eingang nach Südosten in den Staubigen Höhlen (58.5 31.9))." --review Blizzard translation
+L["rootwrithe_note"] = "Stoße die {npc:167928}s bis der seltene Gegner erscheint."
+L["rotbriar_note"] = "Sprich mit {npc:171684} in der Nähe um den seltenen Gegner nach einem Dialog zu beschwören."
+L["slumbering_note"] = "In den Nebel zu laufen wird dich betäuben und wieder herausteleportieren. Benutze ein Leuchtfeuer oder Begleiter mit Flächenschaden um ihn aus dem Nebel zu stoßen."
+L["skuld_vit_note"] = "In einer Höhle, blockiert von einer Barriere. Eine Nachtfae muss {spell:310143} benutzen um die Höhle betreten zu können. Sobald er gepullt wird verschwindet die Barriere."
+L["valfir_note"] = "Klicke the Sparkling Animaseed midway den weg herunter und benutze {spell:338045} um seinen Stärkungszauber {spell:338038} zu entfernen." --review Blizzard translation
+L["wrigglemortis_note"] = "Pull den {npc:164179} um den seltenen Gegner erscheinen zu lassen." --review
+
+L["star_lake"] = "Sternseeamphitheater" --review Blizzard translation
+L["star_lake_note"] = "Sprich mit {npc:171743}, dem Regisseur um eine der besonderen Begegnungen zu beginnen." --review Blizzard translation
+
+L["desiccated_moth"] = "Springe auf {npc:169997}, nordwestlich des Baumes um auf einem Ast zu schwimmen. Verbrenne {item:180784} im Weihrauchbrenner um den Schatz zu sammeln."
+L["dreamsong_heart"] = "Benutze {npc:169997} weit im Nordosten um in den Baum geschleudert zu werden."
+L["enchanted_dreamcatcher"] = "Hängt oben von den Wurzeln herunter. Am einfachsten hochzuklettern ist es auf der Westlichenseite."
+L["faerie_trove"] = "Befindet sich unter der Plattform."
+L["harmonic_chest"] = "Benötigt zwei Spieler. Einer spielt die Harfe und der andere die Trommel um die Truhe freizuschalten."
+L["hearty_dragon_plume"] = "Benutze {spell:333554} an der Spitze der nahe gelegenen Wasserfälle um auf den Ast zu schweben."
+L["old_ardeite_note"] = "Töte {npc:160747} und {npc:160748} in Shimmerbough im Südosten um {item:174042} zu erhalten. Benutze den Gegenstand um zu dem selten Gegner zu fliegen."
+L["swollen_anima_seed"] = "Ein großer Samen der in einem Baumstamm sitzt."
+
+-------------------------------------------------------------------------------
+----------------------------------- BASTION -----------------------------------
+-------------------------------------------------------------------------------
+
+L["aspirant_eolis_note"] = "Plündere {item:180613} in der Nähe und lese es ihm vor. Es erscheint eine Truhe."
+L["basilofos_note"] = "Beweg dich um den Felsen herum bis ein lila Fixierungsmarker über deinem Kopf erscheint, stehe dann still und warte vier Emotes ab bis er erscheinen." --review
+L["beasts_of_bastion"] = "Bestien der Bastion" --review
+L["beasts_of_bastion_note"] = "Sprich mit {npc:161441} um eine der vier Bestien zu beschwören."
+L["corrupted_clawguard_note"] = "Plündere einen {item:180651} im Schmiedefeuer Außenposten (den Berg hoch) und benutze es um {npc:171300} zu reparieren." --review location name
+L["dark_watcher_note"] = "Kann nur während des Todes gesehen werden. Sprich mit ihr und sie wird {spell:332830} auf dich wirken, bevor sie angreift."
+L["demi_hoarder_note"] = "Startet mit 99 Stapeln von {spell:333874}, Reduzierung des erlittenen Schadens. Stapel werden langsam durch Schaden entfernt. Der seltene Gegner folgt einem Pfad und verschwindet wenn er das Ende erreichen."
+L["dionae_note"] = "Wenn sie immun wird klicke auf die vier {npc:163747} um ihr Schild zu brechen."
+L["herculon_note"] = [[
+Sammel {item:172451} und benutze diese um {npc:158659} Stapel von {spell:343531} zu gewähren. Bei 10 Stapeln wird er aktiv.
+
+Partikel können von nahegelegenen erschöpften Anima Kanistern gesammelt werden die sich in diesem Raum oder direkt außerhalb befinden.
+]]
+L["nikara_note"] = "Klicke mit zwei anderen Spielern die uralten Räuchergefäße an um ihn zu beschwören." --review Blizzard translation
+L["wingflayer_note"] = "Klicken zum Beschwören auf das Horn auf dem Tisch in der Nähe (Südosten, die Treppe hoch)."
+
+L["broken_flute"] = "Töte nahegelegene {npc:170009} bis diese {item:180536} fallen lassen, dann benutze die Werkzeuge um es zu reparieren."
+L["experimental_construct_part"] = "Plündere ein {item:180534} in der Nähe und benutze es um den Gegenstand zu reparieren. Das {item:180534} erscheint an verschiedenen"
+L["larion_harness"] = "Befindet sich in der Halle der Bestien."
+L["memorial_offering"] = "Kaufe ein {item:180788} von {npc:171526} in Aspirantenruhe. Stelle es in das Getränketablett in der Nähe der Truhe um {item:180797} zu erhalten." --review location name
+L["scroll_of_aeons"] = "Plündere 2 {item:173973} im mittleren Bereich und lege sie auf die nahe gelegenen Tributplatten um den Schatz zu enthüllen." --review Blizzard translation
+
+L["gift_of_agthia"] = "Klicke auf die brennende Fackel in der Nähe der kaputten Brücke im Nordwesten (39.1 54.5) und trage {spell:333320} von Fackel zu Fackel bis du die Truhe erreichst. Wenn du die letzte Fackel anzündest wird dir {spell:333063} gewährt."
+L["gift_of_chyrus"] = "Knie vor er Truhe um {spell:333045} zu erhalten."
+L["gift_of_thenios"] = [[
+Hinter der Truhe ist eine Flugfeld mit dem Namen "Pfad der Weisheit". Dies führt zu einer Folge von Plattformen mit unterschiedlichen Räuchergefäßen mit denen kommuniziert werden kann:
+
+• Incense of Knowledge
+• Incense of Patience
+• Incense of Insight
+• Incense of Judgement
+
+Kommuniziere mit ihnen in der Reihenfolge Patience => Knowledge => Insight. Auf der Judgement Plattform wird eine Kugel mit dem Namen "Path of Insightful Wisdom" erscheinen.
+
+Die Kugel wird dich zu der wahren Incense of Judgment bringen. Kommuniziere dort mit ihr und das letzte Flugfeld wird dir {spell:333068} gewähren um die Truhe öffnen zu können.
+]] -- review Blizzard translations
+L["gift_of_vesiphone"] = "Läute an den Glocken um {npc:170849} erscheinen zu lassen, töte es um den Schwächungszauber {spell:333239} zu erhalten. Der Wasserfall gegenüber der Truhe wird diesen Schwächungszauber reinigen und dir {spell:332785} gewähren um die Truhe öffnen zu können."
+
+-------------------------------------------------------------------------------
+--------------------------------- MALDRAXXUS ----------------------------------
+-------------------------------------------------------------------------------
+
+L["chelicerae_note"] = "Zerstöre das {npc:159885} um den seltenen Gegner zu aktivieren."
+L["gristlebeak_note"] = "Zerbrich alle {npc:162761} in der Nähe um den seltenen Gegner angreifen zu können."
+L["malkorak_note"] = "An der Spitze des Turms (eine menge Treppenstufen)."
+L["schmitd_note"] = "Benutze nahegelegene {spell:313451} um sein Schild zu brechen."
+
+L["mixed_pool_note"] = [[
+Sammel Materialien von umliegenden Gegnern und wirf diese in den Teich. Sobald 30 Materialien hinzugefügt wurden erscheint je nach verwendeter Kombination einer von sieben seltenen Gegnern.
+
+• Sammel {spell:306713} von {npc:167923} und {npc:167948} aus dem Norden.
+
+• Sammel {spell:306719} von {npc:165015} und {npc:171142} aus dem Süden.
+
+• Sammel {spell:306722} von {npc:165027} und {npc:166438} aus dem Süden.
+]]
+
+L["blackhound_cache"] = "Blackhound Cache" --review Blizzard translation
+L["bladesworn_supply_cache"] = "Bladesworn Supply Cache" --review Blizzard translation
+L["glutharns_note"] = "In einer Höhle hinter dem Schleimwasserfall. Töte {npc:172485} und beide {npc:172479} um die Truhe freizuschalten."
+L["kyrian_keepsake_note"] = "Untersuche {npc:169664} um den Schatz zu plündern."
+L["plaguefallen_chest_note"] = [[
+Stehe im grünen Schleim (benötigt Heilung!) um 10 Stapel von {spell:330069} zu erhalten um in ein {spell:330092} verwandelt zu werden.
+
+Wenn du verwandelt bist, besuche die Höhle unterhalb von {npc:158406}'s Plattform (Eingang auf der Östlichenseite ()) und klicke auf das Rohr um zur Truhe zu gelangen. --add coordinates
+]]
+L["stolen_jar_note"] = "In einer Höhle unter dem Riesenpilz."
+L["strange_growth_note"] = "Ziehe an {npc:165037} um den Schatz zu enthüllen."
+L["vat_of_slime_note"] = "Klicken auf dem Tisch die Flasche an und dann auf den Schleimbehälter."
+
+L["pet_cat"] = "Streichle die verdammte Katze!"
+L["hairball"] = "|cFFFF0000Erscheint nur im Schwärenden Sanktum in dem Dungeon Seuchensturz!|r" --review
+L["lime"] = "Sitzt auf dem großen Knochenbogen."
+L["moldstopheles"] = "Laufe um den Stiel herum und springe auf die Pilzplattformen. Verwende für die letzte Plattform ein Reittier und springe in die Ecke an der die Plattform am Stiel befestigt ist."
+L["pus_in_boots"] = "Befindet sich unter der Brücke."
+
+L["options_toggle_kittens"] = "Kätzchen" --review Blizzard translation
+L["options_toggle_kittens_desc"] = "Zeige die Standorte der Kätzchen für den Erfolg Nine Afterlives an." --review Blizzard traslation
+
+-------------------------------------------------------------------------------
+--------------------------------- REVENDRETH ----------------------------------
+-------------------------------------------------------------------------------
+
+L["endlurker_note"] = "Klicke Anima Stake neben {npc:165229}s Leiche an und benutze {spell:321826} on top of the Shimmering Rift." --review Blizzard traslation
+L["executioner_aatron_note"] = "Töte die drei {npc:166715} in der nähe um {spell:324872} zu entfernen."
+L["famu_note"] = "Sprich mit {npc:166483} um das Event auszulösen."
+L["leeched_soul_note"] = "In der nahe gelegenen Krypta. Gehe in die Nähe von {npc:165151} um das Event zu starten."
+L["lord_mortegore_note"] = "Kill surrounding mobs to obtain {item:174378} and use it to empower a {npc:161870}. The rare will spawn once all four sigils are empowered."
+L["scrivener_lenua_note"] = "Bringe {npc:160753} zurück zur Verbotenen Bibliothek." --review Blizzard traslation
+L["soulstalker_doina_note"] = "Verfolge sie nach unten und durch den Spiegel wenn sie wegläuft."
+
+L["darkreach_supplies"] = "Springe auf einen {npc:169995} und gleiten nach Südwesten in den ausgehöhlten Turm."
+L["dredglaive_note"] = "Unter der Brücke in der {npc:173671} Leiche."
+L["lost_quill_note"] = "Plündere {item:182475} von der Flasche auf dem Tisch in der Verbotenen Bibliothek."
+
+L["bleakwood_chest"] = "Bleakwood Chest" --review Blizzard traslation
+L["chest_of_envious_dreams"] = "Truhe neidischer Träume" --review Blizzard traslation
+L["gilded_plum_chest"] = "Vergoldete Pflaumenkiste" --review Blizzard traslation
+L["gilded_plum_chest_note"] = "Töte {npc:166680} der die Straße hoch und runter läuft."
+L["wayfairer_spoils"] = "Verlassene Beute des Wanderers" --review Blizzard traslation
+
+L["options_toggle_carriages"] = "Kutschen"
+L["options_toggle_carriages_desc"] = "Zeige die Standorte und Wege von fahrbaren Kutschen an."
