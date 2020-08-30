3991efa74ec0f67c150d293cce718d47f8bfe5b5
Krovikan <krovikan@gmail.com>
2020-08-30 23:23:39 +0200

    Maked new esES file for Shadowlands
    Initial commit

---
+++ plugins/09_Shadowlands/localization/esES.lua
@@ -0,0 +1,7 @@
+--[[
+	Spanish Localization by (EU) Krovikan-Minahonda
+--]]
+
+local ADDON_NAME, _ = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "esES", false, true)
+if not L then return end
