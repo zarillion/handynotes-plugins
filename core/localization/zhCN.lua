8fe02aa75ed329e86312e7cd25ed2c7bfd927621
Adavak LEE <adavak@adavak.com>
2020-09-02 18:05:07 +0800

    Add new and updated zhCN translations

--- core/localization/zhCN.lua
+++ core/localization/zhCN.lua
@@ -10,6 +10,7 @@ L["cloth"] = "布甲"
 L["leather"] = "皮甲"
 L["mail"] = "锁甲"
 L["plate"] = "板甲"
+L["cosmetic"] = "装饰品"
 
 L["1h_mace"] = "单手锤"
 L["1h_sword"] = "单手剑"
@@ -30,7 +31,7 @@ L["offhand"] = "副手"
 L["warglaive"] = "战刃"
 
 L["ring"] = "戒指"
-L["amulet"] = "项链"
+L["neck"] = "项链"
 L["cloak"] = "披风"
 L["trinket"] = "饰品"
 
@@ -38,10 +39,17 @@ L["trinket"] = "饰品"
 ---------------------------------- TOOLTIPS -----------------------------------
 -------------------------------------------------------------------------------
 
-L["retrieving"] = "正在获取此项信息"
+L["activation_unknown"] = "激活未知！"
+L["requirement_not_found"] = "所需位置未知！"
+
+L["Requires"] = "所需"
+L["focus"] = "焦点"
+L["retrieving"] = "正在获取物品链接…"
 L["in_cave"] = "在洞穴。"
 L["in_small_cave"] = "在小洞穴。"
 L["in_water_cave"] = "在水下洞穴。"
+L["in_water"] = "在水中。"
+L["daily"] = "每日"
 L["weekly"] = "每周"
 L["normal"] = "普通"
 L["hard"] = "困难"
@@ -54,30 +62,37 @@ L["known"] = "已获得"
 L["missing"] = "未获得"
 L["unobtainable"] = "无法获得"
 L["unlearnable"] = "无法解锁"
+L["A"] = "生" -- available/alive
+L["D"] = "死" -- dead/defeated
+L["defeated"] = "已击杀"
+L["undefeated"] = "未击杀"
 
 -------------------------------------------------------------------------------
 --------------------------------- CONTEXT MENU --------------------------------
 -------------------------------------------------------------------------------
 
-L["context_menu_add_tomtom"] = "加入到TomTom"
+L["context_menu_set_waypoint"] = "设置地图路径点"
+L["context_menu_add_tomtom"] = "加入到 TomTom"
 L["context_menu_hide_node"] = "隐藏此项"
 L["context_menu_restore_hidden_nodes"] = "恢复所有隐藏项"
+
+L["map_button_text"] = "调整此地图上的图标显示，透明度和缩放。"
 
 -------------------------------------------------------------------------------
 ----------------------------------- OPTIONS -----------------------------------
 -------------------------------------------------------------------------------
 
-L["options_global"] = "Global"
-L["options_zones"] = "Zones"
+L["options_global"] = "全局"
+L["options_zones"] = "区域"
 
-L["options_general_description"] = "Settings that control the behavior of nodes and their rewards."
-L["options_global_description"] = "Settings that control the display of all nodes in all zones."
-L["options_zones_description"] = "Settings that control the display of nodes in each individual zone."
+L["options_general_description"] = "控制该项的特性及其奖励的设置。"
+L["options_global_description"] = "控制全部区域中全部的项显示的设置。"
+L["options_zones_description"] = "控制每个单独区域中的项显示的设置。"
 
 ------------------------------------ ICONS ------------------------------------
 
 L["options_icon_settings"] = "图标设置"
-L["options_scale"] = "縮放"
+L["options_scale"] = "缩放"
 L["options_scale_desc"] = "1 = 100%"
 L["options_opacity"] = "透明度"
 L["options_opacity_desc"] = "0 = 透明, 1 = 不透明"
@@ -85,43 +100,53 @@ L["options_opacity_desc"] = "0 = 透明, 1 = 不透明"
 ---------------------------------- VISIBILITY ---------------------------------
 
 L["options_visibility_settings"] = "可见性"
-L["options_general_settings"] = "一般"
-L["options_toggle_hide_done_rare"] = "隐藏已解锁所有掉落的稀有"
-L["options_toggle_hide_done_rare_desc"] = "隐藏已解锁所有掉落的稀有，这意味着如果该稀有你没有需求，则不会显示在地图上"
+L["options_general_settings"] = "通用"
+L["options_show_completed_nodes"] = "显示已完成"
+L["options_show_completed_nodes_desc"] = "显示全部项即使它们今天已被拾取或完成。"
+L["options_toggle_hide_done_rare"] = "隐藏所有拾取已解锁的稀有。"
+L["options_toggle_hide_done_rare_desc"] = "隐藏所有拾取已解锁的稀有。"
 L["options_toggle_hide_minimap"] = "隐藏小地图上的所有图标"
-L["options_toggle_hide_minimap_desc"] = "在小地图上隐藏此插件的所有图标，当启用此选项时，插件只会在世界地图上显示"
+L["options_toggle_hide_minimap_desc"] = "在小地图上隐藏此插件的所有图标，只会在世界地图上显示。"
 L["options_restore_hidden_nodes"] = "恢复隐藏项"
-L["options_restore_hidden_nodes_desc"] = "恢复所有由右键菜单隐藏的节点"
+L["options_restore_hidden_nodes_desc"] = "使用右击菜单恢复全部隐藏项。"
 
-L["options_icons_pet_battles_desc"] = "显示宠物训练师的位置"
+L["options_icons_pet_battles_desc"] = "显示宠物训练师和 NPC 的位置。"
 L["options_icons_pet_battles"] = "战斗宠物"
-L["options_icons_caves_desc"] = "显示其它项的洞穴入口"
+L["options_icons_caves_desc"] = "显示其它项的洞穴入口。"
 L["options_icons_caves"] = "洞穴"
-L["options_icons_daily_chests_desc"] = "显示宝箱位置 (每日刷新)。"
+L["options_icons_daily_chests_desc"] = "显示宝箱位置（每日可拾取）。"
 L["options_icons_daily_chests"] = "宝箱"
+L["options_icons_daily_quests_desc"] = "显示未标记每日任务的位置。"
+L["options_icons_daily_quests"] = "每日任务"
 L["options_icons_misc"] = "杂项"
 L["options_icons_npcs"] = "NPC"
-L["options_icons_other"] = "其他图标"
-L["options_icons_rares_desc"] = "显示稀有位置"
+L["options_icons_other_desc"] = "显示其它未分类项的位置。"
+L["options_icons_other"] = "其它"
+L["options_icons_quests_desc"] = "显示未标记任务线的位置。"
+L["options_icons_quests"] = "任务"
+L["options_icons_rares_desc"] = "显示稀有 NPC 的位置。"
 L["options_icons_rares"] = "稀有"
-L["options_icons_supplies_desc"] = "显示战争补给箱（空投）的位置"
-L["options_icons_supplies"] = "战争补给箱（空投）"
+L["options_icons_supplies_desc"] = "显示全部可能出现战争补给箱的位置。"
+L["options_icons_supplies"] = "战争补给箱"
 L["options_icons_treasures"] = "宝藏"
+L["options_icons_treasures_desc"] = "显示隐藏宝藏位置。"
 
 ---------------------------------- TOOLTIP ---------------------------------
 
-L["options_tooltip_settings"] = "鼠标提示"
-L["options_tooltip_settings_desc"] = "鼠标提示"
-L["options_toggle_show_loot"] = "显示掉落"
-L["options_toggle_show_loot_desc"] = "在鼠标提示中加入掉落信息"
-L["options_toggle_show_notes"] = "显示注解"
-L["options_toggle_show_notes_desc"] = "在鼠标提示中添加注解"
+L["options_tooltip_settings"] = "提示"
+L["options_tooltip_settings_desc"] = "提示"
+L["options_toggle_show_loot"] = "显示拾取"
+L["options_toggle_show_loot_desc"] = "在提示中加入拾取信息"
+L["options_toggle_show_notes"] = "显示注释"
+L["options_toggle_show_notes_desc"] = "在提示中添加注释"
 
 --------------------------------- DEVELOPMENT ---------------------------------
 
 L["options_dev_settings"] = "开发"
 L["options_dev_settings_desc"] = "开发者选项"
-L["options_toggle_show_debug"] = "调试"
-L["options_toggle_show_debug_desc"] = "显示调试内容"
-L["options_toggle_force_nodes"] = "强制显示节点"
-L["options_toggle_force_nodes_desc"] = "强制显示所有节点"
+L["options_toggle_show_debug_map"] = "除错地图 ID"
+L["options_toggle_show_debug_map_desc"] = "显示地图除错信息"
+L["options_toggle_show_debug_quest"] = "除错任务 ID"
+L["options_toggle_show_debug_quest_desc"] = "显示任务除错更改信息"
+L["options_toggle_force_nodes"] = "强制显示项"
+L["options_toggle_force_nodes_desc"] = "强制显示所有项"
