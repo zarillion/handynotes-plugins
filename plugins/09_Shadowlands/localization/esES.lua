b5345d77dc6214ac4e1e21409a16d5c6ee1e0e6b
Krovikan <krovikan@gmail.com>
2020-09-02 19:21:58 +0200

    Added anima #45 and the last in English
    65% translated. Preparing the locale esES.

--- plugins/09_Shadowlands/localization/esES.lua
+++ plugins/09_Shadowlands/localization/esES.lua
@@ -94,6 +94,7 @@ L["gift_of_vesiphone"] = "Toca una de las campanas para hacer que aparezca una {
 L["anima_shard"] = "Fragmento de Ánima perdida"--Revisar que Lost Anima Shard sea Fragmento de Ánima perdida
 L["anima_shard_61225"] = "En una plataforma inferior debajo del puente."
 L["anima_shard_61235"] = "En una repisa sobre la cascada."
+L["anima_shard_61236"] = "Sobre un arco enmedio de la estructura principal."
 L["anima_shard_61237"] = "En una repisa justo sobre el agua."
 L["anima_shard_61238"] = "En el agua debajo de un puente pequeño."
 L["anima_shard_61239"] = "Encima de una fina columna de piedra."
@@ -142,3 +143,84 @@ L["options_toggle_anima_shards_desc"] = "Muestra la localización de los 50 frag
 -------------------------------------------------------------------------------
 --------------------------------- MALDRAXXUS ----------------------------------
 -------------------------------------------------------------------------------
+
+L["chelicerae_note"] = "Destroy the {npc:159885} to activate the rare."
+L["deepscar_note"] = "Can appear in multiple entrances to the Theater of Pain."
+L["gieger_note"] = "Pull the {npc:162815} to activate the rare."
+L["gristlebeak_note"] = "Break all nearby {npc:162761} to engage the rare."
+L["malkorak_note"] = "At the top of the tower (lots of stairs)."
+L["schmitd_note"] = "Use nearby {spell:313451} to break his shield."
+
+L["mixed_pool_note"] = [[
+Gather ingredients from the surrounding mobs and toss them into the pool. Once 30 ingredients have been added, one of seven rares will spawn depending on the combination used.
+
+• Collect {spell:306713} from {npc:167923} and {npc:167948} to the north.
+
+• Collect {spell:306719} from {npc:165015} and {npc:171142} to the south.
+
+• Collect {spell:306722} from {npc:165027} and {npc:166438} to the south.
+]]
+
+L["blackhound_cache"] = "Blackhound Cache"
+L["bladesworn_supply_cache"] = "Bladesworn Supply Cache"
+L["glutharns_note"] = "In a cave behind the waterfall of slime. Kill {npc:172485} and both {npc:172479} to unlock the chest."
+L["kyrian_keepsake_note"] = "Inspect the {npc:169664} to loot the treasure."
+L["plaguefallen_chest_note"] = [[
+Stand in the green slime (requires healing!) to get 10 stacks of {spell:330069} and be transformed into a {spell:330092}.
+
+Once transformed, visit the cave underneath {npc:158406}'s platform (entrance on the east side) and click the pipe to transport to the chest.
+]]
+L["stolen_jar_note"] = "In a cave underneath the giant mushroom."
+L["strange_growth_note"] = "Pull on the {npc:165037} to reveal the treasure."
+L["vat_of_slime_note"] = "Click the bottle on the table and then click on the vat of slime."
+
+L["pet_cat"] = "Pet the damn cat!"
+L["hairball"] = ns.color.Red("Only appears in the Festering Sanctum inside the Plaguefall dungeon!")
+L["lime"] = "Sitting on top of the large bone arch."
+L["moldstopheles"] = "Run around the back of the stalk and jump up the mushroom platforms. For the final platform, use a mount and jump at the corner where it meets the stalk."
+L["pus_in_boots"] = "Located under the bridge."
+
+L["options_toggle_kittens"] = "Kittens"
+L["options_toggle_kittens_desc"] = "Display locations of kittens for the Nine Afterlives achievement."
+
+-------------------------------------------------------------------------------
+--------------------------------- REVENDRETH ----------------------------------
+-------------------------------------------------------------------------------
+
+L["amalgamation_of_light_note"] = "Move all three mirror traps to release the rare."
+L["bog_beast_note"] = "Has a chance to spawn during the world quest \"Muck It Up\" after using {item:177880} on a {npc:166206}."
+L["endlurker_note"] = "Click Anima Stake near {npc:165229} corpse and use {spell:321826} on top of the Shimmering Rift."
+L["executioner_aatron_note"] = "Kill the three nearby {npc:166715} to remove {spell:324872}."
+L["famu_note"] = "Talk to {npc:166483} to trigger the event."
+L["grand_arcanist_dimitri_note"] = "Kill the four {npc:167467} to release the rare."
+L["innervus_note"] = "Kill nearby {npc:160375}s to obtain a {item:177223} and unlock the crypt."
+L["leeched_soul_note"] = "Inside the nearby crypt. Walk near {npc:165151} to start the event."
+L["lord_mortegore_note"] = "Kill surrounding mobs to obtain {item:174378} and use it to empower a {npc:161870}. The rare will spawn once all four sigils are empowered."
+L["manifestation_of_wrath_note"] = "Has a chance to spawn when a {npc:169916} is recovered during the \"Swarming Souls\" world quest."
+L["scrivener_lenua_note"] = "Return {npc:160753} to the Forbidden Library."
+L["sinstone_hoarder_note"] = "Appears on the minimap as a treasure chest. Attempt to loot the {npc:162503} and the rare will reveal itself."
+L["soulstalker_doina_note"] = "Follow downstairs and through the mirror when she runs away."
+L["tomb_burster_note"] = "Can be spawned if {npc:155777} is trapped in a web. Kill nearby {npc:155769}s and waves of {npc:155795}s until the rare spawns."
+L["worldedge_gorger_note"] = "Obtain an {item:173939} from World Reavers, Devourers and Mites in the Banewood and the Endmire. Use it to light the Worldedge Braziers and summon the rare."
+
+L["darkreach_supplies"] = "Jump on the {npc:169995} and glide southwest into the hollowed spire directly above the Cache of the Night treasure."
+L["dredglaive_note"] = "Under the bridge in the {npc:173671} corpse."
+L["lost_quill_note"] = "Loot {item:182475} from the bottle on the table in the Forbidden Library."
+L["secret_chamber_note"] = "Loot a Discarded Anima Canister in front of the locked door and learn {spell:340701}. Use it to drain five of the nearby {npc:173838}s, then use {spell:340866} in front of the {npc:173786}."
+L["taskmaster_trove_note"] = "Read the Ingress and Egress Rites then carefully make your way to the chest."
+L["the_count_note"] = "Farm 99 {currency:1820} in the Endmire and then bring them to {npc:173488}."
+
+L["abandoned_curios"] = "Abandoned Curios"
+L["bleakwood_chest"] = "Bleakwood Chest"
+L["chest_of_envious_dreams"] = "Chest of Envious Dreams"
+L["filchers_prize"] = "Filcher's Prize"
+L["fleeing_soul_bundle"] = "Fleeing Soul's Bundle"
+L["gilded_plum_chest"] = "Gilded Plum Chest"
+L["gilded_plum_chest_note"] = "Kill the {npc:166680} wandering up and down the road."
+L["remlates_cache"] = "Remlate's Hidden Cache"
+L["remlates_cache_note"] = "On the outer wall of Darkhaven behind the crypt."
+L["smuggled_cache"] = "Smuggled Cache"
+L["wayfairer_spoils"] = "Wayfairer's Abandoned Spoils"
+
+L["options_toggle_carriages"] = "Carriages"
+L["options_toggle_carriages_desc"] = "Display locations and paths of rideable carriages."
