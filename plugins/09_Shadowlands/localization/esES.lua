573090061714c89edb5ab1ec13e95d37da3e2338
Krovikan <krovikan@gmail.com>
2020-09-14 02:16:06 +0200

    Updated Shadowlands esES

--- plugins/09_Shadowlands/localization/esES.lua
+++ plugins/09_Shadowlands/localization/esES.lua
@@ -8,7 +8,7 @@ if not L then return end
 ----------------------------------- GENERAL -----------------------------------
 -------------------------------------------------------------------------------
 
-L["context_menu_title"] = "HandyNotes Shadowlands"
+L["context_menu_title"] = "HandyNotes: Shadowlands"
 L["options_title"] = "Shadowlands"
 L["covenant_required"] = "Requiere ser miembro de la curia %s."
 
