781b5358f5b199e7f1f77efc81febbe34e736412
Krovikan <krovikan@gmail.com>
2020-10-18 23:29:09 +0200

    Translated Timeless Isle esES

--- plugins/05_MistsOfPandaria/localization/esES.lua
+++ plugins/05_MistsOfPandaria/localization/esES.lua
@@ -3,3 +3,33 @@
 local ADDON_NAME, _ = ...
 local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "esES", false, true)
 if not L then return end
+
+-------------------------------------------------------------------------------
+-------------------------------- TIMELESS ISLE --------------------------------
+-------------------------------------------------------------------------------
+
+L["chelon_note"] = "Haz click en el Caparazón Vacío Llamativo para hacer aparecer a {npc:72045}."
+L["dread_ship_note"] = "Despoja el {item:104115} de {npc:73279} y úsalo en la Lápida Maldita para invocar al {npc:73281}."
+L["emerald_gander_note"] = "Mata a las {npc:72762} que hay alrededor de la Corte Celestial hasta que aparezca el {npc:73158}."
+L["great_turtle_furyshell_note"] = "Mata {npc:72764} o {npc:72763} hasta que aparezca la {npc:73161}."
+L["imperial_python_note"] = "Mata a las {npc:72841} que hay alrededor de la Corte Celestial hasta que aparezca la {npc:73163}."
+L["ironfur_steelhorn_note"] = "Mata a los {npc:72844} que hay alrededor de la Corte Celestial hasta que aparezca el {npc:73160}."
+L["monstrous_spineclaw_note"] = "Alrededor de toda la isla. Mata a los {npc:72766} hasta que aparezca el {npc:73166}."
+L["spelurk_note"] = "Si no puedes usar la {item:70161} (u otro juguete para sentarse) justo enfrente de las rocas. Siéntate. Cuando te levantes camina recto hacia dentro."
+
+L["blazing_chest"] = "Cofre Llameante"
+L["moss_covered_chest"] = "Cofre Cubierto de Musgo"
+L["skull_covered_chest"] = "Cofre Adornado de Calaveras"
+L["smoldering_chest"] = "Cofre Humeante"
+L["sturdy_chest"] = "Cofre Robusto"
+L["sturdy_chest_note"] = "Golpea a un {npc:73531} para ser llevado aquí."
+
+L["gleaming_treasure_satchel_note"] = "Camina por las cuerdas del barco y salta al poste donde está colgando la cartera."
+L["gleaming_treasure_chest_note"] = "Salta por los pilares hasta llegar al tesoro."
+L["mist_covered_treasure_chest_note"] = [[
+Requiere haber despojado antes Cofre del tesoro reluciente y Cofre del tesoro atado con cuerda.
+
+Haz click en la Estatua de la Grulla Reluciente (58,60) para volar hasta alcanzar el tesoro.
+]]
+L["ropebound_treasure_chest_note"] = "Camina por las cuerdas hasta llegar al tesoro."
+L["sunken_treasure_note"] = "Mata élites en el barco hundido hasta conseguir la llave."
