ba0c4eab6f065ac021760b466f85fc5d2507f989
Krovikan <krovikan@gmail.com>
2020-10-29 12:01:57 +0100

    Translated new lines for Draenor esES

--- plugins/06_WarlordsOfDraenor/localization/esES.lua
+++ plugins/06_WarlordsOfDraenor/localization/esES.lua
@@ -9,3 +9,12 @@ if not L then return end
 -------------------------------------------------------------------------------
 
 L["edge_of_reality"] = "Acantilado de la Realidad"
+
+-------------------------------------------------------------------------------
+--------------------------------- TANAAN JUNGLE -------------------------------
+-------------------------------------------------------------------------------
+
+L["deathtalon_note"] = ns.color.Red("Señor de las Sombras Iskar grita: ¡Tras el velo, todo lo que encontraréis es muerte!")
+L["doomroller_note"] = ns.color.Red("Maestra de asedio Mar'tak grita: ¡Ja ja! ¡Pisotead sus cuerpos!")
+L["terrorfist_note"] = ns.color.Red("Frogan grita: ¡Un gronito enorme va hacia el Refugio Rangari! ¡Vamos a necesitar ayuda!")
+L["vengeance_note"] = ns.color.Red("Tyrana Velhari grita: ¡Los insectos merecen ser aplastados!")
