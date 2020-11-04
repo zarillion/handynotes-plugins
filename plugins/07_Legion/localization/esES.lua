c3ef8d7d730f738ffb2bbf93e3942c5596bae253
Krovikan <krovikan@gmail.com>
2020-11-05 00:02:58 +0100

    Translated new Azsuna lines Legion esES

--- plugins/07_Legion/localization/esES.lua
+++ plugins/07_Legion/localization/esES.lua
@@ -3,33 +3,37 @@
 local ADDON_NAME, ns = ...
 local L = ns.NewLocale("esES")
 if not L then return end
+
 -------------------------------------------------------------------------------
 ----------------------------------- AZSUNA ------------------------------------
 -------------------------------------------------------------------------------
 
-L["arcavellus_note"] = nil
-L["beacher_note"] = nil
-L["brogozog_note"] = nil
-L["chief_bitterbrine_note"] = nil
-L["devious_sunrunner_note"] = nil
-L["doomlord_kazrok_note"] = nil
-L["felwing_note"] = nil
-L["golza_note"] = nil
-L["infernal_lord_note"] = nil
-L["inquisitor_tivos_note"] = nil
+L["arcavellus_note"] = "Mata las {npc:90242} y las {npc:90243} hasta que aparezca {npc:90244}."
+L["beacher_note"] = "Cuando la misión del mundo {wq:Desembarco Helarjar: Marisma Gris} esté activa, no estará {npc:91187}."
+L["brogozog_note"] = "Habla con {npc:91097}."
+L["chief_bitterbrine_note"] = "En el barco, en la cubierta inferior."
+L["devious_sunrunner_note"] = [[
+Usa el Portal Ley en una cueva pequeña.
+No te de olvides despojar el cofre.
+]]
+L["doomlord_kazrok_note"] = "Habla con {npc:91580}."
+L["felwing_note"] = "Habla con {npc:105913} y mata {npc:105919} hasta que aparezca {npc:105938}."
+L["golza_note"] = "Sopla el Cuerno de la Sirena y mata {npc:90774} y {npc:90778} hasta que aparezca {npc:89816}."
+L["infernal_lord_note"] = "Haz click en el Alijo de Infernales y mata {npc:90797} hasta que aparezca el {npc:90803}."
+L["inquisitor_tivos_note"] = "Usa el Portal de la Legión. Está en el piso inferior."
 
-L["disputed_treasure"] = nil
-L["glimmering_treasure_chest"] = nil
-L["seemingly_unguarded_treasure"] = nil
-L["seemingly_unguarded_treasure_note"] = nil
-L["treasure_37958"] = nil
-L["treasure_37980"] = nil
-L["treasure_40711"] = nil
-L["treasure_42282"] = nil
-L["treasure_42283"] = nil
-L["treasure_42284"] = nil
-L["treasure_42287"] = nil
-L["treasure_42339"] = nil
+L["disputed_treasure"] = "Tesoro Disputado"
+L["glimmering_treasure_chest"] = "Cofre del Tesoro Reluciente"
+L["seemingly_unguarded_treasure"] = "Tesoro Aparentemente sin Vigilancia"
+L["seemingly_unguarded_treasure_note"] = "Intenta despojar el tesoro y entonces matar algunas oleadas de {npc:94167}."
+L["treasure_37958"] = "En un nivel inferior del edificio."
+L["treasure_37980"] = "Usa el Portal Ley en el puente roto."
+L["treasure_40711"] = "Usa el Portal Ley dentro de la torre."
+L["treasure_42282"] = "Detrás de un pilar en una esquina."
+L["treasure_42283"] = "En el segundo piso."
+L["treasure_42284"] = "Dentro de la Academia."
+L["treasure_42287"] = "Bajo el agua."
+L["treasure_42339"] = "No despiertes a los osos."
 
 L["nightwatcher_merayl_note"] = nil
 
@@ -37,8 +41,8 @@ L["nightwatcher_merayl_note"] = nil
 --------------------------------- ACROSS ZONES --------------------------------
 -------------------------------------------------------------------------------
 
-L["small_treasure_chest"] = nil
-L["treasure_chest"] = nil
-L["treasures_discovered"] = nil
+L["small_treasure_chest"] = "Arqueta Pequeña"
+L["treasure_chest"] = "Arqueta"
+L["treasures_discovered"] = "tesoros descubiertos"
 
-L["general_pet_tamer_note"] = nil
+L["general_pet_tamer_note"] = ns.color.Orange("Sólo aparece cuando la misión del mundo para él está disponible.")
