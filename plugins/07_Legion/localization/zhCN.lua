d1fe313f2e7ec9030fb6f92ec130a2b9fee27406
Adavak <adavak@adavak.com>
2020-11-05 08:23:32 +0800

    Update zhCN.lua

--- plugins/07_Legion/localization/zhCN.lua
+++ plugins/07_Legion/localization/zhCN.lua
@@ -6,29 +6,32 @@ if not L then return end
 ----------------------------------- AZSUNA ------------------------------------
 -------------------------------------------------------------------------------
 
-L["arcavellus_note"] = nil
-L["beacher_note"] = nil
-L["brogozog_note"] = nil
-L["chief_bitterbrine_note"] = nil
-L["devious_sunrunner_note"] = nil
-L["doomlord_kazrok_note"] = nil
-L["felwing_note"] = nil
-L["golza_note"] = nil
-L["infernal_lord_note"] = nil
-L["inquisitor_tivos_note"] = nil
+L["arcavellus_note"] = "击杀 {npc:90242} 和 {npc:90243} 直到稀有出现。"
+L["beacher_note"] = "当世界任务 {wq:海拉加尔登陆战：灰色浅滩} 激活时，他不出现。"
+L["brogozog_note"] = "和 {npc:91097} 交谈。"
+L["chief_bitterbrine_note"] = "位于船下层甲板。"
+L["devious_sunrunner_note"] = [[
+使用小洞穴内的魔网传送门。
+别忘了拾取宝箱。
+]]
+L["doomlord_kazrok_note"] = "和 {npc:91580} 交谈。"
+L["felwing_note"] = "和 {npc:105913} 交谈并击杀 {npc:105919} 直到稀有出现。"
+L["golza_note"] = "吹响海妖号角并击杀 {npc:90774} 和 {npc:90778} 直到稀有出现。"
+L["infernal_lord_note"] = "点击地狱火宝箱并击杀 {npc:90797} 直到稀有出现。"
+L["inquisitor_tivos_note"] = "使用军团传送门。他在下层。"
 
-L["disputed_treasure"] = nil
-L["glimmering_treasure_chest"] = nil
-L["seemingly_unguarded_treasure"] = nil
-L["seemingly_unguarded_treasure_note"] = nil
-L["treasure_37958"] = nil
-L["treasure_37980"] = nil
-L["treasure_40711"] = nil
-L["treasure_42282"] = nil
-L["treasure_42283"] = nil
-L["treasure_42284"] = nil
-L["treasure_42287"] = nil
-L["treasure_42339"] = nil
+L["disputed_treasure"] = "有争议的宝藏"
+L["glimmering_treasure_chest"] = "闪闪发光的宝箱"
+L["seemingly_unguarded_treasure"] = "看似无人看守的宝藏"
+L["seemingly_unguarded_treasure_note"] = "尝试拾取宝藏并击杀随后出现的数波 {npc:94167}。"
+L["treasure_37958"] = "位于建筑物下层。"
+L["treasure_37980"] = "使用断桥上的魔网传送门。"
+L["treasure_40711"] = "使用塔内的魔网传送门。"
+L["treasure_42282"] = "在阳台的角落。"
+L["treasure_42283"] = "位于第二层。"
+L["treasure_42284"] = "学院内。"
+L["treasure_42287"] = "水下。"
+L["treasure_42339"] = "别吵醒熊。"
 
 L["nightwatcher_merayl_note"] = nil
 
@@ -36,8 +39,8 @@ L["nightwatcher_merayl_note"] = nil
 --------------------------------- ACROSS ZONES --------------------------------
 -------------------------------------------------------------------------------
 
-L["small_treasure_chest"] = nil
-L["treasure_chest"] = nil
-L["treasures_discovered"] = nil
+L["small_treasure_chest"] = "小宝箱"
+L["treasure_chest"] = "宝箱"
+L["treasures_discovered"] = "已发现宝箱"
 
-L["general_pet_tamer_note"] = nil
+L["general_pet_tamer_note"] = ns.color.Orange("只在世界任务他出现时才会出现。")
