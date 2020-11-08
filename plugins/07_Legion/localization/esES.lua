597a4e5e2caf6ef5b1e9a76127f8de4f4269c3a7
Krovikan <krovikan@gmail.com>
2020-11-08 13:57:19 +0100

    Translated All Zones Legion esES

--- plugins/07_Legion/localization/esES.lua
+++ plugins/07_Legion/localization/esES.lua
@@ -23,8 +23,8 @@ L["infernal_lord_note"] = "Haz click en el Alijo de Infernales y mata {npc:90797
 L["inquisitor_tivos_note"] = "Usa el Portal de la Legión. Está en el piso inferior."
 
 L["disputed_treasure"] = "Tesoro Disputado"
-L["in_academy"] = "Dentro de la Academia."
-L["in_oceanus_cove"] = nil
+L["in_academy"] = "Dentro de la Academia de Nar'thalas."
+L["in_oceanus_cove"] = "Dentro de Cala de Oceanus."
 L["seemingly_unguarded_treasure"] = "Tesoro Aparentemente sin Vigilancia"
 L["seemingly_unguarded_treasure_note"] = "Intenta despojar el tesoro y entonces matar algunas oleadas de {npc:94167}."
 L["treasure_37958"] = "En un nivel inferior del edificio."
@@ -35,82 +35,85 @@ L["treasure_42283"] = "En el segundo piso."
 L["treasure_42287"] = "Bajo el agua."
 L["treasure_42339"] = "No despiertes a los osos."
 
-L["nightwatcher_merayl_note"] = nil
+L["nightwatcher_merayl_note"] = "¡Formaciones!"
 
 -------------------------------------------------------------------------------
 ---------------------------------- DALARAN ------------------------------------
 -------------------------------------------------------------------------------
 
 L["sheddles_chest"] = "Cofre de Sheddle"
-L["shoe_shine_kit_note"] = nil
+L["shoe_shine_kit_note"] = [[
+EU: El cofre aparece en la noche del Sábado al Domingo a medianoche CET (Tiempo del Servidor)
+US: El cofre aparece en la noche del Sábado al Domingo a medianoche PST
+]]
 
-L["sir_galveston_note"] = nil
-L["amalia_note"] = nil
-L["tiffany_nelson_note"] = nil
-L["bohdi_sunwayver_note"] = nil
+L["sir_galveston_note"] = "¿Listo Sir Murkeston? ¡A luchar con gallardía!"
+L["amalia_note"] = "Mucho ladrar y poco morder."
+L["tiffany_nelson_note"] = "¡Adelante!"
+L["Bohdi_sunwayver_note"] = "¡Ha salido el sol! ¡Mascotas fuera!"
 
 -------------------------------------------------------------------------------
 -------------------------------- HIGHMOUNTAIN ---------------------------------
 -------------------------------------------------------------------------------
 
-L["odrogg_note"] = nil
-L["grixis_tinypop_note"] = nil
-L["bredda_tenderhide_note"] = nil
+L["odrogg_note"] = "¿Crees que puedes superar a mis caracoles?"
+L["grixis_tinypop_note"] = "¡Esto va a ser fácil!"
+L["bredda_tenderhide_note"] = "¡Que los más valientes resulten victoriosos!"
 
 -------------------------------------------------------------------------------
 --------------------------------- STORMHEIM -----------------------------------
 -------------------------------------------------------------------------------
 
-L["trapper_jarrun_note"] = nil
-L["robert_craig_note"] = nil
+L["trapper_jarrun_note"] = "Reúne tus defensas, mortal."
+L["robert_craig_note"] = "¡Azúzalas!"
 
 -------------------------------------------------------------------------------
 ---------------------------------- SURAMAR ------------------------------------
 -------------------------------------------------------------------------------
 
-L["varenne_note"] = nil
-L["master_tamer_flummox_note"] = nil
-L["aulier_note"] = nil
+L["varenne_note"] = "¡Tengo que seguir cocinando!"
+L["master_tamer_flummox_note"] = "¡Flummox no necesita mascotas! ¡Flummox se las come AHORA!"
+L["aulier_note"] = "Aprende esta lección con humlidad."
 
 -------------------------------------------------------------------------------
 --------------------------------- VAL'SHARA -----------------------------------
 -------------------------------------------------------------------------------
 
-L["anthydas_note"] = nil
-L["elandris_note"] = nil
-L["gathenak_note"] = nil
-L["gorebeak_note"] = nil
-L["jinikki_note"] = nil
-L["kiranys_note"] = nil
-L["mad_henryk_note"] = nil
-L["skulvrax_note"] = nil
-L["theryssia_note"] = nil
-L["unguarded_thistleleaf_treasure"] = nil
+L["anthydas_note"] = "Despoja el cofre del tesoro del segundo piso del edificio, junto al fregadero."
+L["elandris_note"] = "Cuando, durante una invasión de la legión, la misión del mundo {wq:El valle tenebroso} está activa, no estará {npc:109225}."
+L["gathenak_note"] = "Habla con {npc:112472}."
+L["gorebeak_note"] = "Habla con {npc:92111}."
+L["jinikki_note"] = "Habla con el {npc:93677} y mata {npc:93684} hasta que aparezca {npc:93686}."
+L["kiranys_note"] = "Haz click en la Trampa Arcana vibrante."
+L["mad_henryk_note"] = "Pisa la {npc:109602}."
+L["skulvrax_note"] = "Resucita a {npc:92334} y síguela."
+L["theryssia_note"] = "Lee la placa de la lápida de {npc:94194}."
+L["unguarded_thistleleaf_treasure"] = "Tesoro Hojacardo sin Vigilancia"
 
-L["in_darkpens"] = nil
-L["treasure_38366"] = nil
-L["treasure_38386"] = nil
-L["treasure_38387"] = nil
-L["treasure_38391"] = nil
-L["treasure_38943"] = nil
-L["treasure_39069"] = nil
-L["treasure_39074"] = nil
-L["treasure_39080"] = nil
-L["treasure_39083"] = nil
-L["treasure_39088"] = nil
-L["treasure_39093"] = nil
+L["in_darkpens"] = "Dentro de Celdas Oscuras."
+L["treasure_38366"] = "Bajo las raices del árbol."
+L["treasure_38386"] = "En el balcón del segundo piso."
+L["treasure_38387"] = "En una cueva pequeña debajo de la posada. La entrada está detrás del edificio."
+L["treasure_38391"] = "Oculto detrás de un árbol."
+L["treasure_38943"] = "Sube por las escaleras de la derecha."
+L["treasure_39069"] = "En el balcón del segundo piso."
+L["treasure_39074"] = "Debajo del árbol."
+L["treasure_39080"] = "En el sótano. Necesitas empezar una cadena de misiones. Comienza con {quest:38643} dada por {npc:92688} y seguida por {quest:38644} que la da {npc:92683}."
+L["treasure_39083"] = "Oculto en un árbol."
+L["treasure_39088"] = "Oculto en el lago entre algunas raices."
+L["treasure_39093"] = "En el rio bajo las raices."
 
-L["grumpy_note"] = nil
+L["grumpy_note"] = "Sube las escaleras del edificio en llamas."
 
-L["xorvasc_note"] = nil
-L["durian_strongfruit_note"] = nil
+L["xorvasc_note"] = "Soy tu peor pesadilla."
+L["durian_strongfruit_note"] = "Si es necesario."
 
 -------------------------------------------------------------------------------
 --------------------------------- ACROSS ZONES --------------------------------
 -------------------------------------------------------------------------------
 
-L["in_house"] = nil
-L["in_small_cottage"] = nil
+L["in_house"] = "Dentro de la casa."
+L["in_small_cottage"] = "En una casita pequeña."
 
 L["glimmering_treasure_chest"] = "Cofre del Tesoro Reluciente"
 L["small_treasure_chest"] = "Arqueta Pequeña"
