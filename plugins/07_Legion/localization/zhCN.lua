5535e446f59de0e97102b50ce940dc6d935c4568
Adavak <adavak@adavak.com>
2020-11-09 09:47:05 +0800

    Update zhCN.lua

--- plugins/07_Legion/localization/zhCN.lua
+++ plugins/07_Legion/localization/zhCN.lua
@@ -7,19 +7,16 @@ if not L then return end
 -------------------------------------------------------------------------------
 
 L["arcavellus_note"] = "击杀 {npc:90242} 和 {npc:90243} 直到稀有出现。"
-L["beacher_note"] = "当世界任务 {wq:海拉加尔登陆战：灰色浅滩} 激活时，他不出现。"
+L["beacher_note"] = "当世界任务 {wq:海拉加尔登陆战：灰色浅滩} 激活时不出现。"
 L["brogozog_note"] = "和 {npc:91097} 交谈。"
 L["chief_bitterbrine_note"] = "位于船下层甲板。"
-L["devious_sunrunner_note"] = [[
-使用小洞穴内的魔网传送门。
-别忘了拾取宝箱。
-]]
+L["devious_sunrunner_note"] = "使用小洞穴内的魔网传送门。别忘了拾取宝箱。"
 L["doomlord_kazrok_note"] = "和 {npc:91580} 交谈。"
 L["felwing_note"] = "和 {npc:105913} 交谈并击杀 {npc:105919} 直到稀有出现。"
 L["golza_note"] = "吹响海妖号角并击杀 {npc:90774} 和 {npc:90778} 直到稀有出现。"
 L["infernal_lord_note"] = "点击地狱火宝箱并击杀 {npc:90797} 直到稀有出现。"
 L["inquisitor_tivos_note"] = "使用军团传送门。他在下层。"
-L["shaliman_note"] = nil
+L["shaliman_note"] = "在池塘边。"
 
 L["disputed_treasure"] = "有争议的宝藏"
 L["in_academy"] = "纳萨拉斯学院内。"
@@ -40,13 +37,11 @@ L["nightwatcher_merayl_note"] = "列队！"
 ---------------------------------- DALARAN ------------------------------------
 -------------------------------------------------------------------------------
 
+-- Midnight tz per region: US=>PST, KR=>KST, EU=>CET, TW=>CST, CN=>CST
+local tz = ({'PST', 'KST', 'CET', 'CST', 'CST'})[GetCurrentRegion()]
+
 L["sheddles_chest"] = "西德尔的箱子"
-L["shoe_shine_kit_note"] = [[
-箱子出现在楼上 {npc: 97003} 的位置。
-国服：箱子会在周六晚上到周日早上的午夜零时出现。中国标准时间
-美服：箱子会在周六晚上到周日早上的午夜零时出现。太平洋标准时间
-欧服：箱子会在周六晚上到周日早上的午夜零时出现。欧洲中部时间（服务器时间）
-]]
+L["shoe_shine_kit_note"] = "每个星期六晚上的午夜（"..tz.."）{npc:97003} 将他的箱子丢在地上几小时并离开。"
 L["wand_simulated_life_note"] = "楼上的桌子上。"
 
 L["sir_galveston_note"] = "你准备好了吗，奔波尔斯顿爵士？勇敢地战斗吧！"
@@ -82,14 +77,14 @@ L["aulier_note"] = "我就先教教你什么叫谦虚吧。"
 -------------------------------------------------------------------------------
 
 L["anthydas_note"] = "建筑物二楼的水槽旁边拾取宝箱。"
-L["elandris_note"] = "当军团入侵世界任务 {wq:恐惧之谷} 激活时，他不出现。"
+L["elandris_note"] = "当军团入侵世界任务 {wq:恐惧之谷} 激活时不出现。"
 L["gathenak_note"] = "和 {npc:112472} 交谈。"
 L["gorebeak_note"] = "和 {npc:92111} 交谈。"
 L["jinikki_note"] = "和 {npc:93677} 交谈并击杀 {npc:93684} 直到稀有出现。"
 L["kiranys_note"] = "点击魔力震荡陷阱。"
 L["mad_henryk_note"] = "靠近 {npc:109602}。"
 L["skulvrax_note"] = "复苏 {npc:92334} 并跟随她。"
-L["theryssia_note"] = "阅读墓碑上 {npc:94194} 姓名板。"
+L["theryssia_note"] = "阅读墓碑上 {npc:94194} 的姓名板。"
 L["unguarded_thistleleaf_treasure"] = "无人看守的蓟叶宝藏"
 
 L["in_darkpens"] = "黑暗围栏内。"
@@ -100,7 +95,7 @@ L["treasure_38391"] = "隐藏在树后。"
 L["treasure_38943"] = "上右侧楼梯。"
 L["treasure_39069"] = "二层阳台上。"
 L["treasure_39074"] = "树下。"
-L["treasure_39080"] = "地下室内。需要开始任务线。开始于 {npc:92688} 的 {quest:38643} 任务然后是 {npc:92683} 的 {quest:38644}。"
+L["treasure_39080"] = "地下室内。需要开始任务线开始于 {npc:92688} 的 {quest:38643} 任务然后是 {npc:92683} 的 {quest:38644}。"
 L["treasure_39083"] = "藏在树内。"
 L["treasure_39088"] = "隐藏在一些树根之间的湖中。"
 L["treasure_39093"] = "河里树根下。"
@@ -122,4 +117,4 @@ L["small_treasure_chest"] = "小宝箱"
 L["treasure_chest"] = "宝箱"
 L["treasures_discovered"] = "已发现宝箱"
 
-L["general_pet_tamer_note"] = ns.color.Orange("只在世界任务他出现时才会出现。")
+L["general_pet_tamer_note"] = ns.color.Orange("相对应的世界任务激活时才会出现。")
