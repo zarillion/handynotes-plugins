c82dd478f92ecfe3d289f34d1976ab6e1d9d3529
Zarillion <zarillion@zarillion.com>
2020-11-10 18:13:35 -0600

    Update icc boss note

--- plugins/09_Shadowlands/localization/enUS.lua
+++ plugins/09_Shadowlands/localization/enUS.lua
@@ -404,7 +404,7 @@ L["options_icons_riftstone_desc"] = "Display the locations of chaotic riftstone 
 -------------------------------------------------------------------------------
 
 L["prepatch_intro"] = "Complete the intro quests for the Shadowlands prepatch event to unlock rares in this zone."
-L["icecrown_rares"] = "All rares spawn in a fixed order on a 20 minute interval."
+L["icecrown_rares"] = "All bosses spawn in a fixed order on a 20 minute interval. Each can be looted multiple times a day!"
 L["shared_loot"] = "Shared Loot"
 L["unique_loot"] = "Unique Loot"
 
