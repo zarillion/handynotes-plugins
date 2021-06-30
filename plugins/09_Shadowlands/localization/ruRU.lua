3138e413b13928d950ea398d0f85fccbb913421a
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2021-06-30 06:58:41 +0300

    Update ruRU.lua

--- plugins/09_Shadowlands/localization/ruRU.lua
+++ plugins/09_Shadowlands/localization/ruRU.lua
@@ -581,5 +581,5 @@ L["harrower_key_note"] = "На стене {npc:178311}."
 L["helgarde_supply"] = nil
 L["maw_mad_note"] = "Бегает случайным образом по цитадели вечных мук, всегда появляется здесь."
 L["nilganihmaht_note"] = "Вы должны собрать 5 колец и принести их {npc:179572} в разлом."
-L["options_icons_nilganihmaht"] = "Секреты Нилганихмата"
-L["options_icons_nilganihmaht_desc"] = "Показать расположение частей секретного средства передвижения Нилганихмата."
+L["options_icons_nilganihmaht"] = "{npc:179572}"
+L["options_icons_nilganihmaht_desc"] = "Показать расположение частей секретного средства передвижения {spell: 354354}."
