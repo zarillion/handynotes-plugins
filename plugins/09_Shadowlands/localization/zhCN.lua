203d669a71402d1caf1040f2cec6d0d812967ab5
Adavak <adavak@adavak.com>
2021-07-03 12:23:12 +0800

    Update zhCN.lua

--- plugins/09_Shadowlands/localization/zhCN.lua
+++ plugins/09_Shadowlands/localization/zhCN.lua
@@ -277,8 +277,8 @@ L["rift_portal_note"] = [[
 
 |cFFFF8C00并非所有裂隙传送门在任何时间都处于激活状态。|r
 ]]
-L["rift_rare_only_note"] = nil
-L["rift_rare_exit_note"] = nil
+L["rift_rare_only_note"] = "此稀有只在 |cFFFFFF00裂隙|r 位面内可以见到和击杀。"
+L["rift_rare_exit_note"] = "此稀有将离开 |cFFFFFF00裂隙|r 位面一次与裂隙内互动。"
 L["options_icons_rift_portal"] = "{npc:179595}"
 L["options_icons_rift_portal_desc"] = "显示 {npc:179595} 的位置可以进入 |cFFFFFF00裂隙|r。"
 
