9fa78a3e5d5cfed836aa0e0b5b2125c119990085
Krovikan <krovikan@gmail.com>
2021-08-24 12:33:32 +0200

    Translated all Warlords of Draenor esES

--- plugins/06_WarlordsOfDraenor/localization/esES.lua
+++ plugins/06_WarlordsOfDraenor/localization/esES.lua
@@ -9,7 +9,7 @@ if not L then return end
 -------------------------------------------------------------------------------
 
 L["edge_of_reality"] = "Acantilado de la Realidad"
-L["treasures_discovered"] = nil
+L["treasures_discovered"] = "tesoros descubiertos"
 
 -------------------------------------------------------------------------------
 --------------------------------- TANAAN JUNGLE -------------------------------
@@ -19,77 +19,88 @@ L["deathtalon_note"] = ns.color.Red("Señor de las Sombras Iskar grita: ¡Tras e
 L["doomroller_note"] = ns.color.Red("Maestra de asedio Mar'tak grita: ¡Ja ja! ¡Pisotead sus cuerpos!")
 L["terrorfist_note"] = ns.color.Red("Frogan grita: ¡Un gronito enorme va hacia el Refugio Rangari! ¡Vamos a necesitar ayuda!")
 L["vengeance_note"] = ns.color.Red("Tyrana Velhari grita: ¡Los insectos merecen ser aplastados!")
-L["iron_armada_note"] = nil
-L["commander_kraggoth_note"] = nil
-L["grannok_note"] = nil
-L["szirek_the_twisted_note"] = nil
-L["the_iron_houndmaster_note"] = nil
-L["belgork_thromma_note"] = nil
-L["driss_vile_note"] = nil
-L["overlord_magruth_note"] = nil
-L["mistress_thavra_note"] = nil
-L["bloodhunter_zulk_note"] = nil
-L["dorg_the_bloody_note"] = nil
-L["grand_warlock_netherkurse_note"] = nil
-L["ceraxas_note"] = nil
-L["commander_orgmok_note"] = nil
-L["rendrak_note"] = nil
-L["akrrilo_note"] = nil
-L["rendarr_note"] = nil
-L["eyepiercer_note"] = nil
-L["the_night_haunter_note"] = nil
-L["xemirkol_note"] = nil
-L["discarded_helm_treasure"] = nil
-L["weathered_axe_treasure"] = nil
-L["axe_of_the_weeping_wolf_treasure"] = nil
-L["sacrificial_blade_treasure"] = nil
-L["crystallized_essence_of_the_elements_treasure"] = nil
-L["snake_charmers_flute_treasure"] = nil
-L["lodged_hunting_spear_treasure"] = nil
-L["looted_mystical_staff_treasure"] = nil
-L["the_blade_of_kranak_treasure"] = nil
-L["forgotten_champions_blade_treasure"] = nil
-L["rune_etched_femur_treasure"] = nil
-L["book_of_zyzzix_treasure"] = nil
-L["the_commanders_shield_treasure"] = nil
-L["scouts_belongings_treasure"] = nil
-L["polished_crystal_treasure"] = nil
-L["strange_sapphire_treasure"] = nil
-L["censer_of_torment_treasure"] = nil
-L["overgrown_relic_treasure"] = nil
-L["jewel_of_hellfire_treasure"] = nil
-L["skull_of_the_mad_chief_treasure"] = nil
-L["jeweled_arakkoa_effigy_treasure"] = nil
-L["tome_of_secrets_treasure"] = nil
-L["the_perfect_blossom_treasure"] = nil
-L["brazier_of_awakening_treasure"] = nil
-L["dazzling_rod_treasure"] = nil
-L["crystallized_fel_spike_treasure"] = nil
-L["fel_drenched_satchel_treasure"] = nil
-L["the_eye_of_grannok_treasure"] = nil
-L["borrowed_enchanted_spyglass_treasure"] = nil
-L["bleeding_hollow_mushroom_stash_treasure"] = nil
-L["mysterious_corrupted_obelist_treasure"] = nil
-L["spoils_of_war_treasure"] = nil
-L["stolen_captains_chest_treasure"] = nil
-L["bleeding_hollow_warchest_treasure"] = nil
-L["looted_bleeding_hollow_treasure_treasure"] = nil
-L["partially_mined_apexis_crystal_treasure"] = nil
-L["pale_removal_equipment_treasure"] = nil
-L["stashed_iron_sea_booty_treasure"] = nil
-L["ironbeards_treasure_treasure"] = nil
-L["forgotten_sack_treasure"] = nil
-L["blackfang_island_cache_treasure"] = nil
-L["fel_tainted_apexis_formation_treasure"] = nil
-L["jewel_of_the_fallen_star_treasure"] = nil
-L["forgotten_shard_of_the_cipher_treasure"] = nil
-L["strange_fruit_treasure"] = nil
-L["stashed_bleeding_hollow_loot_treasure"] = nil
-L["forgotten_iron_horde_supplies_treasure"] = nil
-L["bejeweled_egg_treasure"] = nil
-L["dead_mans_chest_treasure"] = nil
-L["the_commanders_shield_note"] = nil
-L["the_eye_of_grannok_note"] = nil
-L["tower_chest_note"] = nil
-L["spoils_of_war_note"] = nil
-L["strange_fruit_note"] = nil
+L["iron_armada_note"] = "Este juguete también se puede comprar en la AH y hace falta para {achievement:10353}."
+L["commander_kraggoth_note"] = "Encima de la torre al noreste."
+L["grannok_note"] = "Encima de la torre al sureste."
+L["szirek_the_twisted_note"] = "Captura el Puesto Fortificado del Este para invocar a {npc:93001}."
+L["the_iron_houndmaster_note"] = "Captura el Puesto Fortificado del Oeste para invocar al {npc:92977}."
+L["belgork_thromma_note"] = "Esta cueva tiene 2 entradas."
+L["driss_vile_note"] = "Encima de la torre."
+L["overlord_magruth_note"] = "Mata los orcos de alrededor para hacerlo aparecer."
+L["mistress_thavra_note"] = "En el piso superior de una cueva."
+L["bloodhunter_zulk_note"] = "Puede aparecer en varios sitios."
+L["dorg_the_bloody_note"] = "Mata al {npc:89706} y a los otros enemigos en el sitio donde aparece."
+L["grand_warlock_netherkurse_note"] = "Mata a enemigos alrededor del lugar de aparición."
+L["ceraxas_note"] = "Aparece el {npc:90426} con la misión {quest:38428} para la mascota tras matar al {npc:90434}."
+L["commander_orgmok_note"] = "Trota alrededor subido en {npc:89676}."
+L["rendrak_note"] = "Recoge 10 {item:124045} de los {npc:89788} alrededor del pantano. Combínalas para invocar a {npc:92627}."
+L["akrrilo_note"] = "Compra un {item:124093} a {npc:92805} y úsalo en la Arena de los Colmillo Negro en Fang'rila."
+L["rendarr_note"] = "Compra un {item:124094} a {npc:92805} y úsalo en la Arena de los Colmillo Negro en Fang'rila."
+L["eyepiercer_note"] = "Compra un {item:124095} a {npc:92805} y úsalo en la Arena de los Colmillo Negro en Fang'rila."
+L["the_night_haunter_note"] = [[
+Consigue 10 acumulaciones del debuff {spell:183612}.
+
+El debuff se consigue usando {npc:92651} o siendo encontrando por {npc:92645} (100% de probabilidades).
+]]
+L["xemirkol_note"] = [[
+Compra un {item:128502} o un {item:128503} a {npc:95424} y úsalo en el lugar de aparición para ser teleportado a {npc:96235}.
+
+Los cristales te teleportan a un raro aleatorio cercano, así que lo mejor es matar a {npc:92887} y usar un {item:128502}.
+
+{npc:96235} tiene un contador de reapación largo (alrededor de 1 día) y la mejor forma de conseguirlo es tras el reinicio de servidores o saltando de reino.
+]]
+
+L["discarded_helm_treasure"] = "Yelmo Desechado"
+L["weathered_axe_treasure"] = "Hacha Deteriorada"
+L["axe_of_the_weeping_wolf_treasure"] = "Hacha del Lobo Sollozante"
+L["sacrificial_blade_treasure"] = "Hoja de Sacrificio"
+L["crystallized_essence_of_the_elements_treasure"] = "Esencia de los Elementos Cristalizada"
+L["snake_charmers_flute_treasure"] = "Flauta de Encantador de Serpientes"
+L["lodged_hunting_spear_treasure"] = "Lanza de Caza Atascada"
+L["looted_mystical_staff_treasure"] = "Bastón Místico Despojado"
+L["the_blade_of_kranak_treasure"] = "El Filo de Kra'nak"
+L["forgotten_champions_blade_treasure"] = "Filo de Campeón Olvidado"
+L["rune_etched_femur_treasure"] = "Fémur con Runas Grabadas"
+L["book_of_zyzzix_treasure"] = "Libro de Zyzzix"
+L["the_commanders_shield_treasure"] = "Escudo del Comandante"
+L["scouts_belongings_treasure"] = "Pertenencias de Explorador"
+L["polished_crystal_treasure"] = "Cristal Pulido"
+L["strange_sapphire_treasure"] = "Zafiro Extraño"
+L["censer_of_torment_treasure"] = "Incensario de Tormento"
+L["overgrown_relic_treasure"] = "Reliquia Desmesurada"
+L["jewel_of_hellfire_treasure"] = "Joya de Fuego Infernal"
+L["skull_of_the_mad_chief_treasure"] = "Cráneo del Jefe Loco"
+L["jeweled_arakkoa_effigy_treasure"] = "Efigie Arakkoa con Joyas"
+L["tome_of_secrets_treasure"] = "Escrito de Secretos"
+L["the_perfect_blossom_treasure"] = "La Flor Perfecta"
+L["brazier_of_awakening_treasure"] = "Blandón de Despertar"
+L["dazzling_rod_treasure"] = "Vara Deslumbrante"
+L["crystallized_fel_spike_treasure"] = "Púa Vil Cristalizada"
+L["fel_drenched_satchel_treasure"] = "Cartera Envilecida"
+L["the_eye_of_grannok_treasure"] = "El Ojo de Grannok"
+L["borrowed_enchanted_spyglass_treasure"] = "Catalejo Encantado 'Prestado'"
+L["bleeding_hollow_mushroom_stash_treasure"] = "Alijo de Champiñones de los Foso Sangrante"
+L["mysterious_corrupted_obelist_treasure"] = "Obelisco Corrupto Misterioso"
+L["spoils_of_war_treasure"] = "Botín de Guerra"
+L["stolen_captains_chest_treasure"] = "Cofre del Capitán Robado"
+L["bleeding_hollow_warchest_treasure"] = "Cofre de Guerra de los Foso Sangrante"
+L["looted_bleeding_hollow_treasure_treasure"] = "Tesoro de los Foso Sangrante Despojado"
+L["partially_mined_apexis_crystal_treasure"] = "Cristal Apexis Parcialmente Extraído"
+L["pale_removal_equipment_treasure"] = "Equipo de Eliminación de Pálidos"
+L["stashed_iron_sea_booty_treasure"] = "Botín del Mar de Hierro Oculto"
+L["ironbeards_treasure_treasure"] = "Tesoro de Barbaférrea"
+L["forgotten_sack_treasure"] = "Saco Olvidado"
+L["blackfang_island_cache_treasure"] = "Alijo de la Isla Colmillo Negro"
+L["fel_tainted_apexis_formation_treasure"] = "Formación Apexis Contaminada de Energía Vil"
+L["jewel_of_the_fallen_star_treasure"] = "Joya de la Estrella Caída"
+L["forgotten_shard_of_the_cipher_treasure"] = "Fragmento de la Clave Olvidado"
+L["strange_fruit_treasure"] = "Fruta Extraña"
+L["stashed_bleeding_hollow_loot_treasure"] = "Botín de los Foso Sangrante Oculto"
+L["forgotten_iron_horde_supplies_treasure"] = "Suministros de la Horda de Hierro Olvidados"
+L["bejeweled_egg_treasure"] = "Huevo con Joyas"
+L["dead_mans_chest_treasure"] = "Cofre de un Hombre Muerto"
+L["the_commanders_shield_note"] = "Dentro del edificio."
+L["the_eye_of_grannok_note"] = "En el segundo piso de la torre cerca de las escaleras."
+L["tower_chest_note"] = "Encima de una torre."
+L["spoils_of_war_note"] = "Dentro de la cabaña."
+L["strange_fruit_note"] = "La {item:127396} tras incubar 14 days se convierte en {item:127394}."
