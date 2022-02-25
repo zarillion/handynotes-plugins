09fac505f288a01c28070242a9ef09c5e682fb5b
Krovikan <krovikan@gmail.com>
2022-02-25 11:36:26 +0100

    Updated to v41 Shadowlands esES

--- plugins/09_Shadowlands/localization/esES.lua
+++ plugins/09_Shadowlands/localization/esES.lua
@@ -683,7 +683,7 @@ L['feasting_note'] = 'A veces rodeará la zona antes de volver a este sitio.'
 L['furidian_note'] = 'Activa tres |cFFFFFD00Llave Potenciada|r de la zona, y desbloquea la |cFFFFFD00Cámara Sospechosamente Enfadada|r.'
 L['garudeon_note'] = 'Recoge varios {npc:183562} de los alrededores y usa {spell:362655} para alimentar a los {npc:183554}. Cuando los tres estén alimentados, se activará {npc:180924}.'
 L['gluttonous_overgrowth_note'] = 'Rompe todas las {npc:184048} cercanas para activar al {npc:178778}.'
-L['helmix_emote'] = nil
+L['helmix_emote'] = '|cFFFF8C00¡El suelo tiembla... algo se mueve bajo el suelo!|r'
 L['hirukon_note'] = [[
 Para atraer a {npc:180978} a la superfície, tiene que crearse un {item:187923}.
 
@@ -699,10 +699,10 @@ Para atraer a {npc:180978} a la superfície, tiene que crearse un {item:187923}.
 
 6. Usa el {item:187923} que te permitirá ver los |cFFFFFD00Banco de Aurélido|r junto a {npc:180978} durante 15min. Pesca dentro para invocar a {npc:180978} a la superfície.
 ]]
-L['orixal_note'] = nil
+L['orixal_note'] = 'Comparte punto de aparición con el {npc:185487} que patrulla la zona.'
 L['protector_first_ones_note'] = 'Requiere dos personas. Lleva las runas correspondientes a cada consola (están en los alrededores) para abrir la barrera.'
 L['the_engulfer_note'] = 'Defiende a {npc:183505} hasta que {npc:183516} salte de la grieta.'
-L['zatojin_note'] = 'Atrae a los {npc:183721} cercanos y deja que te pongan 20 acumulaciones de {spell:362976} hasta que estés {spell:362983}. Tienes que estar sobre los cuerpos de los {npc:183774} cuando seas paralizado para que {npc:183764} se fije en ti.'
+L['zatojin_note'] = 'Atrae a los {npc:183721} cercanos y deja que te pongan 20 acumulaciones de {spell:362976} hasta que estés {spell:362983}. Tienes que estar dentro del anillo de cuerpos de los {npc:183774} cuando seas paralizado para que {npc:183764} se fije en ti.'
 
 L['architects_reserve_note'] = 'Completa la cadena de misiones {quest:64829} que da {npc:180630} en Gracia del Peregrino. Al día seguiente, completa la cadena de misiones {quest:65426} que da {npc:181273} para desbloquear el tesoro.'
 L['bushel_of_produce_note'] = 'Mata al {npc:182368} al norte mientras tenga el buff {spell:360945} para ganártelo tú. Mata más {npc:182368} (no necesitan tener el buff) hasta que tengas 5 acumulaciones. Ya puedes romper la puerta que da al tesoro.'
@@ -712,12 +712,12 @@ L['drowned_broker_supplies_note'] = 'Dile a {npc:181059} que coja el control de 
 L['forgotten_protovault_note'] = 'Sólo está disponible durante la misión del mundo {wq:A salto de rana}.'
 L['grateful_boon_note'] = 'Es dificul de alcanzar sin volar; usa {spell:300728} o {spell:111771} para subir la montaña. Tranquiliza a las 12 criaturas de la zona y {npc:185293} hará aparecer el tesoro.'
 L['library_vault_note'] = 'Haz click en las tablillas de la Gruta del Léxico hasta que encuentres el orden correcto y ganes el buff {spell:362062} para abrir la cámara.'
-L['mistaken_ovoid_note'] = 'Dentro de la caverna Cataratas Dimensionales. Has de recoger {item:190239} x5, y llevarlos a {npc:185280}. Se moverá un poco para dejarte coger el tesoro.'
+L['mistaken_ovoid_note'] = 'Dentro de la caverna Cataratas Dimensionales. Has de recoger {item:190239} x5, y llevarlos a la {npc:185280}.'
 L['ripened_protopear_note'] = 'Inicia la cadena de misiones {quest:64641} para ganar accesso a la Fundición Floreciente. Recoge 5 {spell:367180} (nubes verdes) dentro and y llévalas a la {npc:185416} para polinizarla.'
 L['sphere_treasure_note'] = 'Salta las esferas con cuidado hasta llegar al tesoro.'
 L['submerged_chest_note'] = 'Absorbe el |cFFFFFD00Orbe de Poder Peligroso|r al sur. Ahora interactúa con la |cFFFFFD00Bomba Olvidada|r junto al tesoro.'
 L['symphonic_vault_note'] = 'Examina al {npc:183998} para escuchar una secuencia de cuatro sonidos. Cada {npc:183950} de la sala reproduce un sonido. Interactúa con ellas en el orden adecuado para desbloquear el tesoro.'
-L['syntactic_vault_note'] = nil
+L['syntactic_vault_note'] = 'Para desbloquear el tesoro, has de ganar 6 acumulaciones de {spell:367499} interactuando con las |cFFFFFD00Sílabas Rúnicas|r en la zona del Sepulcro de los Primeros.'
 L['template_archive_note'] = 'Empuja el {npc:183339} de la sala dentro del {npc:183337} que bloquea el paso al tesoro.'
 L['undulating_foliage_note'] = [[
 Pulsa los cuatro {npc:185390} para activar el teleportador del centro de la sala de los Distritos Catalizadores.
