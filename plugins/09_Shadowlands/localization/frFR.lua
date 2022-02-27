25524f2b3e6220aeb248be3a99df30a724c7a3d8
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-02-27 10:18:51 +0100

    Update frFR.lua - misaligned_enforcer_note

--- plugins/09_Shadowlands/localization/frFR.lua
+++ plugins/09_Shadowlands/localization/frFR.lua
@@ -787,7 +787,7 @@ L['echoed_jiro_note'] = 'Dépensez l\'énergie de {npc:181059} pour obtenir des 
 
 L['bygone_elemental_note'] = 'Un {npc:181221} peut apparaître à la place de n\'importe quel {npc:179007} dans la zone.'
 L['gaiagantic_note'] = 'Apparaît uniquement lorsque {npc:177958} propose la quête journalière {quest:64785}.'
-L['misaligned_enforcer_note'] = 'Patrouille la zone et disparaît à la fin de son itinéraire.'
+L['misaligned_enforcer_note'] = 'Apparaît ici. Patrouille la zone et disparaît à la fin de son itinéraire.'
 L['overcharged_vespoid_note'] = 'Peut apparaître dans n\'importe quel essaim de la zone.'
 L['runethief_xylora_note'] = 'Apparaît camouflé à la Grâce du Pèlerin.'
 
