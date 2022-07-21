eae1638b20bbbfbed794e0283355e3ff0a96dacc
Wyldclaw <wyldclawthornbringer@gmail.com>
2022-07-20 20:27:37 -0700

    BC rares and Netherwing Eggs

--- plugins/02_BurningCrusade/zones/nagrand.lua
+++ plugins/02_BurningCrusade/zones/nagrand.lua
@@ -1,10 +1,45 @@
 -------------------------------------------------------------------------------
 ---------------------------------- NAMESPACE ----------------------------------
 -------------------------------------------------------------------------------
--- local ADDON_NAME, ns = ...
--- local Map = ns.Map
+local ADDON_NAME, ns = ...
+local Map = ns.Map
+local L = ns.locale
+
+local Rare = ns.node.Rare
+
+local Achievement = ns.reward.Achievement
+local Transmog = ns.reward.Transmog
+
+local POI = ns.poi.POI
+
 -------------------------------------------------------------------------------
-------------------------------------- MAP -------------------------------------
+
+local map = Map({id = 107, settings = true})
+
 -------------------------------------------------------------------------------
--- local map = Map({ id=107, settings=true })
--- local nodes = map.nodes
+------------------------------------ RARES ------------------------------------
+-------------------------------------------------------------------------------
+
+map.nodes[35404720] = Rare({
+    id = 17144,
+    rewards = {
+        Achievement({id = 1312, criteria = 4523}), -- Bloody Rare
+        Transmog({item = 31192, slot = L['cloth']}), -- Crocolisk Hide Bindings
+        Transmog({item = 31191, slot = L['leather']}), -- Crocolisk Hide Bracers
+        Transmog({item = 31189, slot = L['mail']}), -- Crocolisk Scale Wristguards
+        Transmog({item = 31188, slot = L['plate']}) -- Crocolisk Scale Vambraces
+    },
+    pois = {POI({42804780, 42204000, 58802880, 76607880})}
+}) -- Goretooth
+
+map.nodes[35806560] = Rare({
+    id = 18683,
+    rewards = {
+        Achievement({id = 1312, criteria = 4511}), -- Bloody Rare
+        Transmog({item = 31199, slot = L['cloth']}), -- Voidweave Cilice
+        Transmog({item = 31198, slot = L['leather']}), -- Voidhide Cord
+        Transmog({item = 31197, slot = L['mail']}), -- Voidscale Belt
+        Transmog({item = 31195, slot = L['plate']}) -- Voidplate Girdle
+    },
+    pois = {POI({34607680, 32607160, 39807100})}
+}) -- Voidhunter Yar
--- plugins/02_BurningCrusade/zones/shadowmoon_valley.lua
+++ plugins/02_BurningCrusade/zones/shadowmoon_valley.lua
@@ -1,10 +1,188 @@
 -------------------------------------------------------------------------------
 ---------------------------------- NAMESPACE ----------------------------------
 -------------------------------------------------------------------------------
--- local ADDON_NAME, ns = ...
--- local Map = ns.Map
+local ADDON_NAME, ns = ...
+local Map = ns.Map
+local L = ns.locale
+
+local Node = ns.node.Node
+local Rare = ns.node.Rare
+
+local Achievement = ns.reward.Achievement
+local Transmog = ns.reward.Transmog
+
+local POI = ns.poi.POI
+
 -------------------------------------------------------------------------------
-------------------------------------- MAP -------------------------------------
+
+local map = Map({id = 104, settings = true})
+
 -------------------------------------------------------------------------------
--- local map = Map({ id=104, settings=true })
--- local nodes = map.nodes
+------------------------------------ RARES ------------------------------------
+-------------------------------------------------------------------------------
+
+map.nodes[30605860] = Rare({
+    id = 18695,
+    rewards = {
+        Achievement({id = 1312, criteria = 4504}), -- Bloody Rare
+        Transmog({item = 31225, slot = L['cloth']}), -- Illidari Bindings
+        Transmog({item = 31224, slot = L['leather']}), -- Illidari Bracers
+        Transmog({item = 31223, slot = L['mail']}), -- Illidari Wristguards
+        Transmog({item = 31221, slot = L['plate']}) -- Illidari Vambraces
+    },
+    pois = {
+        POI({
+            29805220, 28204800, 46606600, 45606800, 48006840, 46807080,
+            71606320, 68006140, 59603720, 56203860, 57003420, 45603120, 46802660
+        })
+    }
+}) -- Ambassador Jerrikar
+
+map.nodes[45605220] = Rare({
+    id = 18694,
+    rewards = {
+        Achievement({id = 1312, criteria = 4508}), -- Bloody Rare
+        Transmog({item = 31220, slot = L['cloth']}), -- Amethyst Beholder Eye
+        Transmog({item = 31219, slot = L['leather']}), -- Emerald Beholder Eye
+        Transmog({item = 31218, slot = L['mail']}), -- Fiery Beholder Eye
+        Transmog({item = 31217, slot = L['plate']}) -- Crimson Beholder Eye
+    },
+    pois = {
+        POI({
+            38604240, 36404540, 59202200, 66802180, 67402860, 73802920,
+            55807360, 58207360, 62606260, 71606660, 64606800
+        })
+    }
+}) -- Collidus the Warp-Watcher
+
+map.nodes[45601260] = Rare({
+    id = 18696,
+    rewards = {
+        Achievement({id = 1312, criteria = 4515}), -- Bloody Rare
+        Transmog({item = 31216, slot = L['cloth']}), -- Abyssal Cloth Footwraps
+        Transmog({item = 31215, slot = L['leather']}), -- Abyssal Leather Treads
+        Transmog({item = 31214, slot = L['mail']}), -- Abyssal Mail Greaves
+        Transmog({item = 31213, slot = L['plate']}) -- Abyssal Plate Sabatons
+    },
+    pois = {POI({42004000, 31204560, 59604660, 42006880})}
+}) -- Kraator
+
+-------------------------------------------------------------------------------
+------------------------------- NETHERWING EGGS -------------------------------
+-------------------------------------------------------------------------------
+
+local NetherwingEgg = Node({
+    label = L['netherwing_egg'],
+    icon = 'peg_gn',
+    group = ns.groups.NETHERWING_EGGS
+})
+
+map.nodes[59847826] = NetherwingEgg
+map.nodes[60218708] = NetherwingEgg
+map.nodes[62308947] = NetherwingEgg
+map.nodes[62807478] = NetherwingEgg
+map.nodes[63448289] = NetherwingEgg
+map.nodes[63948604] = NetherwingEgg
+map.nodes[63968740] = NetherwingEgg
+map.nodes[64098403] = NetherwingEgg
+map.nodes[64198674] = NetherwingEgg
+map.nodes[64588643] = NetherwingEgg
+map.nodes[64608732] = NetherwingEgg
+map.nodes[64668312] = NetherwingEgg
+map.nodes[64758520] = NetherwingEgg
+map.nodes[64928377] = NetherwingEgg
+map.nodes[64929095] = NetherwingEgg
+map.nodes[65068213] = NetherwingEgg
+map.nodes[65168586] = NetherwingEgg
+map.nodes[65178491] = NetherwingEgg
+map.nodes[65468866] = NetherwingEgg
+map.nodes[65498475] = NetherwingEgg
+map.nodes[65638773] = NetherwingEgg
+map.nodes[65688419] = NetherwingEgg
+map.nodes[65689408] = NetherwingEgg
+map.nodes[65968055] = NetherwingEgg
+map.nodes[66108388] = NetherwingEgg
+map.nodes[66288222] = NetherwingEgg
+map.nodes[66928201] = NetherwingEgg
+map.nodes[66929153] = NetherwingEgg
+map.nodes[67157962] = NetherwingEgg
+map.nodes[67226134] = NetherwingEgg
+map.nodes[67246236] = NetherwingEgg
+map.nodes[67278623] = NetherwingEgg
+map.nodes[67878737] = NetherwingEgg
+map.nodes[67888544] = NetherwingEgg
+map.nodes[67958247] = NetherwingEgg
+map.nodes[68055974] = NetherwingEgg
+map.nodes[68138182] = NetherwingEgg
+map.nodes[68149467] = NetherwingEgg
+map.nodes[68295981] = NetherwingEgg
+map.nodes[68317932] = NetherwingEgg
+map.nodes[68536115] = NetherwingEgg
+map.nodes[68628290] = NetherwingEgg
+map.nodes[68688362] = NetherwingEgg
+map.nodes[68838579] = NetherwingEgg
+map.nodes[68876249] = NetherwingEgg
+map.nodes[68898358] = NetherwingEgg
+map.nodes[69068137] = NetherwingEgg
+map.nodes[69108822] = NetherwingEgg
+map.nodes[69268432] = NetherwingEgg
+map.nodes[69338656] = NetherwingEgg
+map.nodes[69386377] = NetherwingEgg
+map.nodes[69468006] = NetherwingEgg
+map.nodes[69625854] = NetherwingEgg
+map.nodes[69678434] = NetherwingEgg
+map.nodes[69858208] = NetherwingEgg
+map.nodes[70076201] = NetherwingEgg
+map.nodes[70086030] = NetherwingEgg
+map.nodes[70218812] = NetherwingEgg
+map.nodes[70278392] = NetherwingEgg
+map.nodes[70408697] = NetherwingEgg
+map.nodes[70518397] = NetherwingEgg
+map.nodes[70718568] = NetherwingEgg
+map.nodes[70888218] = NetherwingEgg
+map.nodes[70916264] = NetherwingEgg
+map.nodes[70948145] = NetherwingEgg
+map.nodes[70968911] = NetherwingEgg
+map.nodes[71058081] = NetherwingEgg
+map.nodes[71148469] = NetherwingEgg
+map.nodes[71376074] = NetherwingEgg
+map.nodes[71458645] = NetherwingEgg
+map.nodes[71558410] = NetherwingEgg
+map.nodes[71588129] = NetherwingEgg
+map.nodes[72007470] = NetherwingEgg
+map.nodes[72248790] = NetherwingEgg
+map.nodes[72278638] = NetherwingEgg
+map.nodes[72508373] = NetherwingEgg
+map.nodes[72598373] = NetherwingEgg
+map.nodes[72648886] = NetherwingEgg
+map.nodes[72829032] = NetherwingEgg
+map.nodes[72898217] = NetherwingEgg
+map.nodes[73218428] = NetherwingEgg
+map.nodes[73308573] = NetherwingEgg
+map.nodes[73358715] = NetherwingEgg
+map.nodes[73429035] = NetherwingEgg
+map.nodes[73588511] = NetherwingEgg
+map.nodes[73808599] = NetherwingEgg
+map.nodes[73978306] = NetherwingEgg
+map.nodes[74068587] = NetherwingEgg
+map.nodes[74208239] = NetherwingEgg
+map.nodes[74288554] = NetherwingEgg
+map.nodes[74318974] = NetherwingEgg
+map.nodes[74348729] = NetherwingEgg
+map.nodes[74588840] = NetherwingEgg
+map.nodes[74618466] = NetherwingEgg
+map.nodes[75198645] = NetherwingEgg
+map.nodes[75228248] = NetherwingEgg
+map.nodes[75658606] = NetherwingEgg
+map.nodes[75769165] = NetherwingEgg
+map.nodes[76068134] = NetherwingEgg
+map.nodes[76408566] = NetherwingEgg
+map.nodes[76558335] = NetherwingEgg
+map.nodes[77269548] = NetherwingEgg
+map.nodes[77368586] = NetherwingEgg
+map.nodes[77619255] = NetherwingEgg
+map.nodes[78108112] = NetherwingEgg
+map.nodes[78828644] = NetherwingEgg
+map.nodes[78867961] = NetherwingEgg
+map.nodes[78888334] = NetherwingEgg
+map.nodes[79588800] = NetherwingEgg
