162e95ac2a26c9ec3d08ea56949a15218b633df2
Adavak <adavak@adavak.com>
2022-09-11 09:20:26 +0800

    Update enUS.lua

--- plugins/03_WrathOfTheLichKing/localization/enUS.lua
+++ plugins/03_WrathOfTheLichKing/localization/enUS.lua
@@ -6,10 +6,10 @@ if not L then return end
 L['area_spawn'] = 'Spawns in the surrounding area.'
 
 L['squirrels_note'] = 'You must use the emote /love on critters not battle pets'
-L['options_icons_squirrels'] = 'To All The Squirrels ...'
-L['options_icons_squirrels_desc'] = 'Critter locations for "To All The Squirrels..." achievements'
+L['options_icons_squirrels'] = '{achievement:2557}'
+L['options_icons_squirrels_desc'] = 'Critter locations for {achievement:2557} achievements'
 
 L['achievement_friend_or_fowl_desc'] = 'Slay 15 turkeys in 3 minutes'
-L['note_devouring_maggot'] = 'Deep down in the Utgarde Catacombs'
-L['dalaran_sewers'] = 'In the Dalaran Sewers'
-L['in_nexus'] = 'Inside The Nexus at the Hall of Stasis'
+L['note_devouring_maggot'] = 'Deep down in the |cFFFFFD00Utgarde Catacombs|r'
+L['dalaran_sewers'] = 'In the |cFFFFFD00Dalaran Sewers|r'
+L['in_nexus'] = 'Inside |cFFFFFD00The Nexus|r at the |cFFFFFD00Hall of Stasis|r'
