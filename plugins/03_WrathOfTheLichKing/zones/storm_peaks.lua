39b6780cba1c205391457fd2bdf9ab714f9bb983
Dathwada <dathwada@gmail.com>
2022-09-11 11:40:48 +0200

    Fix formatting issues

--- plugins/03_WrathOfTheLichKing/zones/storm_peaks.lua
+++ plugins/03_WrathOfTheLichKing/zones/storm_peaks.lua
@@ -1,7 +1,6 @@
 -------------------------------------------------------------------------------
 ---------------------------------- NAMESPACE ----------------------------------
 -------------------------------------------------------------------------------
-
 local ADDON_NAME, ns = ...
 local Map = ns.Map
 local L = ns.locale
@@ -18,14 +17,14 @@ local POI = ns.poi.POI
 ------------------------------------- MAP -------------------------------------
 -------------------------------------------------------------------------------
 
-local map = Map({ id=120, settings=true })
+local map = Map({id = 120, settings = true})
 
 local tlpd = Rare({
     id = 32491,
     label = '{npc:32491}, {npc:32630}',
-    fgroup='tlpd',
+    fgroup = 'tlpd',
     rewards = {
-        Mount({item=44168,id=265}),
+        Mount({item = 44168, id = 265}),
         Achievement({id = 2257, criteria = 8120}), -- Frostbitten
         Transmog({item = 44732, slot = L['leather']}) -- Azure Dragonleather Helm
     }
@@ -40,11 +39,7 @@ map.nodes[41403900] = Rare({
     id = 32500,
     rewards = {
         Achievement({id = 2257, criteria = 8121}), -- Frostbitten
-        Transmog({item = 44708, slot = L['2h_mace']}), -- Dirkee's Superstructure
+        Transmog({item = 44708, slot = L['2h_mace']}) -- Dirkee's Superstructure
     },
-    pois = {
-        POI({
-            41604060, 41005160, 37805860, 68204760, 41604290, 64904100
-        })
-    }
+    pois = {POI({41604060, 41005160, 37805860, 68204760, 41604290, 64904100})}
 }) -- Dirkee
