1284d1166678ee2e94dfc46e23507827d7be39a1
Adavak <adavak@adavak.com>
2022-09-11 09:20:48 +0800

    Update zhCN.lua

--- plugins/03_WrathOfTheLichKing/localization/zhCN.lua
+++ plugins/03_WrathOfTheLichKing/localization/zhCN.lua
@@ -5,10 +5,11 @@ if not L then return end
 
 L['area_spawn'] = '在周围地区刷新。'
 
-L['squirrels_note'] = nil
+L['squirrels_note'] = '使用 /love 给非战斗宠物的小动物。'
 L['options_icons_squirrels'] = '{achievement:2557}'
-L['options_icons_squirrels_desc'] = nil
+L['options_icons_squirrels_desc'] = '显示 {achievement:2557} 成就中小动物的位置。'
 
-L['achievement_friend_or_fowl_desc'] = nil
-L['note_devouring_maggot'] = nil
-L['dalaran_sewers'] = nil
+L['achievement_friend_or_fowl_desc'] = '在3分钟内击杀15只火鸡。'
+L['note_devouring_maggot'] = '|cFFFFFD00乌特加德墓穴|r 下面深处。'
+L['dalaran_sewers'] = '|cFFFFFD00达拉然下水道|r 内。'
+L['in_nexus'] = '|cFFFFFD00魔枢|r 内的 |cFFFFFD00停滞大厅|r。'
