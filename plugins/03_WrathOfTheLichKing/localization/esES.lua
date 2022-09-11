7e31414592a11ce3b51682249626409bd014b918
Krovikan <krovikan@gmail.com>
2022-09-11 12:08:18 +0200

    Updated WotLK esES

--- plugins/03_WrathOfTheLichKing/localization/esES.lua
+++ plugins/03_WrathOfTheLichKing/localization/esES.lua
@@ -5,11 +5,11 @@ if not L then return end
 
 L['area_spawn'] = 'Aparece en los alrededores.'
 
-L['squirrels_note'] = nil
+L['squirrels_note'] = 'Tienes que usar la emoción /amar en las alimañas que no sean mascotas de combate.'
 L['options_icons_squirrels'] = '{achievement:2557}'
-L['options_icons_squirrels_desc'] = nil
+L['options_icons_squirrels_desc'] = 'Muesta dónde están las alimañas para el logro {achievement:2557}.'
 
-L['achievement_friend_or_fowl_desc'] = nil
-L['note_devouring_maggot'] = nil
-L['dalaran_sewers'] = nil
-L['in_nexus'] = nil
+L['achievement_friend_or_fowl_desc'] = 'Mata 15 pavos en menos de 3 minutos.'
+L['note_devouring_maggot'] = 'En lo profundo de las |cFFFFFD00Catacumbas de Utgarde|r.'
+L['dalaran_sewers'] = 'En las |cFFFFFD00Cloacas de Dalaran|r.'
+L['in_nexus'] = 'Dentro de |cFFFFFD00El Nexo|r en las |cFFFFFD00Cámaras de Éxtasis|r.'
