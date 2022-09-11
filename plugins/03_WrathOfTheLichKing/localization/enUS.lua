6fb2368f03d31960197d78596197838dcd62baec
Krovikan <krovikan@gmail.com>
2022-09-11 12:09:15 +0200

    Updated WotLK enUS

--- plugins/03_WrathOfTheLichKing/localization/enUS.lua
+++ plugins/03_WrathOfTheLichKing/localization/enUS.lua
@@ -4,11 +4,11 @@ if not L then return end
 
 L['area_spawn'] = 'Spawns in the surrounding area.'
 
-L['squirrels_note'] = 'You must use the emote /love on critters not battle pets'
+L['squirrels_note'] = 'You must use the emote /love on critters not battle pets.'
 L['options_icons_squirrels'] = '{achievement:2557}'
-L['options_icons_squirrels_desc'] = 'Critter locations for {achievement:2557} achievements'
+L['options_icons_squirrels_desc'] = 'Critter locations for {achievement:2557} achievement.'
 
-L['achievement_friend_or_fowl_desc'] = 'Slay 15 turkeys in 3 minutes'
-L['note_devouring_maggot'] = 'Deep down in the |cFFFFFD00Utgarde Catacombs|r'
-L['dalaran_sewers'] = 'In the |cFFFFFD00Dalaran Sewers|r'
-L['in_nexus'] = 'Inside |cFFFFFD00The Nexus|r at the |cFFFFFD00Hall of Stasis|r'
+L['achievement_friend_or_fowl_desc'] = 'Slay 15 turkeys in 3 minutes.'
+L['note_devouring_maggot'] = 'Deep down in the |cFFFFFD00Utgarde Catacombs|r.'
+L['dalaran_sewers'] = 'In the |cFFFFFD00Dalaran Sewers|r.'
+L['in_nexus'] = 'Inside |cFFFFFD00The Nexus|r at the |cFFFFFD00Hall of Stasis|r.'
