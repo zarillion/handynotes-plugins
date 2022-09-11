6d13a8edd66f47f50ac5c33cae2f7048b241a958
Krovikan <krovikan@gmail.com>
2022-09-11 11:18:32 +0200

    Clearing blank line esES

--- plugins/03_WrathOfTheLichKing/localization/esES.lua
+++ plugins/03_WrathOfTheLichKing/localization/esES.lua
@@ -1,7 +1,7 @@
+-- Spanish Translation by (EU) Krovikan-Minahonda
 local ADDON_NAME, ns = ...
 local L = ns.NewLocale('esES')
 if not L then return end
-
 
 L['area_spawn'] = 'Aparece en los alrededores.'
 
