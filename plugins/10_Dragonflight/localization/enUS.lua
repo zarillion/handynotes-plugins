5b5b0f9c3d2adbed3e80f6161c01a81ac4b5669d
wyldclaw <wyldclawthornbringer@gmail.com>
2022-09-13 09:53:30 -0700

    Add initial data for Lizi (Thunderspine Trampler) mount

--- plugins/10_Dragonflight/localization/enUS.lua
+++ plugins/10_Dragonflight/localization/enUS.lua
@@ -10,6 +10,22 @@ if not L then return end
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
 -------------------------------------------------------------------------------
 
+-- {quest:65901} = Sneaking Out
+-- {currency:2003} = Dragon Isle Supplies
+-- {item:192615} = Flourescent Fluid
+-- {item:192658} = High-Fiber Leaf
+-- {item:194966} = Thousandbite Piranha
+-- {item:192636} = Woolly Mountain Pelt
+-- {item:200598} = Meluun's Green Curry
+-- {npc:190015} = Ohn Meluun
+
+L['lizi_note'] = 'Complete the Initiate\'s Day Out storyline starting with {quest:65901}. Complete the quests each day to mend Lizi and receive you mount.\n\nIn addition to items required all quests require 150x {currency:2003} for a total of 750x {currency:2003}.'
+L['lizi_note_day1'] = 'Collect 20x {item:192615} from insect mobs in the |cFFFFFD00Dragon Isles|r'
+L['lizi_note_day2'] = 'Collect 10x {item:192658} from plant mobs in the |cFFFFFD00Dragon Isles|r'
+L['lizi_note_day3'] = 'Collect 10x {item:194966} fished from any waters in the |cFFFFFD00Dragon Isles|r. Most commonly found in inland |cFFFFFD00Ohn\'ahran Plains|r'
+L['lizi_note_day4'] = 'Collect 20x {item:192636} from mammoths in |cFFFFFD00Ohn\'ahran Plains|r'
+L['lizi_note_day5'] = 'purchase 1x {item:200598} from {npc:190015} in a tent south of |cFFFFFD00Ohn\'iri Springs|r'
+
 -------------------------------------------------------------------------------
 --------------------------------- THALDRASZUS ---------------------------------
 -------------------------------------------------------------------------------
