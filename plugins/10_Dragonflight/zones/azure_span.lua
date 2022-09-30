aaf76dfae0eb82fe2b87ccc4100158453c04b3da
Dathwada <dathwada@gmail.com>
2022-09-30 22:47:38 +0200

    Add treasures

--- plugins/10_Dragonflight/zones/azure_span.lua
+++ plugins/10_Dragonflight/zones/azure_span.lua
@@ -1,7 +1,52 @@
 -------------------------------------------------------------------------------
 ---------------------------------- NAMESPACE ----------------------------------
 -------------------------------------------------------------------------------
--- local ADDON_NAME, ns = ...
--- local Map = ns.Map
+local ADDON_NAME, ns = ...
+local Class = ns.Class
+-- local L = ns.locale
+local Map = ns.Map
+
+-- local Rare = ns.node.Rare
+-- local Treasure = ns.node.Treasure
+
+local Achievement = ns.reward.Achievement
+
 -------------------------------------------------------------------------------
--- local map = Map({id = 2024, settings = true})
+
+local map = Map({id = 2024, settings = true})
+
+-------------------------------------------------------------------------------
+---------------------------------- TREASURES ----------------------------------
+-------------------------------------------------------------------------------
+
+-- https://www.wowhead.com/beta/achievement=16300/treasures-of-the-azure-span#comments
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16300, criteria = 54804})}
+-- }) -- Forgotten Jewel Box
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16300, criteria = 54805})}
+-- }) -- Gnoll Fiend Flail
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16300, criteria = 54807})}
+-- }) -- Lost Compass
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16300, criteria = 54809})}
+-- }) -- Pepper Hammer
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16300, criteria = 54808})}
+-- }) -- Rubber Fish
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16300, criteria = 54806})}
+-- }) -- Sapphire Gem Cluster
--- plugins/10_Dragonflight/zones/waking_shores.lua
+++ plugins/10_Dragonflight/zones/waking_shores.lua
@@ -1,7 +1,62 @@
 -------------------------------------------------------------------------------
 ---------------------------------- NAMESPACE ----------------------------------
 -------------------------------------------------------------------------------
--- local ADDON_NAME, ns = ...
--- local Map = ns.Map
+local ADDON_NAME, ns = ...
+local Class = ns.Class
+-- local L = ns.locale
+local Map = ns.Map
+
+-- local Rare = ns.node.Rare
+local Treasure = ns.node.Treasure
+
+local Achievement = ns.reward.Achievement
+
 -------------------------------------------------------------------------------
--- local map = Map({id = 2022, settings = true})
+
+local map = Map({id = 2022, settings = true})
+
+-------------------------------------------------------------------------------
+---------------------------------- TREASURES ----------------------------------
+-------------------------------------------------------------------------------
+
+-- https://www.wowhead.com/beta/achievement=16297/treasures-of-the-waking-shores#comments
+
+map.nodes[40454136] = Treasure({
+    quest = nil,
+    rewards = {Achievement({id = 16297, criteria = 54699})}
+}) -- Bubble Drifter
+
+map.nodes[69314658] = Treasure({
+    quest = nil,
+    rewards = {Achievement({id = 16297, criteria = 54702})}
+}) -- Dead Man's Chestplate
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16297, criteria = 55403})}
+-- }) -- Misty Treasure Chest
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16297, criteria = 55448})}
+-- }) -- Onyx Gem Cluster
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16297, criteria = 54698})}
+-- }) -- Replica Dragon Goblet
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16297, criteria = 54713})}
+-- }) -- Ruby Gem Cluster
+
+-- map.nodes[] = Treasure({
+--     quest = nil,
+--     rewards = {Achievement({id = 16297, criteria = 54703})}
+-- }) -- Torn Riding Pack
+
+map.nodes[46713121] = Treasure({
+    quest = nil,
+    rewards = {Achievement({id = 16297, criteria = 54701})}
+}) -- Yennu's Kite
