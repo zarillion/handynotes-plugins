e8e0d743bdac78a54369d180f2a03973276da612
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-10-09 22:00:16 +0200

    add frFr.lua - Mists Of Pandaria

---
+++ plugins/05_MistsOfPandaria/localization/frFR.lua
@@ -0,0 +1,151 @@
+-- French translation by Lightuky EU-Khaz Modan
+local ADDON_NAME, ns = ...
+local L = ns.NewLocale('frFR')
+if not L then return end
+
+-------------------------------------------------------------------------------
+-------------------------------- TIMELESS ISLE --------------------------------
+-------------------------------------------------------------------------------
+
+L['cavern_of_lost_spirits'] = 'Dans la Caverne des Esprits perdus.'
+L['looted_twice'] = 'Vous n\'avez jamais tué ce rare sur ce personnage. Il peut être looté deux fois aujourd\'hui.'
+L['neverending_spritewood'] = 'Bois de lutins sans fin'
+L['neverending_spritewood_note'] = 'Après avoir brisé le Bois de lutins sans fin, tuez autant de {npc:71824s} que possible pendant que vous avez le debuff {spell:144052} !'
+L['zarhym_note'] = 'Une fois par jour, vous pouvez parler à {npc:71876} pour entrer dans le {spell:144145} et tenter de récupérer son corps.'
+
+L['archiereus_note'] = 'Achetez un {item:103684} au {npc:73306} pour invoquer ce rare.'
+L['chelon_note'] = 'Cliquez sur la Carapace visiblement vide pour faire apparaître le rare.'
+L['cranegnasher_note'] = 'Kitez une {npc:72095} depuis le sud jusqu\'au cadavre de la grue.'
+L['dread_ship_note'] = 'Récupérez la {item:104115} sur {npc:73279} et utilisez-la à la Pierre tombale maudite pour invoquer le rare.'
+L['emerald_gander_note'] = 'Tuez des {npc:72762s} autour de la Cour Céleste jusqu\'à ce que {npc:73158} apparaisse.'
+L['evermaw_note'] = 'Nage dans le sens des aiguilles d\'une montre tout autour de l\'île.'
+L['great_turtle_furyshell_note'] = 'Tuez des {npc:72764s} ou {npc:72763s} jusqu\'à ce que {npc:73161} apparaisse.'
+L['imperial_python_note'] = 'Tuez des {npc:72841s} autour de la Cour Céleste jusqu\'à ce que {npc:73163} apparaisse.'
+L['ironfur_steelhorn_note'] = 'Tuez des {npc:72844s} autour de la Cour Céleste jusqu\'à ce que {npc:73160} apparaisse.'
+L['karkanos_note'] = 'Parlez à {npc:72151} pour remonter le rare. Quelle prise !'
+L['monstrous_spineclaw_note'] = 'Tuez des {npc:72766s} tout autour de l\'île jusqu\'à ce que {npc:73166} apparaisse.'
+L['rattleskew_note'] = 'Vainquez des vagues de {npc:72033s} jusqu\'à ce que le rare apparaisse.'
+L['spelurk_cave'] = 'Dans la grotte de {npc:71864}.'
+L['spelurk_note'] = [[
+Pour pénétrer dans la grotte de {npc:71864}, trouvez l'un des artefacts perdus éparpillés sur l'île et utilisez sa capacité sur les rochers qui bloquent l'entrée.
+
+• {spell:147278}
+• {spell:149333}
+• {spell:149345}
+• {spell:149350}
+
+Si vous ne trouvez pas d'artefact, vous pouvez utiliser {item:70161} (ou d'autres jouets permettant de s'asseoir) juste devant les rochers. Asseyez-vous, puis levez-vous et avancez tout droit.
+]]
+L['zhugon_note'] = 'Lorsque l\'événement de brassage est actif, tuez des {npc:71908s} et brisez des barils jusqu\'à ce que le rare apparaisse.'
+
+L['blazing_chest'] = 'Coffre flamboyant'
+L['moss_covered_chest'] = 'Coffre couvert de mousse'
+L['skull_covered_chest'] = 'Coffre recouvert de crânes'
+L['smoldering_chest'] = 'Coffre fumant'
+L['sturdy_chest'] = 'Coffre solide'
+L['sturdy_chest_note'] = 'Frappez un {npc:73531} pour être transporté ici.'
+
+L['gleaming_treasure_satchel_note'] = 'Marchez sur les cordes du bateau puis sautez sur le poteau où est suspendue la sacoche.'
+L['gleaming_treasure_chest_note'] = 'Sautez sur les piliers pour atteindre le trésor.'
+L['mist_covered_treasure_chest_note'] = [[
+Nécessite de looter d'abord le Coffre au trésor resplendissant et le Coffre au trésor fermé par une corde.
+
+Cliquez sur la Statue de grue resplendissante pour vous envoler vers le trésor.
+]]
+L['ropebound_treasure_chest_note'] = 'Marchez sur les cordes jusqu\'au trésor.'
+L['sunken_treasure_note'] = 'Tuez des élites sur le navire englouti pour obtenir la clé.'
+
+-------------------------------------------------------------------------------
+--------------------------------- ACROSS ZONES --------------------------------
+-------------------------------------------------------------------------------
+
+L['zandalari_warbringer_note'] = [[
+Peut être looté plusieurs fois par jour.
+
+La couleur de la monture du {npc:69842} déterminera la couleur de la monture lâchée.
+]]
+L['zandalari_warbringer_killed'] = 'Porteguerre zandalari tué.'
+
+L['squirrels_note'] = 'Vous devez utiliser l\'émote /amour sur les bestioles, pas sur les familiers de combat.'
+L['options_icons_squirrels'] = '{achievement:6350}'
+L['options_icons_squirrels_desc'] = 'Emplacements des bestioles pour le haut-fait {achievement:6350}.'
+
+L['options_icons_lorewalker'] = '{achievement:6548}'
+L['options_icons_lorewalker_desc'] = 'Affiche les emplacements pour le haut-fait {achievement:6548}.'
+
+-------------------------------------------------------------------------------
+--------------------------------- Jade Forest ---------------------------------
+-------------------------------------------------------------------------------
+
+L['ancient_pick'] = '{item:85777} se trouve dans la Carrière de Pierre-Verte au niveau inférieur.'
+L['ships_locker'] = 'Placard de bateau'
+L['ships_locker_note'] = 'dans le navire englouti. Contient environ 96 or.'
+L['chest_of_supplies'] = 'Coffre de fournitures'
+L['chest_of_supplies_note'] = 'Contient environ 10 or.'
+L['offering_of_rememberance'] = 'Offrandes du souvenir'
+L['offering_of_rememberance_note'] = 'Contient environ 30 or.'
+L['stash_of_gems'] = 'Planque de gemmes'
+L['stash_of_gems_note'] = 'Contient environ 7 or et des gemmes.'
+
+-------------------------------------------------------------------------------
+-------------------------------- Krasarang Wilds ------------------------------
+-------------------------------------------------------------------------------
+
+L['equipment_locker'] = 'Placard d’équipement'
+
+-------------------------------------------------------------------------------
+------------------------------- The Veiled Stair ------------------------------
+-------------------------------------------------------------------------------
+
+L['forgotten_lockbox'] = 'Coffret oublié'
+L['forgotten_lockbox_note'] = 'Dans la taverne au deuxième étage.\nContient environ 10 or.'
+
+-------------------------------------------------------------------------------
+-------------------------- Vale of Eternal Blossoms ---------------------------
+-------------------------------------------------------------------------------
+
+L['guolai_halls'] = 'Dans les Salles de Guo-Lai'
+L['guolai_cache'] = 'Trouvez une {item:87779} et ouvrez une Cache antique de Guo-Lai.'
+
+-------------------------------------------------------------------------------
+------------------------------- Kun Lai Summit --------------------------------
+-------------------------------------------------------------------------------
+
+L['lost_adventurers_belongings'] = 'Affaires de l’aventurier perdu' -- wowhead.com/object=213774
+L['lost_adventurers_belongings_note'] = 'Contient environ 97 or.'
+L['momos_treasure_chest'] = 'Coffre au trésor de Mo Mo' -- wowhead.com/object=214407
+L['momos_treasure_chest_note'] = 'Contient environ 10 or.'
+L['hozen_treasure_cache'] = 'Cache au trésor hozen' -- wowhead.com/object=213769
+L['hozen_treasure_cache_note'] = 'Contient environ 99 or.'
+L['rikktiks_tick_remover'] = 'Minuscule coffre de Rikktik' -- wowhead.com/object=213793
+L['stolen_sprite_treasure'] = 'Trésor de lutin volé' -- wowhead.com/object=213770
+L['stolen_sprite_treasure_note'] = 'Contient environ 104 or.'
+L['sturdy_yaungol_spear'] = 'Planque d’armes yaungoles' -- wowhead.com/object=213842
+L['sprites_cloth_chest'] = 'Coffre de lutin en tissu' -- wowhead.com/object=213751
+
+-------------------------------------------------------------------------------
+------------------------------- Townlong Steppes ------------------------------
+-------------------------------------------------------------------------------
+
+L['abandoned_crate_of_goods'] = 'Caisse de marchandises abandonnée' -- wowhead.com/object=213961
+L['abandoned_crate_of_goods_note'] = 'Contient environ 103 or.'
+
+-------------------------------------------------------------------------------
+----------------------------- Valley of Four Winds ----------------------------
+-------------------------------------------------------------------------------
+
+L['cache_of_pilfered_goods'] = 'Cache de fournitures chapardées'
+L['virmen_treasure_cache'] = 'Cache au trésor virmène'
+
+-------------------------------------------------------------------------------
+------------------------------- Isle of Thunder -------------------------------
+-------------------------------------------------------------------------------
+
+L['options_icons_kroshik'] = '{achievement:8108}'
+L['options_icons_kroshik_desc'] = 'Affiche les emplacements pour {achievement:8108}.'
+
+L['iot_portal'] = 'Portail'
+L['ritualstone_needed'] = 'Il vous faut 3 {item:94221} pour invoquer le rare.'
+L['kroshik_bow'] = '/incliner'
+L['kroshik_adult'] = 'Court autour du lac.\n/incliner'
+L['kroshik_baby'] = 'Court dans les environs.\n/incliner'
