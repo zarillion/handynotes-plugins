fffa7e7645fa0751be58caf8cd604889c27fad8c
Krovikan <krovikan@gmail.com>
2022-10-18 01:33:23 +0200

    Translated all DF file until now esES

--- plugins/10_Dragonflight/localization/esES.lua
+++ plugins/10_Dragonflight/localization/esES.lua
@@ -11,41 +11,41 @@ L['dragon_glyph'] = 'Glifos Dracónicos'
 L['options_icons_dragon_glyph'] = 'Glifos Dracónicos'
 L['options_icons_dragon_glyph_desc'] = 'Muestra dónde están los 48 Glifos dracónicos.'
 
-L['dragonscale_expedition_flag'] = nil
-L['flags_placed'] = nil
+L['dragonscale_expedition_flag'] = 'Expedición Dragontina'
+L['flags_placed'] = 'banderas puestas'
 L['options_icons_flag'] = '{achievement:15890}'
-L['options_icons_flag_desc'] = nil
+L['options_icons_flag_desc'] = 'Muestra dónde están las 20 banderas para el logro {achievement:15890}.'
 
 L['options_icons_kite'] = '{achievement:16584}'
-L['options_icons_kite_desc'] = nil
+L['options_icons_kite_desc'] = 'Muestra dónde están las {npc:198118} para el logro {achievement:16584}.'
 
-L['disturbed_dirt'] = nil
-L['options_icons_disturbed_dirt'] = nil
-L['options_icons_disturbed_dirt_desc'] = nil
+L['disturbed_dirt'] = 'Tierra Perturbada'
+L['options_icons_disturbed_dirt'] = 'Tierra Perturbada'
+L['options_icons_disturbed_dirt_desc'] = 'Muestrá dónde están las Tierra Perturbada.'
 
-L['scout_pack'] = nil
-L['options_icons_scout_pack'] = nil
-L['options_icons_scout_pack_desc'] = nil
+L['scout_pack'] = 'Mochilas de Explorador de Expedición'
+L['options_icons_scout_pack'] = 'Mochilas de explorador de expedición'
+L['options_icons_scout_pack_desc'] = 'Muestra dónde están las Mochilas de explorador de expedición.'
 
 -------------------------------------------------------------------------------
 ------------------------------- THE AZURE SPAN --------------------------------
 -------------------------------------------------------------------------------
 
-L['blisterhide_note'] = nil
-L['gnarls_note'] = nil
-L['high_shaman_rotknuckle_note'] = nil
-L['snarglebone_note'] = nil
-L['trilvarus_loreweaver_note'] = nil
+L['blisterhide_note'] = 'Aparece simultáneamente con {npc:197344}, {npc:197356} y {npc:197354}.'
+L['gnarls_note'] = 'Aparece simultáneamente con {npc:197344}, {npc:197353} y {npc:197356}.'
+L['high_shaman_rotknuckle_note'] = 'Aparece simultáneamente con {npc:197344}, {npc:197353} y {npc:197354}.'
+L['snarglebone_note'] = 'Aparece simultáneamente con {npc:197353}, {npc:197356} y {npc:197354}.'
+L['trilvarus_loreweaver_note'] = 'Recoge Fragmentos Cantarines para conseguir {spell:382076} y usar el Enfoque Descargado para hacer aparecer al raro.'
 
-L['forgotten_jewel_box_note'] = nil
+L['forgotten_jewel_box_note'] = 'Despoja la {item:199065} de un Cofre del Clan en Retiro Crestagua (49.4 67.3) en las Llanuras de Ohn\'ahran y úsala.'
 
 -------------------------------------------------------------------------------
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
 -------------------------------------------------------------------------------
 
-L['shade_of_grief_note'] = nil
+L['shade_of_grief_note'] = 'Haz click en {npc:193166} para hacer aparecer al raro.'
 
-L['nokhud_warspear_note'] = nil
+L['nokhud_warspear_note'] = 'Las {item:194540} se pueden encontrar en las Mochilas de explorador de expedición.'
 L['slightly_chewed_duck_egg_note'] = 'Encuentra y acaricia a {npc:192997} para conseguir el {item:195453} y entonces usarlo. Tras incubar el {item:199171} durante 3 días eclosionará en {item:199172}.'
 
 L['lizi_note'] = 'Completa la cadena de misiones Día de Iniciación comenzando con {quest:65901}. Completa las misiones cada día para curar a Lizi y recibir su montura.\n\nAdemás de los objetos necesarios, todas las misiones requieren 150x {currency:2003} para un total de 750x {currency:2003}.'
@@ -63,12 +63,12 @@ L['lizi_note_day5'] = 'Compra 1x {item:200598} a {npc:190015} en la tienda al su
 ------------------------------ THE WAKING SHORE -------------------------------
 -------------------------------------------------------------------------------
 
-L['brundin_the_dragonbane_note'] = nil
-L['shasith_note'] = nil
+L['brundin_the_dragonbane_note'] = 'El Destacamento de Guerra Qalashi viaja encima del {npc:192737} a esta Torre.'
+L['shasith_note'] = 'Dentro del Trono Obsidiana. \n\nNECESITA MÁS INVESTIGACIÓN.'
 
-L['onyx_gem_cluster_note'] = nil
-L['replica_dragon_goblet_note'] = nil
+L['onyx_gem_cluster_note'] = 'Compra el {item:200738} a {npc:189065} por 3 {item:192863} y 500 {currency:2003} a Renombre 21 con la Expedición Dragontina y úsalo.'
+L['replica_dragon_goblet_note'] = 'Compra las {item:198854} a {npc:193915} en la Embajada Reposo Alado y úsalas.'
 
-L['fullsails_supply_chest'] = nil
-L['hidden_hornswog_hoard'] = 'Botín oculto de los cornisapos'
-L['hidden_hornswog_hoard_note'] = nil
+L['fullsails_supply_chest'] = 'Cofre de Suministros Todavela'
+L['hidden_hornswog_hoard'] = 'Botín Oculto de los Cornisapos'
+L['hidden_hornswog_hoard_note'] = 'Recoge {item:200064}, {item:200065} y {item:200066}, combínalos en "Acertijo de observador: Una Guía de Campo" cerca del tesoro y entonces alimenta a la rana.'
