df3f7304facf0191ec29bd568c30e1c05bc60f66
Ioney <dominik.hier@gmail.com>
2022-10-18 21:01:28 +0200

    Update common.lua

--- plugins/06_WarlordsOfDraenor/common.lua
+++ plugins/06_WarlordsOfDraenor/common.lua
@@ -60,7 +60,8 @@ ns.groups.WRITING_IN_THE_SNOW = Group('writing_in_the_snow', 133680,
 ns.groups.YOU_HAVE_BEEN_RYLAKINATED = Group('you_have_been_rylakinated', 894222,
     {defaults = ns.GROUP_HIDDEN})
 ns.groups.SQUIRRELS = Group('squirrels', 237182, {defaults = ns.GROUP_HIDDEN})
-ns.groups.GARRFOLLOWER = Group('GarrFollower', 1033590, {defaults = ns.GROUP_HIDDEN})
+ns.groups.GARRFOLLOWER = Group('GarrFollower', 1033590,
+    {defaults = ns.GROUP_HIDDEN})
 
 -------------------------------------------------------------------------------
 ---------------------------- Squirrel Achievement -----------------------------
