29e6c2cc9677ee417eb999c1357174d214cb35ff
Adavak <adavak@adavak.com>
2022-10-26 08:32:56 +0800

    Update zhCN.lua

--- plugins/10_Dragonflight/localization/zhCN.lua
+++ plugins/10_Dragonflight/localization/zhCN.lua
@@ -6,53 +6,61 @@ if not L then return end
 -------------------------------- DRAGON ISLES ---------------------------------
 -------------------------------------------------------------------------------
 
-L['dragon_glyph'] = nil
-L['options_icons_dragon_glyph'] = nil
-L['options_icons_dragon_glyph_desc'] = nil
+L['dragon_glyph'] = '巨龙魔符'
+L['options_icons_dragon_glyph'] = '巨龙魔符'
+L['options_icons_dragon_glyph_desc'] = '显示全部48个巨龙魔符的位置。'
 
-L['dragonscale_expedition_flag'] = nil
-L['flags_placed'] = nil
+L['dragonscale_expedition_flag'] = '龙鳞探险队旗帜'
+L['flags_placed'] = '已插旗帜'
 L['options_icons_flag'] = '{achievement:15890}'
-L['options_icons_flag_desc'] = nil
+L['options_icons_flag_desc'] = '显示 {achievement:15890} 成就中全部20个旗帜的位置。'
 
 L['options_icons_kite'] = '{achievement:16584}'
-L['options_icons_kite_desc'] = nil
+L['options_icons_kite_desc'] = '显示 {achievement:16584} 成就中 {npc:198118} 的位置。'
 
-L['disturbed_dirt'] = nil
-L['options_icons_disturbed_dirt'] = nil
-L['options_icons_disturbed_dirt_desc'] = nil
+L['disturbed_dirt'] = '翻动过的泥土'
+L['options_icons_disturbed_dirt'] = '翻动过的泥土'
+L['options_icons_disturbed_dirt_desc'] = '显示翻动过的泥土的位置。'
 
-L['scout_pack'] = nil
-L['options_icons_scout_pack'] = nil
-L['options_icons_scout_pack_desc'] = nil
+L['scout_pack'] = '探险队斥候包裹'
+L['options_icons_scout_pack'] = '探险队斥候包裹'
+L['options_icons_scout_pack_desc'] = '显示探险队斥候包裹的位置。'
 
 -------------------------------------------------------------------------------
 ------------------------------- THE AZURE SPAN --------------------------------
 -------------------------------------------------------------------------------
 
-L['blisterhide_note'] = nil
-L['gnarls_note'] = nil
-L['high_shaman_rotknuckle_note'] = nil
-L['snarglebone_note'] = nil
-L['trilvarus_loreweaver_note'] = nil
+L['blisterhide_note'] = '同 {npc:197344}、{npc:197356}、{npc:197354} 同时出现。'
+L['gnarls_note'] = '同 {npc:197344}、{npc:197353}、{npc:197356} 同时出现。'
+L['high_shaman_rotknuckle_note'] = '同 {npc:197344}、{npc:197353}、{npc:197354} 同时出现。'
+L['snarglebone_note'] = '同 {npc:197353}、{npc:197356}、{npc:197354} 同时出现。'
+L['trilvarus_loreweaver_note'] = '收集歌声碎片即可获得 {spell:382076} 并使用未知焦点刷新稀有。'
 
-L['forgotten_jewel_box_note'] = nil
+L['forgotten_jewel_box_note'] = '从欧恩哈拉平原的峦水居所拾取 {item:199065} 氏族宝箱（49.4 67.3）并使用它。'
 
 -------------------------------------------------------------------------------
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
 -------------------------------------------------------------------------------
 
-L['shade_of_grief_note'] = nil
+-- {quest:65901} = Sneaking Out
+-- {currency:2003} = Dragon Isle Supplies
+-- {item:192615} = Flourescent Fluid
+-- {item:192658} = High-Fiber Leaf
+-- {item:194966} = Thousandbite Piranha
+-- {item:192636} = Woolly Mountain Pelt
+-- {item:200598} = Meluun's Green Curry
+-- {npc:190015} = Ohn Meluun
+L['shade_of_grief_note'] = '点击 {npc:193166} 刷新稀有。'
 
-L['nokhud_warspear_note'] = nil
-L['slightly_chewed_duck_egg_note'] = nil
+L['nokhud_warspear_note'] = '{item:194540} 可以在探险队斥候包裹找到。'
+L['slightly_chewed_duck_egg_note'] = '找到并拍打 {npc:192997} 获得 {item:195453} 并使用它。{item:199171} 3天后孵化为 {item:199172}。'
 
-L['lizi_note'] = nil
-L['lizi_note_day1'] = nil
-L['lizi_note_day2'] = nil
-L['lizi_note_day3'] = nil
-L['lizi_note_day4'] = nil
-L['lizi_note_day5'] = nil
+L['lizi_note'] = '完成从 {quest:65901} 开始的 Initiate\'s Day Out 任务线。每天完成任务以修复莉姬并获得坐骑。\n\n除了需要的物品外，所有任务都需要150个 {currency:2003}，总共需要750个 {currency:2003}。'
+L['lizi_note_day1'] = '从 |cFFFFFD00巨龙群岛s|r 的昆虫怪物收集20个 {item:192615}。'
+L['lizi_note_day2'] = '从 |cFFFFFD00巨龙群岛s|r 的植物怪物收集10个 {item:192658}。'
+L['lizi_note_day3'] = '从 |cFFFFFD00巨龙群岛s|r 的任意水域钓鱼收集10个 {item:194966}。常见于 |cFFFFFD00欧恩哈拉平原|r 内陆。'
+L['lizi_note_day4'] = '从 |cFFFFFD00欧恩哈拉平原|r 的猛犸象收集20个 {item:192636}。'
+L['lizi_note_day5'] = '从 |cFFFFFD00欧恩伊尔清泉|r 南边的帐篷的 {npc:190015} 购买1个 {item:200598}。'
 
 -------------------------------------------------------------------------------
 --------------------------------- THALDRASZUS ---------------------------------
@@ -62,12 +70,12 @@ L['lizi_note_day5'] = nil
 ------------------------------ THE WAKING SHORE -------------------------------
 -------------------------------------------------------------------------------
 
-L['brundin_the_dragonbane_note'] = nil
-L['shasith_note'] = nil
+L['brundin_the_dragonbane_note'] = '卡拉希战队从 {npc:192737} 前往这座塔。'
+L['shasith_note'] = '黑曜王座内。\n\n需要更多探索'
 
-L['onyx_gem_cluster_note'] = nil
-L['replica_dragon_goblet_note'] = nil
+L['onyx_gem_cluster_note'] = '龙鳞探险队声望到达21级，从 {npc:189065} 购买 {item:200738} 需要3个 {item:192863} 和500 {currency:2003} 并使用它。'
+L['replica_dragon_goblet_note'] = '从翼眠大使馆的 {npc:193915} 购买 {item:198854} 并使用它。'
 
-L['fullsails_supply_chest'] = nil
-L['hidden_hornswog_hoard'] = '隐藏的龙蛙宝藏'
-L['hidden_hornswog_hoard_note'] = nil
+L['fullsails_supply_chest'] = '满帆补给箱'
+L['hidden_hornswog_hoard'] = '隐藏的龙蛙存货'
+L['hidden_hornswog_hoard_note'] = '收集 {item:200064}、{item:200065}、{item:200066}，在宝藏附近的“细心的谜语：现场指南”将它们组合起来，然后喂青蛙。'
