9d1c3753b20bc3a05d2c396f9d0663d2e34c8511
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-10-27 08:26:03 +0200

    add translation frFR.lua - Dragonflight

--- plugins/10_Dragonflight/localization/frFR.lua
+++ plugins/10_Dragonflight/localization/frFR.lua
@@ -1,3 +1,4 @@
+-- French translation by Lightuky EU-Khaz Modan
 local ADDON_NAME, ns = ...
 local L = ns.NewLocale('frFR')
 if not L then return end
@@ -6,53 +7,53 @@ if not L then return end
 -------------------------------- DRAGON ISLES ---------------------------------
 -------------------------------------------------------------------------------
 
-L['dragon_glyph'] = nil
-L['options_icons_dragon_glyph'] = nil
-L['options_icons_dragon_glyph_desc'] = nil
+L['dragon_glyph'] = 'Dracoglyphe'
+L['options_icons_dragon_glyph'] = 'Dracoglyphes'
+L['options_icons_dragon_glyph_desc'] = 'Afficher les emplacements des 48 dracoglyphes.'
 
-L['dragonscale_expedition_flag'] = nil
-L['flags_placed'] = nil
+L['dragonscale_expedition_flag'] = 'Drapeau de l\'Expédition du Dracaret'
+L['flags_placed'] = 'drapeaux placés'
 L['options_icons_flag'] = '{achievement:15890}'
-L['options_icons_flag_desc'] = nil
+L['options_icons_flag_desc'] = 'Afficher les emplacements des 20 drapeaux pour le haut-fait {achievement:15890}.'
 
 L['options_icons_kite'] = '{achievement:16584}'
-L['options_icons_kite_desc'] = nil
+L['options_icons_kite_desc'] = 'Afficher les emplacements des {npc:198118s} pour le haut-fait {achievement:16584}.'
 
-L['disturbed_dirt'] = nil
-L['options_icons_disturbed_dirt'] = nil
-L['options_icons_disturbed_dirt_desc'] = nil
+L['disturbed_dirt'] = 'Poussière dérangée'
+L['options_icons_disturbed_dirt'] = 'Poussières dérangées'
+L['options_icons_disturbed_dirt_desc'] = 'Afficher les emplacements des Poussières dérangées.'
 
-L['scout_pack'] = nil
-L['options_icons_scout_pack'] = nil
-L['options_icons_scout_pack_desc'] = nil
+L['scout_pack'] = 'Paquetage d\'éclaireur de l\'expédition'
+L['options_icons_scout_pack'] = 'Paquetages d\'éclaireurs de l\'expédition'
+L['options_icons_scout_pack_desc'] = 'Affichez les emplacements des Paquetages d\'éclaireurs de l\'expédition.'
 
 -------------------------------------------------------------------------------
 ------------------------------- THE AZURE SPAN --------------------------------
 -------------------------------------------------------------------------------
 
-L['blisterhide_note'] = nil
-L['gnarls_note'] = nil
-L['high_shaman_rotknuckle_note'] = nil
-L['snarglebone_note'] = nil
-L['trilvarus_loreweaver_note'] = nil
+L['blisterhide_note'] = 'Apparaît simultanément avec {npc:197344}, {npc:197356} et {npc:197354}'
+L['gnarls_note'] = 'Apparaît simultanément avec {npc:197344}, {npc:197353} et {npc:197356}'
+L['high_shaman_rotknuckle_note'] = 'Apparaît simultanément avec {npc:197344}, {npc:197353} et {npc:197354}'
+L['snarglebone_note'] = 'Apparaît simultanément avec {npc:197353}, {npc:197356} et {npc:197354}'
+L['trilvarus_loreweaver_note'] = 'Récupérez un Fragment chantant pour obtenir {spell:382076} et utilisez le Focalisateur déchargé pour faire apparaître le rare.'
 
-L['forgotten_jewel_box_note'] = nil
+L['forgotten_jewel_box_note'] = 'Récupérez la {item:199065} dans un Coffre clanique au Refuge d\'Ondecrête (49.4 67.3) dans les Plaines d\'Ohn\'ahra et utilisez-la.'
 
 -------------------------------------------------------------------------------
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
 -------------------------------------------------------------------------------
 
-L['shade_of_grief_note'] = nil
+L['shade_of_grief_note'] = 'Cliquez sur la {npc:193166} pour faire apparaître le rare.'
 
-L['nokhud_warspear_note'] = nil
-L['slightly_chewed_duck_egg_note'] = nil
+L['nokhud_warspear_note'] = '{item:194540} peut être trouvé dans les Paquetages d\'éclaireurs de l\'expédition.'
+L['slightly_chewed_duck_egg_note'] = 'Trouvez et caressez {npc:192997} pour obtenir une {item:195453} puis utilisez-la. l\'{item:199171} incube pendant 3 jours pour devenir {item:199172}.'
 
-L['lizi_note'] = nil
-L['lizi_note_day1'] = nil
-L['lizi_note_day2'] = nil
-L['lizi_note_day3'] = nil
-L['lizi_note_day4'] = nil
-L['lizi_note_day5'] = nil
+L['lizi_note'] = 'Terminez la suite de quêtes Une sortie d’initié en commençant par {quest:65901}. Accomplissez les quêtes chaque jour pour soigner Lizi et recevoir votre monture.\n\nEn plus des objets requis, toutes les quêtes demandent 150x {currency:2003} pour un total de 750x {currency:2003}.'
+L['lizi_note_day1'] = 'Collectez 20x {item:192615} sur des monstres insectes dans les |cFFFFFD00Îles aux Dragons|r'
+L['lizi_note_day2'] = 'Collectez 10x {item:192658} sur des monstres végétaux dans les |cFFFFFD00Îles aux Dragons|r'
+L['lizi_note_day3'] = 'Collectez 10x {item:194966} pêchés dans n\'importe quelles eaux des |cFFFFFD00Îles aux Dragons|r. Ils sont trouvés le plus souvent à l\'intérieur des terres des |cFFFFFD00Plaines d\'Ohn\'ahra|r'
+L['lizi_note_day4'] = 'Collectez 20x {item:192636} sur des mammouths dans les |cFFFFFD00Plaines d\'Ohn\'ahra|r'
+L['lizi_note_day5'] = 'Achetez 1x {item:200598} à {npc:190015} dans une tente au sud de |cFFFFFD00Sources des Ohn\'irs|r'
 
 -------------------------------------------------------------------------------
 --------------------------------- THALDRASZUS ---------------------------------
@@ -62,12 +63,12 @@ L['lizi_note_day5'] = nil
 ------------------------------ THE WAKING SHORE -------------------------------
 -------------------------------------------------------------------------------
 
-L['brundin_the_dragonbane_note'] = nil
-L['shasith_note'] = nil
+L['brundin_the_dragonbane_note'] = 'La Troupe Qalashi voyage sur leur {npc:192737} jusqu\'à cette Tour.'
+L['shasith_note'] = 'à l\'intérieur du Trône d\'obsidienne. \n\nNÉCESSITE PLUS DE RECHERCHES'
 
-L['onyx_gem_cluster_note'] = nil
-L['replica_dragon_goblet_note'] = nil
+L['onyx_gem_cluster_note'] = 'Achetez une {item:200738} à {npc:189065} pour 3 {item:192863} et 500 {currency:2003} au Renom 21 avec l\'Expédition du Dracaret et utilisez-la.'
+L['replica_dragon_goblet_note'] = 'Achetez une {item:198854} à {npc:193915} à l\'Ambassade de Trêve-Ailée et utilisez-la.'
 
-L['fullsails_supply_chest'] = nil
+L['fullsails_supply_chest'] = 'Clé de coffre de fournitures des Gréements'
 L['hidden_hornswog_hoard'] = 'Horde de bufflaciens cachée'
-L['hidden_hornswog_hoard_note'] = nil
+L['hidden_hornswog_hoard_note'] = 'Collectez {item:200064}, {item:200065} et {item:200066}, combinez-les à "l\'Énigmes sagaces : un guide de poche" près du trésor puis nourrissez la grenouille.'
