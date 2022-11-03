69f98be846e0e96cd9df0cfa534cbdedee7f6d6d
Krovikan <krovikan@gmail.com>
2022-11-03 12:37:50 +0100

    Updated Treasures DF esES

--- plugins/10_Dragonflight/localization/esES.lua
+++ plugins/10_Dragonflight/localization/esES.lua
@@ -9,7 +9,7 @@ if not L then return end
 
 L['dragon_glyph'] = 'Glifos Dracónicos'
 L['options_icons_dragon_glyph'] = 'Glifos Dracónicos'
-L['options_icons_dragon_glyph_desc'] = 'Muestra dónde están los 48 Glifos dracónicos.'
+L['options_icons_dragon_glyph_desc'] = 'Muestra dónde están los 48 Glifos Dracónicos.'
 
 L['dragonscale_expedition_flag'] = 'Expedición Dragontina'
 L['flags_placed'] = 'banderas puestas'
@@ -21,11 +21,11 @@ L['options_icons_kite_desc'] = 'Muestra dónde están las {npc:198118} para el l
 
 L['disturbed_dirt'] = 'Tierra Perturbada'
 L['options_icons_disturbed_dirt'] = 'Tierra Perturbada'
-L['options_icons_disturbed_dirt_desc'] = 'Muestrá dónde están las Tierra Perturbada.'
+L['options_icons_disturbed_dirt_desc'] = 'Muestrá dónde están las Tierras Perturbadas.'
 
 L['scout_pack'] = 'Mochilas de Explorador de Expedición'
 L['options_icons_scout_pack'] = 'Mochilas de explorador de expedición'
-L['options_icons_scout_pack_desc'] = 'Muestra dónde están las Mochilas de explorador de expedición.'
+L['options_icons_scout_pack_desc'] = 'Muestra dónde están las Mochilas de Explorador de Expedición.'
 
 -------------------------------------------------------------------------------
 ------------------------------- THE AZURE SPAN --------------------------------
@@ -38,7 +38,7 @@ L['snarglebone_note'] = 'Aparece simultáneamente con {npc:197353}, {npc:197356}
 L['trilvarus_loreweaver_note'] = 'Recoge Fragmentos Cantarines para conseguir {spell:382076} y usar el Enfoque Descargado para hacer aparecer al raro.'
 
 L['forgotten_jewel_box_note'] = 'Despoja la {item:199065} de un Cofre del Clan en Retiro Crestagua (49.4 67.3) en las Llanuras de Ohn\'ahran y úsala.'
-L['gnoll_fiend_flail_note'] = nil
+L['gnoll_fiend_flail_note'] = 'Las {item:199066} se pueden encontrar en las Mochilas de Explorador de Expedición y en las Tierras Perturbadas.'
 
 -------------------------------------------------------------------------------
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
@@ -54,10 +54,10 @@ L['gnoll_fiend_flail_note'] = nil
 -- {npc:190015} = Ohn Meluun
 L['shade_of_grief_note'] = 'Haz click en {npc:193166} para hacer aparecer al raro.'
 
-L['nokhud_warspear_note'] = 'Las {item:194540} se pueden encontrar en las Mochilas de explorador de expedición.'
+L['nokhud_warspear_note'] = 'Las {item:194540} se pueden encontrar en las Mochilas de Explorador de Expedición y en las Tierras Perturbadas.'
 L['slightly_chewed_duck_egg_note'] = 'Encuentra y acaricia a {npc:192997} para conseguir el {item:195453} y entonces usarlo. Tras incubar el {item:199171} durante 3 días eclosionará en {item:199172}.'
-L['yennus_boat'] = nil
-L['yennus_boat_note'] = nil
+L['yennus_boat'] = 'Barco de Juguete Tuskarr'
+L['yennus_boat_note'] = 'Despoja el Barco de Juguete Tuskarr para conseguir el {item:200876}, el cual comienza la misión {quest:72063} para devolvérselo a {npc:195252}.'
 
 L['lizi_note'] = 'Completa la cadena de misiones Día de Iniciación comenzando con {quest:65901}. Completa las misiones cada día para curar a Lizi y recibir su montura.\n\nAdemás de los objetos necesarios, todas las misiones requieren 150x {currency:2003} para un total de 750x {currency:2003}.'
 L['lizi_note_day1'] = 'Recoge 20x {item:192615} de los enemigos insecto en las |cFFFFFD00Islas Dragón|r.'
@@ -70,9 +70,9 @@ L['lizi_note_day5'] = 'Compra 1x {item:200598} a {npc:190015} en la tienda al su
 --------------------------------- THALDRASZUS ---------------------------------
 -------------------------------------------------------------------------------
 
-L['acorn_harvester_note'] = nil
-L['cracked_hourglass_note'] = nil
-L['sandy_wooden_duck_note'] = nil
+L['acorn_harvester_note'] = 'Recoge una Bellota del suelo cercano para conseguir {spell:388485} y entonces interactúa con {npc:196172}.'
+L['cracked_hourglass_note'] = 'La {item:199068} se puede encontrar en las Mochilas de Explorador de Expedición y en las Tierras Perturbadas.'
+L['sandy_wooden_duck_note'] = 'Recoge el {item:199069} y úsalo.'
 
 -------------------------------------------------------------------------------
 ------------------------------ THE WAKING SHORE -------------------------------
@@ -81,7 +81,7 @@ L['sandy_wooden_duck_note'] = nil
 L['brundin_the_dragonbane_note'] = 'El Destacamento de Guerra Qalashi viaja encima del {npc:192737} a esta Torre.'
 L['shasith_note'] = 'Dentro del Trono Obsidiana. \n\nNECESITA MÁS INVESTIGACIÓN.'
 
-L['bubble_drifter_note'] = nil
+L['bubble_drifter_note'] = 'La {item:199061} se puede encontrar en las Mochilas de Explorador de Expedición y en las Tierras Perturbadas.'
 L['onyx_gem_cluster_note'] = 'Compra el {item:200738} a {npc:189065} por 3 {item:192863} y 500 {currency:2003} a Renombre 21 con la Expedición Dragontina y úsalo.'
 L['replica_dragon_goblet_note'] = 'Compra las {item:198854} a {npc:193915} en la Embajada Reposo Alado y úsalas.'
 
