e9c7e550d4756369ca5a5499ed65cf06f199ad23
Dathwada <dathwada@gmail.com>
2022-11-06 00:39:00 +0100

    Add forbidden reach

---
+++ plugins/10_Dragonflight/zones/forbidden_reach.lua
@@ -0,0 +1,19 @@
+-------------------------------------------------------------------------------
+---------------------------------- NAMESPACE ----------------------------------
+-------------------------------------------------------------------------------
+-- local ADDON_NAME, ns = ...
+
+-- local L = ns.locale
+-- local Map = ns.Map
+
+-- local Rare = ns.node.Rare
+-- local Treasure = ns.node.Treasure
+
+-- local Item = ns.reward.Item
+
+-- local Path = ns.poi.Path
+-- local POI = ns.poi.POI
+
+-------------------------------------------------------------------------------
+
+-- local map = Map({id = 2118, settings = true})
