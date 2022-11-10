60ff437deed676e1441f083cbacc084676134b53
mcc <mcc@mccma.cc>
2022-11-11 03:19:18 +0800

    update zhTW.lua

--- plugins/10_Dragonflight/localization/zhTW.lua
+++ plugins/10_Dragonflight/localization/zhTW.lua
@@ -6,39 +6,39 @@ if not L then return end
 -------------------------------- DRAGON ISLES ---------------------------------
 -------------------------------------------------------------------------------
 
-L['dragon_glyph'] = nil
-L['options_icons_dragon_glyph'] = nil
-L['options_icons_dragon_glyph_desc'] = nil
+L['dragon_glyph'] = '龍之雕紋'
+L['options_icons_dragon_glyph'] = '龍之雕紋'
+L['options_icons_dragon_glyph_desc'] = '顯示全部48個龍之雕紋的位置'
 
-L['dragonscale_expedition_flag'] = nil
-L['flags_placed'] = nil
+L['dragonscale_expedition_flag'] = '龍鱗遠征隊'
+L['flags_placed'] = '旗幟已插上'
 L['options_icons_flag'] = '{achievement:15890}'
-L['options_icons_flag_desc'] = nil
+L['options_icons_flag_desc'] = '顯示 {achievement:15890} 成就中全部20個旗幟的位置.'
 
 L['options_icons_kite'] = '{achievement:16584}'
-L['options_icons_kite_desc'] = nil
+L['options_icons_kite_desc'] = '顯示成就 {achievement:16584} 所需的 {npc:198118} 位置.'
 
-L['disturbed_dirt'] = nil
-L['options_icons_disturbed_dirt'] = nil
-L['options_icons_disturbed_dirt_desc'] = nil
+L['disturbed_dirt'] = '挖過的土'
+L['options_icons_disturbed_dirt'] = '挖過的土'
+L['options_icons_disturbed_dirt_desc'] = '顯示挖過的土的位置'
 
-L['scout_pack'] = nil
-L['options_icons_scout_pack'] = nil
-L['options_icons_scout_pack_desc'] = nil
+L['scout_pack'] = '遠征隊斥侯包'
+L['options_icons_scout_pack'] = '遠征隊斥侯包'
+L['options_icons_scout_pack_desc'] = '顯示遠征隊斥侯包的位置'
 
 -------------------------------------------------------------------------------
 ------------------------------- THE AZURE SPAN --------------------------------
 -------------------------------------------------------------------------------
 
-L['blisterhide_note'] = nil
-L['fisherman_tinnak_note'] = nil
-L['gnarls_note'] = nil
-L['high_shaman_rotknuckle_note'] = nil
-L['snarglebone_note'] = nil
-L['trilvarus_loreweaver_note'] = nil
+L['blisterhide_note'] = '和 {npc:197344}, {npc:197356}, {npc:197354} 同時出現'
+L['fisherman_tinnak_note'] = '收集 破損的釣魚竿, 撕裂的釣魚網 和 老舊的魚叉 來召喚稀有.'
+L['gnarls_note'] = '和 {npc:197344}, {npc:197353}, {npc:197356} 同時出現'
+L['high_shaman_rotknuckle_note'] = '和 {npc:197344}, {npc:197353}, {npc:197354} 同時出現'
+L['snarglebone_note'] = '和 {npc:197353}, {npc:197356}, {npc:197354} 同時出現'
+L['trilvarus_loreweaver_note'] = '收集一個歌唱碎片來取得 {spell:382076} 後使用 Uncharded Focus 來召喚稀有.'
 
-L['forgotten_jewel_box_note'] = nil
-L['gnoll_fiend_flail_note'] = nil
+L['forgotten_jewel_box_note'] = '從雍亞拉平原的嶺水靜地 (49.4 67.3) 上的 氏族寶箱 拾取{item:199065} 並使用它.'
+L['gnoll_fiend_flail_note'] = '{item:199066} 可以在遠征隊斥侯包和挖過的土中找到.'
 
 -------------------------------------------------------------------------------
 ------------------------------- FORBIDDEN REACH -------------------------------
@@ -55,39 +55,47 @@ L['mysterious_wand_note'] = nil
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
 -------------------------------------------------------------------------------
 
-L['shade_of_grief_note'] = nil
+-- {quest:65901} = Sneaking Out
+-- {currency:2003} = Dragon Isle Supplies
+-- {item:192615} = Flourescent Fluid
+-- {item:192658} = High-Fiber Leaf
+-- {item:194966} = Thousandbite Piranha
+-- {item:192636} = Woolly Mountain Pelt
+-- {item:200598} = Meluun's Green Curry
+-- {npc:190015} = Ohn Meluun
+L['shade_of_grief_note'] = '點選 {npc:193166} 來召喚稀有.'
 
-L['nokhud_warspear_note'] = nil
-L['slightly_chewed_duck_egg_note'] = nil
-L['yennus_boat'] = nil
-L['yennus_boat_note'] = nil
+L['nokhud_warspear_note'] = '{item:194540} 可以在遠征隊斥侯包和挖過的土中找到.'
+L['slightly_chewed_duck_egg_note'] = '找到並撫摸 {npc:192997} 來取得 {item:195453} 後再使用它. {item:199171} 將在3天後孵化成 {item:199172}.'
+L['yennus_boat'] = '巨牙海民玩具船'
+L['yennus_boat_note'] = '拾取巨牙海民玩具船來取得 {item:200876}, 其可以開始任務 {quest:72063}, 可以向 {npc:195252} 回報.'
 
-L['lizi_note'] = nil
-L['lizi_note_day1'] = nil
-L['lizi_note_day2'] = nil
-L['lizi_note_day3'] = nil
-L['lizi_note_day4'] = nil
-L['lizi_note_day5'] = nil
+L['lizi_note'] = '完成從 {quest:65901} 開始的 Initiate\'s Day Out 故事線. 每天完成任務來修補莉茲並取得你的坐騎.\n\n除了任務需要的物品外, 所有任務都需要150個 {currency:2003}, 總共需要750個 {currency:2003}.'
+L['lizi_note_day1'] = '從 |cFFFFFD00巨龍群島|r 的昆蟲怪物收集20個 {item:192615}'
+L['lizi_note_day2'] = '從 |cFFFFFD00巨龍群島|r 的植物怪物收集10個 {item:192658}'
+L['lizi_note_day3'] = '從 |cFFFFFD00巨龍群島|r 的任意水域釣到10個 {item:194966}. 常見於 |cFFFFFD00雍亞拉平原|r 的內陸'
+L['lizi_note_day4'] = '從 |cFFFFFD00雍亞拉平原|r 的猛瑪象收集20個 {item:192636}'
+L['lizi_note_day5'] = '從 |cFFFFFD00雍伊爾溫泉|r 南邊帳篷內的 {npc:190015} 購買1個 {item:200598}'
 
 -------------------------------------------------------------------------------
 --------------------------------- THALDRASZUS ---------------------------------
 -------------------------------------------------------------------------------
 
-L['acorn_harvester_note'] = nil
-L['cracked_hourglass_note'] = nil
-L['sandy_wooden_duck_note'] = nil
+L['acorn_harvester_note'] = '在附近的地上收集一個橡實來取得 {spell:388485} 之後再和 {npc:196172} 互動.'
+L['cracked_hourglass_note'] = '{item:199068} 可以在遠征隊斥侯包和挖過的土中找到.'
+L['sandy_wooden_duck_note'] = '收集 {item:199069} 並使用它.'
 
 -------------------------------------------------------------------------------
 ------------------------------ THE WAKING SHORE -------------------------------
 -------------------------------------------------------------------------------
 
-L['brundin_the_dragonbane_note'] = nil
-L['shasith_note'] = nil
+L['brundin_the_dragonbane_note'] = '喀拉希戰爭隊伍坐在 {npc:192737} 旅行到這座塔.'
+L['shasith_note'] = '在黑曜王座內. \n\n需要更多的調查'
 
-L['bubble_drifter_note'] = nil
-L['onyx_gem_cluster_note'] = nil
-L['replica_dragon_goblet_note'] = nil
+L['bubble_drifter_note'] = '{item:199061} 可以在遠征隊斥侯包和挖過的土中找到.'
+L['onyx_gem_cluster_note'] = '當龍鱗遠征隊的名望到達21時, 從 {npc:189065} 用3個 {item:192863} 和 500{currency:2003} 購買 {item:200738} 並使用它.'
+L['replica_dragon_goblet_note'] = '和翼息大使館裡的 {npc:193915} 購買 {item:198854} 並使用它.'
 
-L['fullsails_supply_chest'] = nil
+L['fullsails_supply_chest'] = '滿帆補給箱'
 L['hidden_hornswog_hoard'] = '隱藏的角蛙寶箱'
-L['hidden_hornswog_hoard_note'] = nil
+L['hidden_hornswog_hoard_note'] = '收集 {item:200064}, {item:200065} 和 {item:200066}, 在寶藏附近的 "觀察謎題: A Field Guide" 組合成 {item:200063} 並且餵給 {npc:192362}.'
