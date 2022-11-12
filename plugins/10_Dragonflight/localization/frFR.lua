e9b2423a6b646841c1cc52fdf3f66673edcfc2d9
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-11-13 00:13:25 +0100

    Update frFR.lua - Dragonflight

--- plugins/10_Dragonflight/localization/frFR.lua
+++ plugins/10_Dragonflight/localization/frFR.lua
@@ -32,15 +32,15 @@ L['options_icons_scout_pack_desc'] = 'Affichez les emplacements des Paquetages d
 -------------------------------------------------------------------------------
 
 L['blisterhide_note'] = 'Apparaît simultanément avec {npc:197344}, {npc:197356} et {npc:197354}'
-L['fisherman_tinnak_note'] = 'Récupérez la Canne à pêche cassée, le Filet de pêche déchiré et le Vieux harpon pour faire apparaître le rare.'
+L['fisherman_tinnak_note'] = 'Récupérez la |cFFFFFD00Canne à pêche cassée|r, le |cFFFFFD00Filet de pêche déchiré|r et le |cFFFFFD00Vieux harpon|r pour faire apparaître le rare.'
 L['gnarls_note'] = 'Apparaît simultanément avec {npc:197344}, {npc:197353} et {npc:197356}'
 L['high_shaman_rotknuckle_note'] = 'Apparaît simultanément avec {npc:197344}, {npc:197353} et {npc:197354}'
 L['snarglebone_note'] = 'Apparaît simultanément avec {npc:197353}, {npc:197356} et {npc:197354}'
-L['trilvarus_loreweaver_note'] = 'Récupérez un Fragment chantant pour obtenir {spell:382076} et utilisez le Focalisateur déchargé pour faire apparaître le rare.'
+L['trilvarus_loreweaver_note'] = 'Récupérez un |cFFFFFD00Fragment chantant|r pour obtenir {spell:382076} et utilisez le |cFFFFFD00Focalisateur déchargé|r pour faire apparaître le rare.'
 
 L['forgotten_jewel_box_note'] = 'Récupérez la {item:199065} dans un Coffre clanique au Refuge d\'Ondecrête (49.4 67.3) dans les Plaines d\'Ohn\'ahra et utilisez-la.'
 L['gnoll_fiend_flail_note'] = '{item:199066} peut être trouvé dans les Paquetages d\'éclaireurs de l\'expédition et les Poussières dérangées.'
-L['pepper_hammer_note'] = nil
+L['pepper_hammer_note'] = 'Récupérez la |cFFFFFD00Sève|r et utilisez le |cFFFFFD00Bâton|r pour attirer le {npc:195373}.\n\n|cFFFF0000(BUG : Pour cliquer sur le bâton, un rechargement peut être nécessaire)|r'
 
 -------------------------------------------------------------------------------
 ------------------------------- FORBIDDEN REACH -------------------------------
@@ -51,7 +51,7 @@ L['bag_of_enchanted_wind_note'] = 'Situé en haut de la tour.'
 L['hessethiash_treasure'] = 'Coffre au trésor mal caché d\'Hessethiash'
 L['lost_draconic_hourglass'] = 'Sablier draconique perdu'
 L['mysterious_wand'] = 'Baguette mystérieuse'
-L['mysterious_wand_note'] = 'Ramassez la Clé en cristal et placez-la dans le Focalisateur en cristal.'
+L['mysterious_wand_note'] = 'Ramassez la |cFFFFFD00Clé en cristal|r et placez-la dans le |cFFFFFD00Focalisateur en cristal|r.'
 
 -------------------------------------------------------------------------------
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
@@ -59,11 +59,11 @@ L['mysterious_wand_note'] = 'Ramassez la Clé en cristal et placez-la dans le Fo
 
 L['shade_of_grief_note'] = 'Cliquez sur la {npc:193166} pour faire apparaître le rare.'
 
-L['gold_swong_coin_note'] = nil
+L['gold_swong_coin_note'] = 'Dans la grotte avec {npc:191608} à sa droite.'
 L['nokhud_warspear_note'] = '{item:194540} peut être trouvé dans les Paquetages d\'éclaireurs de l\'expédition et les Poussières dérangées.'
 L['slightly_chewed_duck_egg_note'] = 'Trouvez et caressez {npc:192997} pour obtenir une {item:195453} puis utilisez-la. l\'{item:199171} incube pendant 3 jours pour devenir {item:199172}.'
 L['yennus_boat'] = 'Bateau jouet rohart'
-L['yennus_boat_note'] = 'Récupérez le Bateau jouet rohart pour obtenir {item:200876}, ce qui lance la quête {quest:72063} qui peut être rendue à {npc:195252}.'
+L['yennus_boat_note'] = 'Récupérez le |cFFFFFD00Bateau jouet rohart|r pour obtenir {item:200876}, ce qui lance la quête {quest:72063} qui peut être rendue à {npc:195252}.'
 
 L['lizi_note'] = 'Terminez la suite de quêtes Une sortie d’initié en commençant par {quest:65901}. Accomplissez les quêtes chaque jour pour soigner Lizi et recevoir votre monture.\n\nEn plus des objets requis, toutes les quêtes demandent 150x {currency:2003} pour un total de 750x {currency:2003}.'
 L['lizi_note_day1'] = 'Collectez 20x {item:192615} sur des monstres insectes dans les |cFFFFFD00Îles aux Dragons|r'
@@ -72,17 +72,17 @@ L['lizi_note_day3'] = 'Collectez 10x {item:194966} pêchés dans n\'importe quel
 L['lizi_note_day4'] = 'Collectez 20x {item:192636} sur des mammouths dans les |cFFFFFD00Plaines d\'Ohn\'ahra|r'
 L['lizi_note_day5'] = 'Achetez 1x {item:200598} à {npc:190015} dans une tente au sud de |cFFFFFD00Sources des Ohn\'irs|r'
 
-L['ohnahra_note_start'] = nil
-L['ohnahra_note_item1'] = nil
-L['ohnahra_note_item2'] = nil
-L['ohnahra_note_item3'] = nil
-L['ohnahra_note_end'] = nil
+L['ohnahra_note_start'] = 'Terminez la suite de quêtes Une sortie d’initié commençant par {quest:65901} puis {npc:190022} apparaîtra aux |cFFFFFD00Sources des Ohn\'irs|r derrière une hutte de Sage-du-vent.\n\nRassemblez les matériaux suivants :'
+L['ohnahra_note_item1'] = 'Récupérez 3x {item:201929} sur {npc:186151}, le boss final du donjon |cFFFFFD00L\'offensive nokhud|r (difficulté héroïque). Ce n\'est pas un drop garanti à 100%.'
+L['ohnahra_note_item2'] = 'Achetez 1x {item:201323} à l\'{npc:196707} pour 50x {currency:2003} et 1x {item:194562}.\nLe {item:194562} peut être récupéré sur des monstres perdus dans le temps à |cFFFFFD00Thaldraszus|r.'
+L['ohnahra_note_item3'] = 'Achetez 1x {item:191507} à l\'Hôtel des Ventes. (Les alchimistes peuvent acheter la {item:191588} à l\'{npc:196707} à partir du Renom 22)'
+L['ohnahra_note_end'] = 'Une fois que vous avez tous les matériaux, revenez voir {npc:190022} et acceptez {quest:72512}. Allez voir {npc:194796} pour rendre la quête et recevoir votre monture.'
 
 -------------------------------------------------------------------------------
 --------------------------------- THALDRASZUS ---------------------------------
 -------------------------------------------------------------------------------
 
-L['acorn_harvester_note'] = 'Ramassez un Gland sur le sol à proximité pour obtenir {spell:388485} et interagissez ensuite avec {npc:196172}.'
+L['acorn_harvester_note'] = 'Ramassez un |cFFFFFD00Gland|r sur le sol à proximité pour obtenir {spell:388485} et interagissez ensuite avec {npc:196172}.'
 L['cracked_hourglass_note'] = '{item:199068} peut être trouvé dans les Paquetages d\'éclaireurs de l\'expédition et les Poussières dérangées.'
 L['sandy_wooden_duck_note'] = 'Récupérez {item:199069} et utilisez-la.'
 
@@ -90,13 +90,13 @@ L['sandy_wooden_duck_note'] = 'Récupérez {item:199069} et utilisez-la.'
 ------------------------------ THE WAKING SHORE -------------------------------
 -------------------------------------------------------------------------------
 
-L['brundin_the_dragonbane_note'] = 'La Troupe Qalashi voyage sur leur {npc:192737} jusqu\'à cette Tour.'
-L['shasith_note'] = 'à l\'intérieur du Trône d\'obsidienne. \n\nNÉCESSITE PLUS DE RECHERCHES'
+L['brundin_the_dragonbane_note'] = 'La Troupe Qalashi voyage sur leur {npc:192737} jusqu\'à cette tour.'
+L['shasith_note'] = 'À l\'intérieur du |cFFFFFD00Trône d\'obsidienne|r. \n\nVous et d\'autres joueurs devez rapporter un total de 20x {item:191264}. Pour fabriquer une clé, vous devez combiner 30x {item:191251} et 3x {item:193201}. Vous pouvez obtenir ces objets sur les monstres de la Citadelle d\'obsidienne.'
 
 L['bubble_drifter_note'] = '{item:199061} peut être trouvé dans les Paquetages d\'éclaireurs de l\'expédition et les Poussières dérangées.'
 L['onyx_gem_cluster_note'] = 'Achetez une {item:200738} à {npc:189065} pour 3 {item:192863} et 500 {currency:2003} au Renom 21 avec l\'Expédition du Dracaret et utilisez-la.'
-L['replica_dragon_goblet_note'] = 'Achetez une {item:198854} à {npc:193915} à l\'Ambassade de Trêve-Ailée et utilisez-la.'
+L['replica_dragon_goblet_note'] = 'Achetez une {item:198854} à {npc:193915} à l\'|cFFFFFD00Ambassade de Trêve-Ailée|r et utilisez-la.'
 
 L['fullsails_supply_chest'] = 'Clé de coffre de fournitures des Gréements'
 L['hidden_hornswog_hoard'] = 'Horde de bufflaciens cachée'
-L['hidden_hornswog_hoard_note'] = 'Collectez {item:200064}, {item:200065} et {item:200066}, combinez-les à "l\'Énigmes sagaces : un guide de poche" près du trésor puis nourrissez la grenouille.'
+L['hidden_hornswog_hoard_note'] = 'Collectez {item:200064}, {item:200065} et {item:200066}, combinez-les à |cFFFFFD00"l\'Énigmes sagaces : un guide de poche"|r près du trésor puis nourrissez la grenouille.'
