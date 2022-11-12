5ccd4bffe0e4a3c63f496c1ee5a64b8651ea042c
Dathwada <dathwada@gmail.com>
2022-11-12 21:47:57 +0100

    Update enUS and deDE

--- plugins/10_Dragonflight/localization/enUS.lua
+++ plugins/10_Dragonflight/localization/enUS.lua
@@ -31,11 +31,11 @@ L['options_icons_scout_pack_desc'] = 'Display the location of Expedition Scout\'
 -------------------------------------------------------------------------------
 
 L['blisterhide_note'] = 'Appears simultaneously with {npc:197344}, {npc:197356} and {npc:197354}'
-L['fisherman_tinnak_note'] = 'Collect Broken Fishing Pole, Torn Fishing Net and Old Harpoon to spawn the rare.'
+L['fisherman_tinnak_note'] = 'Collect |cFFFFFD00Broken Fishing Pole|r, |cFFFFFD00Torn Fishing Net|r and |cFFFFFD00Old Harpoon|r to spawn the rare.'
 L['gnarls_note'] = 'Appears simultaneously with {npc:197344}, {npc:197353} and {npc:197356}'
 L['high_shaman_rotknuckle_note'] = 'Appears simultaneously with {npc:197344}, {npc:197353} and {npc:197354}'
 L['snarglebone_note'] = 'Appears simultaneously with {npc:197353}, {npc:197356} and {npc:197354}'
-L['trilvarus_loreweaver_note'] = 'Collect a Singing Fragment to get {spell:382076} and use the Uncharded Focus to spawn the rare.'
+L['trilvarus_loreweaver_note'] = 'Collect a |cFFFFFD00Singing Fragment|r to get {spell:382076} and use the |cFFFFFD00Uncharded Focus|r to spawn the rare.'
 
 L['forgotten_jewel_box_note'] = 'Loot {item:199065} from a Clan Chest in Ridgewater Retreat (49.4 67.3) on the Ohn\'ahran Plains and use it.'
 L['gnoll_fiend_flail_note'] = '{item:199066} can be found in Expedition Scout\'s Packs and Disturbed Dirts.'
@@ -49,7 +49,7 @@ L['bag_of_enchanted_wind_note'] = 'Located up in the tower.'
 L['hessethiash_treasure'] = 'Hessethiash\'s Poorly Hidden Treasure'
 L['lost_draconic_hourglass'] = 'Lost Draconic Hourglass'
 L['mysterious_wand'] = 'Mysterious Wand'
-L['mysterious_wand_note'] = 'Pick up the Crystal Key and place it into the Crystal Focus.'
+L['mysterious_wand_note'] = 'Pick up the |cFFFFFD00Crystal Key|r and place it into the |cFFFFFD00Crystal Focus|r.'
 
 -------------------------------------------------------------------------------
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
@@ -68,7 +68,7 @@ L['shade_of_grief_note'] = 'Click the {npc:193166} to spawn the rare.'
 L['nokhud_warspear_note'] = '{item:194540} can be found in Expedition Scout\'s Packs and Disturbed Dirts.'
 L['slightly_chewed_duck_egg_note'] = 'Find and pet {npc:192997} to get {item:195453} then use it. {item:199171} incubates in 3 days into {item:199172}.'
 L['yennus_boat'] = 'Tuskarr Toy Boat'
-L['yennus_boat_note'] = 'Loot the Tuskarr Toy Boat to get {item:200876}, which starts the quest {quest:72063} that can be turned in at {npc:195252}.'
+L['yennus_boat_note'] = 'Loot the |cFFFFFD00Tuskarr Toy Boat|r to get {item:200876}, which starts the quest {quest:72063} that can be turned in at {npc:195252}.'
 
 L['lizi_note'] = 'Complete the Initiate\'s Day Out storyline starting with {quest:65901}. Complete the quests each day to mend Lizi and receive your mount.\n\nIn addition to items required all quests require 150x {currency:2003} for a total of 750x {currency:2003}.'
 L['lizi_note_day1'] = 'Collect 20x {item:192615} from insect mobs in the |cFFFFFD00Dragon Isles|r'
@@ -87,7 +87,7 @@ L['ohnahra_note_end'] = 'Once you have all materials, return to {npc:190022} and
 --------------------------------- THALDRASZUS ---------------------------------
 -------------------------------------------------------------------------------
 
-L['acorn_harvester_note'] = 'Collect an Acorn from the ground nearby to get {spell:388485} and then interact with {npc:196172}.'
+L['acorn_harvester_note'] = 'Collect an |cFFFFFD00Acorn|r from the ground nearby to get {spell:388485} and then interact with {npc:196172}.'
 L['cracked_hourglass_note'] = '{item:199068} can be found in Expedition Scout\'s Packs and Disturbed Dirts.'
 L['sandy_wooden_duck_note'] = 'Collect {item:199069} and use it.'
 
@@ -95,13 +95,13 @@ L['sandy_wooden_duck_note'] = 'Collect {item:199069} and use it.'
 ------------------------------ THE WAKING SHORE -------------------------------
 -------------------------------------------------------------------------------
 
-L['brundin_the_dragonbane_note'] = 'The Qalashi War Party travels on there {npc:192737} to this Tower.'
-L['shasith_note'] = 'inside the Obsidian Throne. \n\nNEEDS MORE INVESTIGATION'
+L['brundin_the_dragonbane_note'] = 'The Qalashi War Party travels on there {npc:192737} to this tower.'
+L['shasith_note'] = 'Inside the |cFFFFFD00Obsidian Throne|r. \n\nYou and other Players have to return a total of 20x {item:191264}. To craft a key you need to combine 30x {item:191251} and 3x {item:193201}, you can get these items from Obsidian Citadel Mobs.'
 
 L['bubble_drifter_note'] = '{item:199061} can be found in Expedition Scout\'s Packs and Disturbed Dirts.'
 L['onyx_gem_cluster_note'] = 'Buy {item:200738} from {npc:189065} for 3 {item:192863} and 500 {currency:2003} at Renown 21 with the Dragonscale Expedition and use it.'
-L['replica_dragon_goblet_note'] = 'Buy {item:198854} from {npc:193915} in Wingrest Embassy and use it.'
+L['replica_dragon_goblet_note'] = 'Buy {item:198854} from {npc:193915} in |cFFFFFD00Wingrest Embassy|r and use it.'
 
 L['fullsails_supply_chest'] = 'Fullsails Supply Chest'
 L['hidden_hornswog_hoard'] = 'Hidden Hornswog Hoard'
-L['hidden_hornswog_hoard_note'] = 'Collect {item:200064}, {item:200065} and {item:200066}, combine them at the "Observant Riddles: A Field Guide" near the treasure then feed the frog.'
+L['hidden_hornswog_hoard_note'] = 'Collect {item:200064}, {item:200065} and {item:200066}, combine them at the |cFFFFFD00"Observant Riddles: A Field Guide"|r near the treasure then feed the frog.'
