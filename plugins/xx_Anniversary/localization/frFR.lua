a0c3bfbbdf10cccddcc1163489b7065a5cfbcd4a
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-11-12 10:55:58 +0100

    Update frFR.Lua - Anniversary

--- plugins/xx_Anniversary/localization/frFR.lua
+++ plugins/xx_Anniversary/localization/frFR.lua
@@ -1,6 +1,7 @@
+-- French translation by Lightuky EU-Khaz Modan
 local ADDON_NAME, ns = ...
 local L = ns.NewLocale('frFR')
 if not L then return end
 
-L['anniversary'] = nil
-L['shared_drop_note'] = nil
+L['anniversary'] = 'Anniversaire'
+L['shared_drop_note'] = 'Drop partagé'
