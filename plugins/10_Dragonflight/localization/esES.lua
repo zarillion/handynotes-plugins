590aa166baf13ac854a299a4d6bf1c801e40db26
Krovikan <krovikan@gmail.com>
2022-11-13 12:14:52 +0100

    Updated and formated DF esES

--- plugins/10_Dragonflight/localization/esES.lua
+++ plugins/10_Dragonflight/localization/esES.lua
@@ -32,15 +32,15 @@ L['options_icons_scout_pack_desc'] = 'Muestra dónde están las Mochilas de Expl
 -------------------------------------------------------------------------------
 
 L['blisterhide_note'] = 'Aparece simultáneamente con {npc:197344}, {npc:197356} y {npc:197354}.'
-L['fisherman_tinnak_note'] = 'Recoge la Caña de Pescar Rota, Red de Pescar Rota y Arpón Viejo para hacer aparecer al raro.'
+L['fisherman_tinnak_note'] = 'Recoge la |cFFFFFD00Caña de Pescar Rota|r, la |cFFFFFD00Red de Pescar Rota|r y el |cFFFFFD00Arpón Viejo|r para hacer aparecer al raro.'
 L['gnarls_note'] = 'Aparece simultáneamente con {npc:197344}, {npc:197353} y {npc:197356}.'
 L['high_shaman_rotknuckle_note'] = 'Aparece simultáneamente con {npc:197344}, {npc:197353} y {npc:197354}.'
 L['snarglebone_note'] = 'Aparece simultáneamente con {npc:197353}, {npc:197356} y {npc:197354}.'
-L['trilvarus_loreweaver_note'] = 'Recoge Fragmentos Cantarines para conseguir {spell:382076} y usar el Enfoque Descargado para hacer aparecer al raro.'
+L['trilvarus_loreweaver_note'] = 'Recoge un |cFFFFFD00Fragmento Cantarín|r para conseguir {spell:382076} y usar el |cFFFFFD00Enfoque Descargado|r para hacer aparecer al raro.'
 
 L['forgotten_jewel_box_note'] = 'Despoja la {item:199065} de un Cofre del Clan en Retiro Crestagua (49.4 67.3) en las Llanuras de Ohn\'ahran y úsala.'
 L['gnoll_fiend_flail_note'] = 'Las {item:199066} se pueden encontrar en las Mochilas de Explorador de Expedición y en las Tierras Perturbadas.'
-L['pepper_hammer_note'] = nil
+L['pepper_hammer_note'] = 'Recoge |cFFFFFD00Savia de Árbol|r y entonces usa el |cFFFFFD00Palo|r para engañar a {npc:195373}.\n\n|cFFFF0000(BUG: Para hacer click en el palo puede ser necesario recargar)|r'
 
 -------------------------------------------------------------------------------
 ------------------------------- FORBIDDEN REACH -------------------------------
@@ -51,7 +51,7 @@ L['bag_of_enchanted_wind_note'] = 'Está en lo alto de la torre.'
 L['hessethiash_treasure'] = 'Tesoro Mal Escondido de Hessethiash'
 L['lost_draconic_hourglass'] = 'Reloj de Arena Dracónico Perdido'
 L['mysterious_wand'] = 'Varita Misteriosa'
-L['mysterious_wand_note'] = 'Coge la Llave de Cristal y ponla dentro del Foco de Cristal.'
+L['mysterious_wand_note'] = 'Coge la |cFFFFFD00Llave de Cristal|r y ponla dentro del |cFFFFFD00Foco de Cristal|r.'
 
 -------------------------------------------------------------------------------
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
@@ -67,11 +67,11 @@ L['mysterious_wand_note'] = 'Coge la Llave de Cristal y ponla dentro del Foco de
 -- {npc:190015} = Ohn Meluun
 L['shade_of_grief_note'] = 'Haz click en {npc:193166} para hacer aparecer al raro.'
 
-L['gold_swong_coin_note'] = nil
+L['gold_swong_coin_note'] = 'Dentro de la cueva con {npc:191608} a su lado derecho.'
 L['nokhud_warspear_note'] = 'Las {item:194540} se pueden encontrar en las Mochilas de Explorador de Expedición y en las Tierras Perturbadas.'
 L['slightly_chewed_duck_egg_note'] = 'Encuentra y acaricia a {npc:192997} para conseguir el {item:195453} y entonces usarlo. Tras incubar el {item:199171} durante 3 días eclosionará en {item:199172}.'
 L['yennus_boat'] = 'Barco de Juguete Tuskarr'
-L['yennus_boat_note'] = 'Despoja el Barco de Juguete Tuskarr para conseguir el {item:200876}, el cual comienza la misión {quest:72063} para devolvérselo a {npc:195252}.'
+L['yennus_boat_note'] = 'Despoja el |cFFFFFD00Barco de Juguete Tuskarr|r para conseguir el {item:200876}, el cual comienza la misión {quest:72063} para devolvérselo a {npc:195252}.'
 
 L['lizi_note'] = 'Completa la cadena de misiones Día de Iniciación comenzando con {quest:65901}. Completa las misiones cada día para curar a Lizi y recibir su montura.\n\nAdemás de los objetos necesarios, todas las misiones requieren 150x {currency:2003} para un total de 750x {currency:2003}.'
 L['lizi_note_day1'] = 'Recoge 20x {item:192615} de los enemigos insecto en las |cFFFFFD00Islas Dragón|r.'
@@ -80,17 +80,17 @@ L['lizi_note_day3'] = 'Recoge 10x {item:194966} pescadas en cualquier masa de ag
 L['lizi_note_day4'] = 'Recoge 20x {item:192636} de los mamuts de las |cFFFFFD00Llanuras de Ohn\'ahran|r.'
 L['lizi_note_day5'] = 'Compra 1x {item:200598} a {npc:190015} en la tienda al sur de los |cFFFFFD00Manantiales Ohn\'iri|r.'
 
-L['ohnahra_note_start'] = nil
-L['ohnahra_note_item1'] = nil
-L['ohnahra_note_item2'] = nil
-L['ohnahra_note_item3'] = nil
-L['ohnahra_note_end'] = nil
+L['ohnahra_note_start'] = 'Completa la cadena de misiones Día de Iniciación comenzando con {quest:65901} y entonces aparecerá {npc:190022} en los |cFFFFFD00Manantiales Ohn\'iri|r detrás de la choza del sabio del viento.\n\nReúne los siguientes objetos:'
+L['ohnahra_note_item1'] = 'Recoge 3x {item:201929} de los {npc:186151}, el boss final de la mazmorra |cFFFFFD00Ofensiva Nokhud|r (dificultad Heroica). No tiene un 100% de caer.'
+L['ohnahra_note_item2'] = 'Compra 1x {item:201323} de {npc:196707} por 50x {currency:2003} y 1x {item:194562}.\n{item:194562} puede ser despojado de los enemigos Perdidos en el Tiempo en |cFFFFFD00Thaldrazsus|r.'
+L['ohnahra_note_item3'] = 'Compra 1x {item:191507} de la Casa de Subastas. (los Alqumistas pueden comprar la {item:191588} a {npc:196707} al llegar a Renombre 22)'
+L['ohnahra_note_end'] = 'Una vez con todos los objetos, vuelve a {npc:190022} y acepta {quest:72512}. Ves a {npc:194796} para entregar la misión y recibir tu montura.'
 
 -------------------------------------------------------------------------------
 --------------------------------- THALDRASZUS ---------------------------------
 -------------------------------------------------------------------------------
 
-L['acorn_harvester_note'] = 'Recoge una Bellota del suelo cercano para conseguir {spell:388485} y entonces interactúa con {npc:196172}.'
+L['acorn_harvester_note'] = 'Recoge una |cFFFFFD00Bellota|r del suelo cercano para conseguir {spell:388485} y entonces interactúa con {npc:196172}.'
 L['cracked_hourglass_note'] = 'La {item:199068} se puede encontrar en las Mochilas de Explorador de Expedición y en las Tierras Perturbadas.'
 L['sandy_wooden_duck_note'] = 'Recoge el {item:199069} y úsalo.'
 
@@ -99,12 +99,12 @@ L['sandy_wooden_duck_note'] = 'Recoge el {item:199069} y úsalo.'
 -------------------------------------------------------------------------------
 
 L['brundin_the_dragonbane_note'] = 'El Destacamento de Guerra Qalashi viaja encima del {npc:192737} a esta Torre.'
-L['shasith_note'] = 'Dentro del Trono Obsidiana. \n\nNECESITA MÁS INVESTIGACIÓN.'
+L['shasith_note'] = 'Dentro del |cFFFFFD00Trono Obsidiana|r. \n\nNECESITA MÁS INVESTIGACIÓN.'
 
 L['bubble_drifter_note'] = 'La {item:199061} se puede encontrar en las Mochilas de Explorador de Expedición y en las Tierras Perturbadas.'
 L['onyx_gem_cluster_note'] = 'Compra el {item:200738} a {npc:189065} por 3 {item:192863} y 500 {currency:2003} a Renombre 21 con la Expedición Dragontina y úsalo.'
-L['replica_dragon_goblet_note'] = 'Compra las {item:198854} a {npc:193915} en la Embajada Reposo Alado y úsalas.'
+L['replica_dragon_goblet_note'] = 'Compra las {item:198854} a {npc:193915} en la |cFFFFFD00Embajada Reposo Alado|r y úsalas.'
 
 L['fullsails_supply_chest'] = 'Cofre de Suministros Todavela'
 L['hidden_hornswog_hoard'] = 'Botín Oculto de los Cornisapos'
-L['hidden_hornswog_hoard_note'] = 'Recoge {item:200064}, {item:200065} y {item:200066}, combínalos en "Acertijo de observador: Una Guía de Campo" cerca del tesoro y entonces alimenta a la rana.'
+L['hidden_hornswog_hoard_note'] = 'Recoge {item:200064}, {item:200065} y {item:200066}, combínalos en |cFFFFFD00"Acertijo de observador: Una Guía de Campo"|r cerca del tesoro y entonces alimenta a la rana.'
