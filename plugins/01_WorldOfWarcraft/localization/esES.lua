aaad6e0aecb9f8ab2f701d5d927880767a758254
Krovikan <krovikan@gmail.com>
2022-11-14 01:24:59 +0100

    New file esES.lua WoW 01 esES

---
+++ plugins/01_WorldOfWarcraft/localization/esES.lua
@@ -0,0 +1,10 @@
+-- Spanish Translation by (EU) Krovikan-Minahonda
+local ADDON_NAME, ns = ...
+local L = ns.NewLocale('esES')
+if not L then return end
+
+-------------------------------------------------------------------------------
+----------------------------------- COMMON ------------------------------------
+-------------------------------------------------------------------------------
+
+L['anniversary_note'] = ns.color.Orange('¡Sólo disponible durante el eventuo anual de aniversario!')
