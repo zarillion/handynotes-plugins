c23ba00263d430aa5e916522fe83e5f33bd50d97
Dathwada <dathwada@gmail.com>
2022-11-14 18:10:12 +0100

    Add deDE (Anniversary)

---
+++ plugins/01_WorldOfWarcraft/localization/deDE.lua
@@ -0,0 +1,9 @@
+local ADDON_NAME, ns = ...
+local L = ns.NewLocale('enUS')
+if not L then return end
+-- German translation by Dathwada EU-Eredar
+-------------------------------------------------------------------------------
+----------------------------------- COMMON ------------------------------------
+-------------------------------------------------------------------------------
+
+L['anniversary_note'] = ns.color.Orange('Nur während des jährlichen Geburtstags von WoW verfügbar!')
