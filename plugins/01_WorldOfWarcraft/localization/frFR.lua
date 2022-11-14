73a248398e77e5f81ef747010ff014fa77d06654
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-11-14 08:29:38 +0100

    add anniversary file + tl

---
+++ plugins/01_WorldOfWarcraft/localization/frFR.lua
@@ -0,0 +1,10 @@
+-- French translation by Lightuky EU-Khaz Modan
+local ADDON_NAME, ns = ...
+local L = ns.NewLocale('frFR')
+if not L then return end
+
+-------------------------------------------------------------------------------
+----------------------------------- COMMON ------------------------------------
+-------------------------------------------------------------------------------
+
+L['anniversary_note'] = ns.color.Orange('Disponible uniquement lors de l\'événement anniversaire annuel !')
