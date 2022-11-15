e324c5961907aa44b365a3ac1100e406a67bfb07
Adavak <adavak@adavak.com>
2022-11-15 10:52:15 +0800

    Update common.lua

--- plugins/01_WorldOfWarcraft/common.lua
+++ plugins/01_WorldOfWarcraft/common.lua
@@ -43,7 +43,8 @@ local function IsAnniversaryEvent()
             event.title:match('WoW\'s %d%d%l%l Anniversary') or -- enUS
             event.title:match('%d%d. Geburtstag von WoW') or -- deDE
             event.title:match('%d%de anniversaire de Wo') or -- frFR
-            event.title:match('%d%d-я годовщина Wo') -- ruRU
+            event.title:match('%d%d-я годовщина Wo') or -- ruRU
+            event.title:match('魔兽世界') and event.title:match('周年') -- zhCN 魔兽世界十八周年
             -- additional language help needed!
         ) then return true end
         -- LuaFormatter on
