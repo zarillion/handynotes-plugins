b0f9fe177c68aa4bf4463b81841eb936f214c349
Wyldclaw <wyldclawthornbringer@gmail.com>
2022-11-17 17:19:19 -0700

    Add ALL Wowhead nodes

--- plugins/02_BurningCrusade/zones/nagrand.lua
+++ plugins/02_BurningCrusade/zones/nagrand.lua
@@ -20,7 +20,7 @@ local map = Map({id = 107, settings = true})
 ------------------------------------ RARES ------------------------------------
 -------------------------------------------------------------------------------
 
-map.nodes[35404720] = Rare({
+map.nodes[76207800] = Rare({
     id = 17144,
     rewards = {
         Achievement({id = 1312, criteria = 4513}), -- Bloody Rare
@@ -29,10 +29,15 @@ map.nodes[35404720] = Rare({
         Transmog({item = 31189, slot = L['mail']}), -- Crocolisk Scale Wristguards
         Transmog({item = 31188, slot = L['plate']}) -- Crocolisk Scale Vambraces
     },
-    pois = {POI({42804780, 42204000, 58802880, 76607880})}
+    pois = {
+        POI({
+            41404140, 44004700, 44804040, 58202780, 74607600, 75807560,
+            76007960, 76008080
+        })
+    }
 }) -- Goretooth
 
-map.nodes[35806560] = Rare({
+map.nodes[39607280] = Rare({
     id = 18683,
     rewards = {
         Achievement({id = 1312, criteria = 4522}), -- Bloody Rare
@@ -41,5 +46,13 @@ map.nodes[35806560] = Rare({
         Transmog({item = 31197, slot = L['mail']}), -- Voidscale Belt
         Transmog({item = 31195, slot = L['plate']}) -- Voidplate Girdle
     },
-    pois = {POI({34607680, 32607160, 39807100})}
+    pois = {
+        POI({
+            32207040, 32207100, 32207160, 32407320, 32606960, 32607360,
+            32806880, 33007460, 33206780, 33406740, 33606700, 33607580,
+            34206620, 34606600, 35007680, 35806560, 36006540, 36207700,
+            36807660, 37206580, 37606600, 38207600, 39006820, 39007440,
+            39007540, 39406880, 39407320, 39607080, 39607200
+        })
+    }
 }) -- Voidhunter Yar
