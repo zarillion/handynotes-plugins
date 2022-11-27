1ea7da6c608519cf6c2c551c077619332291e759
Soaa Kim <soaa12@gmail.com>
2022-11-27 18:49:54 +0900

    updating ko translation

---
+++ plugins/10_Dragonflight/localization/koKR.lua
@@ -0,0 +1,111 @@
+local ADDON_NAME, ns = ...
+local L = ns.NewLocale('koKR')
+if not L then return end
+
+-------------------------------------------------------------------------------
+-------------------------------- DRAGON ISLES ---------------------------------
+-------------------------------------------------------------------------------
+
+L['dragon_glyph'] = '용 문양'
+L['options_icons_dragon_glyph'] = '용 문양'
+L['options_icons_dragon_glyph_desc'] = '48개의 전체 용 문양 위치를 표시합니다.'
+
+L['dragonscale_expedition_flag'] = '용비늘 원정대 깃발'
+L['flags_placed'] = '깃발 꽂기 완료'
+L['options_icons_flag'] = '{achievement:15890}'
+L['options_icons_flag_desc'] = '{achievement:15890} 업적을 위한 전체 20개의 깃발 꽂기 위치를 표시합니다.'
+
+L['options_icons_kite'] = '{achievement:16584}'
+L['options_icons_kite_desc'] = '{achievement:16584} 업적을 위한 {npc:198118} 위치를 표시합니다.'
+
+L['disturbed_dirt'] = '흐트러진 흙'
+L['options_icons_disturbed_dirt'] = '흐트러진 흙'
+L['options_icons_disturbed_dirt_desc'] = '흐트러진 흙의 위치를 표시합니다.'
+
+L['scout_pack'] = nil
+L['options_icons_scout_pack'] = nil
+L['options_icons_scout_pack_desc'] = nil
+
+-------------------------------------------------------------------------------
+------------------------------- THE AZURE SPAN --------------------------------
+-------------------------------------------------------------------------------
+
+L['blisterhide_note'] = '{npc:197344}, {npc:197356}, {npc:197354}와 동시에 나타납니다.'
+L['fisherman_tinnak_note'] = nil
+L['gnarls_note'] = '{npc:197344}, {npc:197353}, {npc:197356}와 동시에 나타납니다.'
+L['high_shaman_rotknuckle_note'] = '{npc:197344}, {npc:197353}, {npc:197354}와 동시에 나타납니다.'
+L['snarglebone_note'] = '{npc:197353}, {npc:197356}, {npc:197354}와 동시에 나타납니다.'
+L['trilvarus_loreweaver_note'] = nil
+
+L['forgotten_jewel_box_note'] = nil
+L['gnoll_fiend_flail_note'] = nil
+L['pepper_hammer_note'] = nil
+
+L['leyline_note'] = nil
+L['options_icons_layline'] = '{achievement:16638}'
+L['options_icons_layline_desc'] = nil
+
+-------------------------------------------------------------------------------
+------------------------------- FORBIDDEN REACH -------------------------------
+-------------------------------------------------------------------------------
+
+L['bag_of_enchanted_wind'] = nil
+L['bag_of_enchanted_wind_note'] = nil
+L['hessethiash_treasure'] = nil
+L['lost_draconic_hourglass'] = nil
+L['mysterious_wand'] = nil
+L['mysterious_wand_note'] = nil
+
+-------------------------------------------------------------------------------
+------------------------------ OHN'AHRAN PLAINS -------------------------------
+-------------------------------------------------------------------------------
+
+L['shade_of_grief_note'] = nil
+
+L['gold_swong_coin_note'] = nil
+L['nokhud_warspear_note'] = nil
+L['slightly_chewed_duck_egg_note'] = nil
+L['yennus_boat'] = nil
+L['yennus_boat_note'] = nil
+
+L['lizi_note'] = nil
+L['lizi_note_day1'] = nil
+L['lizi_note_day2'] = nil
+L['lizi_note_day3'] = nil
+L['lizi_note_day4'] = nil
+L['lizi_note_day5'] = nil
+
+L['ohnahra_note_start'] = nil
+L['ohnahra_note_item1'] = nil
+L['ohnahra_note_item2'] = nil
+L['ohnahra_note_item3'] = nil
+L['ohnahra_note_end'] = nil
+
+L['bakar_note'] = nil
+L['bakar_ellam_note'] = nil
+L['bakar_hugo_note'] = nil
+L['options_icons_bakar'] = '{achievement:16424}'
+L['options_icons_bakar_desc'] = nil
+
+-------------------------------------------------------------------------------
+--------------------------------- THALDRASZUS ---------------------------------
+-------------------------------------------------------------------------------
+
+L['acorn_harvester_note'] = nil
+L['cracked_hourglass_note'] = nil
+L['sandy_wooden_duck_note'] = nil
+
+-------------------------------------------------------------------------------
+------------------------------ THE WAKING SHORE -------------------------------
+-------------------------------------------------------------------------------
+
+L['brundin_the_dragonbane_note'] = nil
+L['shasith_note'] = nil
+
+L['bubble_drifter_note'] = nil
+L['onyx_gem_cluster_note'] = nil
+L['replica_dragon_goblet_note'] = nil
+
+L['fullsails_supply_chest'] = nil
+L['hidden_hornswog_hoard'] = '숨겨진 용뿔두꺼비 비축물'
+L['hidden_hornswog_hoard_note'] = nil
