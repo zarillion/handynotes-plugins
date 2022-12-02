6a441c3c58a3ba8897b6eaf27b2c0f91328d3cb9
Ioney <dominik.hier@gmail.com>
2022-12-02 01:46:19 +0100

    Dash Races
    chagned formatting for dash races

--- plugins/10_Dragonflight/localization/frFR.lua
+++ plugins/10_Dragonflight/localization/frFR.lua
@@ -33,7 +33,9 @@ L['options_icons_scout_pack'] = 'Paquetages d\'éclaireurs de l\'expédition'
 L['options_icons_scout_pack_desc'] = 'Affichez les emplacements des Paquetages d\'éclaireurs de l\'expédition.'
 
 L['dr_best'] = nil
+L['dr_best_dash'] = nil
 L['dr_note'] = nil
+L['dr_note_dash'] = nil
 L['dr_bronze'] = nil
 L['options_icons_dragonrace'] = nil
 L['options_icons_dragonrace_desc'] = nil
