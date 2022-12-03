2735c577ee26726fe936052bdfeea038cdf38aa0
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-12-03 21:51:51 +0100

    Update frFR.lua - snowman - DF

--- plugins/10_Dragonflight/localization/frFR.lua
+++ plugins/10_Dragonflight/localization/frFR.lua
@@ -78,8 +78,8 @@ L['leyline_note'] = 'Réalignez la ligne tellurique.'
 L['options_icons_layline'] = '{achievement:16638}'
 L['options_icons_layline_desc'] = 'Afficher les emplacements de toutes les lignes telluriques pour le haut-fait {achievement:16638}.'
 
-L['seeing_blue_note'] = 'Voler du sommet des archives d’Azur jusqu’à l’assemblée de Cobalt sans atterrir.'
-L['snowman_note'] = nil
+L['seeing_blue_note'] = 'Volez du sommet des archives d’Azur jusqu’à l’assemblée de Cobalt sans atterrir.'
+L['snowman_note'] = 'Il y a trois Boules de neige dans la zone (elles ont pu être déplacées par d\'autres joueurs), faites-les rouler vers les deux enfants {npc:197838} et {npc:197839}.\nVous obtenez le haut-fait lorsque les boules de neige ont la bonne taille.'
 
 -------------------------------------------------------------------------------
 ------------------------------- FORBIDDEN REACH -------------------------------
