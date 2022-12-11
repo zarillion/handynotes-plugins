edf58295c2a406d6a5bd63282714622583c471cb
Soaa Kim <soaa12@gmail.com>
2022-12-11 17:36:34 +0900

    .

--- plugins/10_Dragonflight/localization/koKR.lua
+++ plugins/10_Dragonflight/localization/koKR.lua
@@ -6,6 +6,14 @@ if not L then return end
 -------------------------------- DRAGON ISLES ---------------------------------
 -------------------------------------------------------------------------------
 
+L['gem_cluster_note'] = 'The required item can be found at Renown 21 with the Dragonscale Expedition in Expedition Scout\'s Packs and Disturbed Dirts.'
+
+L['options_icons_bonus_boss'] = nil
+L['options_icons_bonus_boss_desc'] = nil
+
+L['options_icons_profession_treasures'] = '전문기술 보물'
+L['options_icons_profession_treasures_desc'] = '전문기술 관련 지식을 습득할 수 있는 보물 위치를 표시합니다.'
+
 L['dragon_glyph'] = '용 문양'
 L['options_icons_dragon_glyph'] = '용 문양'
 L['options_icons_dragon_glyph_desc'] = '48개의 전체 용 문양 위치를 표시합니다.'
@@ -15,6 +23,19 @@ L['flags_placed'] = '깃발 꽂기 완료'
 L['options_icons_flag'] = '{achievement:15890}'
 L['options_icons_flag_desc'] = '{achievement:15890} 업적을 위한 전체 20개의 깃발 꽂기 위치를 표시합니다.'
 
+L['broken_banding_note'] = '석상의 오른발 발목 안쪽에 있음.'
+L['chunk_of_sculpture_note'] = 'On the ground, from the dragon statue on the left.'
+L['dislodged_dragoneye_note'] = '용 조각상 몸통 아래 잔해 위에 있음.'
+L['finely_carved_wing_note'] = '용 조각상 오른 무릎 아래 있음.'
+L['fragment_requirement_note'] = ns.color.Red('파편수집이 가능하려면 먼저 |cFFFFFD00날개쉼터 대사관|r 안의 용 조각상에서 작업중인 {npc:193914}와 대화해야 합니다.')
+L['golden_claw_note'] = 'At the dragon statue\'s rear right claw.'
+L['precious_stone_fragment_note'] = 'Under the statue\'s right foot.'
+L['stone_dragontooth_note'] = 'On the ground next to the pedestal of the dragon statue.'
+L['tail_fragment_note'] = '용 조각상 꼬리 안쪽에 있음.'
+L['wrapped_gold_band_note'] = 'Under the dragon statue\'s rear left claw.'
+L['options_icons_fragment'] = '{achievement:16323}'
+L['options_icons_fragment_desc'] = 'Display the location of loose pieces for the achievement {achievement:16323}.'
+
 L['options_icons_kite'] = '{achievement:16584}'
 L['options_icons_kite_desc'] = '{achievement:16584} 업적을 위한 {npc:198118} 위치를 표시합니다.'
 
@@ -22,9 +43,9 @@ L['disturbed_dirt'] = '흐트러진 흙'
 L['options_icons_disturbed_dirt'] = '흐트러진 흙'
 L['options_icons_disturbed_dirt_desc'] = '흐트러진 흙의 위치를 표시합니다.'
 
-L['scout_pack'] = nil
-L['options_icons_scout_pack'] = nil
-L['options_icons_scout_pack_desc'] = nil
+L['scout_pack'] = '원정대 정찰병의 짐'
+L['options_icons_scout_pack'] = '원정대 정찰병의 짐'
+L['options_icons_scout_pack_desc'] = '원정대 정찰병의 짐 위치를 표시합니다.'
 
 -------------------------------------------------------------------------------
 ------------------------------- THE AZURE SPAN --------------------------------
