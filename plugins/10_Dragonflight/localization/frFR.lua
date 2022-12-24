81db1aa528b93a5e28c8e15f059cc18fac184864
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-12-24 12:05:34 +0100

    Update frFR.lua - eaglemaster_niraak - DF

--- plugins/10_Dragonflight/localization/frFR.lua
+++ plugins/10_Dragonflight/localization/frFR.lua
@@ -163,7 +163,7 @@ L['mysterious_wand_note'] = 'Ramassez la |cFFFFFD00Clé en cristal|r et placez-l
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
 -------------------------------------------------------------------------------
 
-L['eaglemaster_niraak_note'] = nil
+L['eaglemaster_niraak_note'] = 'Tuez les {npc:186295s} et {npc:186299s} proches pour faire apparaître le rare.'
 L['hunter_of_the_deep_note'] = 'Cliquez sur le râtelier d\'armes et tirez sur les poissons jusqu\'à ce que le rare apparaisse.'
 L['scaleseeker_mezeri_note'] = 'Offrez un {item:194681} à {npc:193224} et suivez-la jusqu\'à ce qu\'elle révèle le rare.'
 L['shade_of_grief_note'] = 'Cliquez sur la {npc:193166} pour faire apparaître le rare.'
