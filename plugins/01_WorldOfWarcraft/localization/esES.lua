6ef43e6fea5f2ace52c93052e4917d1f1fc73413
Krovikan <krovikan@gmail.com>
2022-12-30 22:17:18 +0100

    Updated Summer and added tag note Vanilla esES

--- plugins/01_WorldOfWarcraft/localization/esES.lua
+++ plugins/01_WorldOfWarcraft/localization/esES.lua
@@ -7,6 +7,6 @@ if not L then return end
 ----------------------------------- COMMON ------------------------------------
 -------------------------------------------------------------------------------
 
-L['anniversary_note'] = ns.color.Orange('¡Sólo disponible durante el eventuo anual de aniversario!')
+L['anniversary_note'] = '{note:¡Sólo disponible durante el eventuo anual de aniversario!}'
 
-L['summer_only'] = nil
+L['summer_only'] = 'Aparece sólo en Verano. (Del 21 de Junio al 22 de Septiembre)'
