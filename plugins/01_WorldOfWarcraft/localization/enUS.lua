bde8c08da8522c758390d1f7ce98f0321056f57e
Krovikan <krovikan@gmail.com>
2022-12-30 22:17:22 +0100

    Fixes, added EOF and tag note Vanilla enUS

--- plugins/01_WorldOfWarcraft/localization/enUS.lua
+++ plugins/01_WorldOfWarcraft/localization/enUS.lua
@@ -6,6 +6,6 @@ if not L then return end
 ----------------------------------- COMMON ------------------------------------
 -------------------------------------------------------------------------------
 
-L['anniversary_note'] = ns.color.Orange('Only available during the yearly anniversary event!')
+L['anniversary_note'] = '{note:Only available during the yearly anniversary event!}'
 
-L['summer_only'] = 'Spawns only in the Summer. (Juni 21 until September 22)'
+L['summer_only'] = 'Spawns only in the Summer. (From June 21 until September 22)'
