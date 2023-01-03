0d5740aa195de13e85be0aec8478faa0ae84c322
Ioney <dominik.hier@gmail.com>
2023-01-03 13:47:09 +0100

    Pandaria, deDE, enUS, added missing tags

--- plugins/05_MistsOfPandaria/localization/deDE.lua
+++ plugins/05_MistsOfPandaria/localization/deDE.lua
@@ -7,21 +7,21 @@ if not L then return end
 -------------------------------- TIMELESS ISLE --------------------------------
 -------------------------------------------------------------------------------
 
-L['cavern_of_lost_spirits'] = 'In der Höhle der Verlorenen Geister.'
+L['cavern_of_lost_spirits'] = 'In der {location:Höhle der Verlorenen Geister}.'
 L['looted_twice'] = 'Du hast diesen seltenen Gegner auf diesem Charakter noch nie getötet. Er kann heute zweimal geplündert werden.'
 L['neverending_spritewood'] = 'Ewiges Geistergehölz'
-L['neverending_spritewood_note'] = 'Nach dem Zerstören des Ewigen Geistergehölzes, töte soviele {npc:Wütende Geister} wie du kannst, während du den Schwächungszauber {spell:144052} hast!' -- {npc:71824}
+L['neverending_spritewood_note'] = 'Nach dem Zerstören des {object:Ewigen Geistergehölzes}, töte soviele {npc:Wütende Geister} wie du kannst, während du den Schwächungszauber {spell:144052} hast!' -- {npc:71824}
 L['zarhym_note'] = 'Einmal am Tag kann mit {npc:71876} gesprochen werden, um die {spell:144145} zu betreten und zu versuchen seinen Körper zurückzuholen.'
 
 L['archiereus_note'] = 'Kauf eine {item:103684} von {npc:73306}, um den seltenen Gegner zu beschwören.'
-L['chelon_note'] = 'Klicke den Verdächtig leeren Panzer an.'
+L['chelon_note'] = 'Klicke den {object:Verdächtig leeren Panzer} an.'
 L['cranegnasher_note'] = 'Ziehe einen {npc:72095} aus dem Süden bis zu der Leiche eines Krachichs.'
-L['dread_ship_note'] = 'Plündere die {item:104115} von {npc:73279} und verwende diese beim Verfluchten Grabstein, um das Schreckensschiff zu beschwören.'
-L['emerald_gander_note'] = 'Töte {npc:72762} um den Hof der Erhabenen herum, bis das {npc:73158} erscheint.'
+L['dread_ship_note'] = 'Plündere die {item:104115} von {npc:73279} und verwende diese beim {object:Verfluchten Grabstein}, um das {object:Schreckensschiff} zu beschwören.'
+L['emerald_gander_note'] = 'Töte {npc:72762} um den {location:Hof der Erhabenen} herum, bis das {npc:73158} erscheint.'
 L['evermaw_note'] = 'Schwimmt im Uhrzeigersinn um die gesamte Insel herum.'
 L['great_turtle_furyshell_note'] = 'Töte {npc:72764} oder {npc:72763}, bis die {npc:73161} erscheint.'
-L['imperial_python_note'] = 'Töte {npc:72841} um den Hof der Erhabenen herum, bis die {npc:73163} erscheint.'
-L['ironfur_steelhorn_note'] = 'Töte {npc:72844} um den Hof der Erhabenen herum, bis das {npc:73160} erscheint.'
+L['imperial_python_note'] = 'Töte {npc:72841} um den {location:Hof der Erhabenen} herum, bis die {npc:73163} erscheint.'
+L['ironfur_steelhorn_note'] = 'Töte {npc:72844} um den {location:Hof der Erhabenen} herum, bis das {npc:73160} erscheint.'
 L['karkanos_note'] = 'Sprich mit {npc:72151}, um den seltenen Gegner an Land zu ziehen. Was für ein Fang!'
 L['monstrous_spineclaw_note'] = 'Töte {npc:72766} um die Insel herum, bis die {npc:73166} erscheint.'
 L['rattleskew_note'] = 'Töte Wellen von {npc:72033}, bis der seltene Gegner erscheint.'
@@ -48,9 +48,9 @@ L['sturdy_chest_note'] = 'Greife einen {npc:73531} an, um hierher getragen zu we
 L['gleaming_treasure_satchel_note'] = 'Lauf auf den Seilen des Schiffs und spring dann auf den Mast, an dem der Schatzbeutel hängt.'
 L['gleaming_treasure_chest_note'] = 'Springe auf den Säulen zum Schatz.'
 L['mist_covered_treasure_chest_note'] = [[
-{note:Zuvor müssen die Funkelnde Schatztruhe und die Verschnürte Schatztruhe geplündert worden sein.}
+{note:Zuvor müssen die {object:Funkelnde Schatztruhe} und die {object:Verschnürte Schatztruhe} geplündert worden sein.}
 
-Klicke die Glitzernde Kranichstatue an, um zur Truhe zu fliegen.
+Klicke die {object:Glitzernde Kranichstatue} an, um zur Truhe zu fliegen.
 ]]
 L['ropebound_treasure_chest_note'] = 'Laufe auf den Seilen zum Schatz.'
 L['sunken_treasure_note'] = 'Töte die Elitegegner auf dem gesunkenen Schiff für den Schlüssel.'
@@ -62,7 +62,7 @@ L['sunken_treasure_note'] = 'Töte die Elitegegner auf dem gesunkenen Schiff fü
 L['zandalari_warbringer_note'] = 'Kann mehrfach am Tag geplündert werden. \nDie Farbe vom Reittier des {npc:69842} bestimmt die Farbe des geplünderten Reittiers.'
 L['zandalari_warbringer_killed'] = 'Kriegshetzer der Zandalari getötet.'
 
-L['squirrels_note'] = 'Du musst das Emote /liebe auf Kleintiere anwenden (KEINE Kampfhaustiere).'
+L['squirrels_note'] = 'Du musst das Emote {emote:/liebe} auf Kleintiere anwenden (KEINE Kampfhaustiere).'
 L['options_icons_squirrels'] = '{achievement:6350}'
 L['options_icons_squirrels_desc'] = 'Zeigt die Positionen der Kleintiere für den Erfolg {achievement:6350} an.'
 
@@ -73,7 +73,7 @@ L['options_icons_lorewalker_desc'] = 'Zeigt die Orte für den Erfolg {achievemen
 --------------------------------- Jade Forest ---------------------------------
 -------------------------------------------------------------------------------
 
-L['ancient_pick'] = '{item:85777} befindet sich im Grünsteinbruch auf der unteren Ebene.'
+L['ancient_pick'] = '{item:85777} befindet sich im {location:Grünsteinbruch} auf der unteren Ebene.'
 L['ships_locker'] = 'Schiffstruhe'
 L['ships_locker_note'] = 'Im versunkenen Schiff. enthält ca. 96 Gold.'
 L['chest_of_supplies'] = 'Kiste mit Vorräten'
@@ -100,8 +100,8 @@ L['forgotten_lockbox_note'] = 'In der Taverne auf der zweiten Ebene.\nEnthält c
 -------------------------- Vale of Eternal Blossoms ---------------------------
 -------------------------------------------------------------------------------
 
-L['guolai_halls'] = 'In den Guo-Lai-Hallen'
-L['guolai_cache'] = 'Finde einen {item:87779} und öffne eine uralte Guo-Lai-Truhe.'
+L['guolai_halls'] = 'In den {location:Guo-Lai-Hallen}'
+L['guolai_cache'] = 'Finde einen {item:87779} und öffne eine {object:uralte Guo-Lai-Truhe}.'
 
 -------------------------------------------------------------------------------
 ------------------------------- Kun Lai Summit --------------------------------
@@ -142,6 +142,6 @@ L['options_icons_kroshik_desc'] = 'Zeigt die Orte für den Erfolg {achievement:8
 
 L['iot_portal'] = 'Portal'
 L['ritualstone_needed'] = 'Du brauchst 3 {item:94221} um den Seltenen Gegner zu beschwören.'
-L['kroshik_bow'] = '/verbeugen'
-L['kroshik_adult'] = 'Läuft um den kleinen See herum.\n/verbeugen'
-L['kroshik_baby'] = 'Läuft in der Gegend herum.\n/verbeugen'
+L['kroshik_bow'] = '{emote:/verbeugen}'
+L['kroshik_adult'] = 'Läuft um den kleinen See herum.\n{emote:/verbeugen}'
+L['kroshik_baby'] = 'Läuft in der Gegend herum.\n{emote:/verbeugen}'
