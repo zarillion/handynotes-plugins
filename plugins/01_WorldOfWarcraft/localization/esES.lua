2f731cd63d0c87972ebc6b1f52cbf7057f352bbb
Krovikan <krovikan@gmail.com>
2023-01-05 00:14:36 +0100

    Updated Safari Vanilla esES

--- plugins/01_WorldOfWarcraft/localization/esES.lua
+++ plugins/01_WorldOfWarcraft/localization/esES.lua
@@ -10,8 +10,8 @@ if not L then return end
 L['anniversary_note'] = '{note:¡Sólo disponible durante el eventuo anual de aniversario!}'
 
 L['summer_only'] = 'Aparece sólo en Verano. (Del 21 de Junio al 22 de Septiembre)'
-L['options_icons_safari'] = nil
-L['options_icons_safari_desc'] = nil
+L['options_icons_safari'] = 'Safari de Kalimdor / Reinos del Este'
+L['options_icons_safari_desc'] = 'Muestra dónde están  las mascotas de combate para los logros {achievement:6585} y {achievement:6586}.'
 
 L['options_icons_crazyforcats'] = '{achievement:8397}'
-L['options_icons_crazyforcats_desc'] = nil
+L['options_icons_crazyforcats_desc'] = 'Muestra dónde están  las mascotas de combate para el logro {achievement:8397}.'
