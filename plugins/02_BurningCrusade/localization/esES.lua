0098e3852e08ac0514613aa757d0636ffdd53913
Krovikan <krovikan@gmail.com>
2023-01-04 23:35:39 +0100

    Updated Safari BC esES

--- plugins/02_BurningCrusade/localization/esES.lua
+++ plugins/02_BurningCrusade/localization/esES.lua
@@ -10,8 +10,8 @@ L['netherwing_egg'] = '{item:32506}'
 L['options_icons_netherwing_eggs'] = '{achievement:898}'
 L['options_icons_netherwing_eggs_desc'] = 'Muestra donde están los {item:32506} para el logro {achievement:898}.'
 
-L['options_icons_safari'] = '{achievement:6587}'
-L['options_icons_safari_desc'] = nil
+L['options_icons_safari'] = 'Safari de Kalimdor / Reinos del Este'
+L['options_icons_safari_desc'] = 'Muestra dónde están  las mascotas de combate para los logros {achievement:6585} y {achievement:6586}.'
 
 L['options_icons_crazyforcats'] = '{achievement:8397}'
-L['options_icons_crazyforcats_desc'] = nil
+L['options_icons_crazyforcats_desc'] = 'Muestra dónde están  las mascotas de combate para el logro {achievement:8397}.'
