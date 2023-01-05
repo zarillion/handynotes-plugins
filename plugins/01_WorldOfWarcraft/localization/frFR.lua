7d2244f2fe08944e3e76f464c12e49667b94ea14
Lightuky <47103897+Lightuky@users.noreply.github.com>
2023-01-05 07:34:19 +0100

    Update frFR.lua - safari - Vanilla

--- plugins/01_WorldOfWarcraft/localization/frFR.lua
+++ plugins/01_WorldOfWarcraft/localization/frFR.lua
@@ -9,9 +9,9 @@ if not L then return end
 
 L['anniversary_note'] = '{note:Disponible uniquement lors de l\'événement anniversaire annuel !}'
 
-L['summer_only'] = nil
-L['options_icons_safari'] = nil
-L['options_icons_safari_desc'] = nil
+L['summer_only'] = 'Apparaît uniquement en été. (Du 21 juin au 22 septembre)'
+L['options_icons_safari'] = 'Safari en Kalimdor / dans les Royaumes de l\'Est'
+L['options_icons_safari_desc'] = 'Afficher les emplacements des familiers de combat pour les hauts-faits {achievement:6585} et {achievement:6586}.'
 
 L['options_icons_crazyforcats'] = '{achievement:8397}'
-L['options_icons_crazyforcats_desc'] = nil
+L['options_icons_crazyforcats_desc'] = 'Afficher les emplacements des familiers de combat pour le haut-fait {achievement:8397}.'
