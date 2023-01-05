8d0eec8abcc8d28f25ffcb226f11b8b3615995e8
Adavak <adavak@adavak.com>
2023-01-05 08:15:45 +0800

    update zhCN.

--- plugins/01_WorldOfWarcraft/localization/zhCN.lua
+++ plugins/01_WorldOfWarcraft/localization/zhCN.lua
@@ -8,9 +8,9 @@ if not L then return end
 
 L['anniversary_note'] = '{note:仅在周年纪念活动期间可用！}'
 
-L['summer_only'] = nil
-L['options_icons_safari'] = nil
-L['options_icons_safari_desc'] = nil
+L['summer_only'] = '只在夏季出现。（从6月21日到9月22日）'
+L['options_icons_safari'] = '{achievement:6585} / {achievement:6586}'
+L['options_icons_safari_desc'] = '显示 {achievement:6585} 和 {achievement:6586} 成就中战斗宠物的位置。'
 
 L['options_icons_crazyforcats'] = '{achievement:8397}'
-L['options_icons_crazyforcats_desc'] = nil
+L['options_icons_crazyforcats_desc'] = '显示 {achievement:8397} 成就中战斗宠物的位置。'
--- plugins/02_BurningCrusade/localization/zhCN.lua
+++ plugins/02_BurningCrusade/localization/zhCN.lua
@@ -11,7 +11,7 @@ L['options_icons_netherwing_eggs'] = '{achievement:898}'
 L['options_icons_netherwing_eggs_desc'] = '显示 {achievement:898} 成就中 {item:32506} 的位置。'
 
 L['options_icons_safari'] = '{achievement:6587}'
-L['options_icons_safari_desc'] = nil
+L['options_icons_safari_desc'] = '显示 {achievement:6587} 成就中战斗宠物的位置。'
 
 L['options_icons_crazyforcats'] = '{achievement:8397}'
-L['options_icons_crazyforcats_desc'] = nil
+L['options_icons_crazyforcats_desc'] = '显示 {achievement:8397} 成就中战斗宠物的位置。'
