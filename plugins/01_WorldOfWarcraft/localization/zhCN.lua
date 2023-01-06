da5da9e0b99b254927e075a1056d1164458b9e0f
Adavak <adavak@adavak.com>
2023-01-06 09:21:27 +0800

    Update zhCN.lua

--- plugins/01_WorldOfWarcraft/localization/zhCN.lua
+++ plugins/01_WorldOfWarcraft/localization/zhCN.lua
@@ -14,4 +14,4 @@ L['options_icons_safari_desc'] = '显示 {achievement:6585} 和 {achievement:658
 
 L['options_icons_crazyforcats'] = '{achievement:8397}'
 L['options_icons_crazyforcats_desc'] = '显示 {achievement:8397} 成就中战斗宠物的位置。'
-L['cfc_blacktabby'] = nil
+L['cfc_blacktabby'] = '{item:8491} 可以从 {location:希尔斯布莱德丘陵} 的各种敌人身上拾取。\n\n推荐的敌人是 {npc:49116}（仅限部落）、{npc:48017} 或 {npc:2248}。\n\n（掉率低于0.1%）'
