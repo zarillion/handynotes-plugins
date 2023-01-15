9a3d44dacc723e59d14e96a442b05c1ce4e3bb0f
Krovikan <krovikan@gmail.com>
2023-01-15 13:20:55 +0100

    {title:}, blank lines in [[ ]] Cataclysm enUS
    To adapt format with the always used by Zarillion for [[ ]]

--- plugins/04_Cataclysm/localization/enUS.lua
+++ plugins/04_Cataclysm/localization/enUS.lua
@@ -17,21 +17,25 @@ L['hyjal_phase5'] = 'Phase 5 - The Regrowth'
 L['hyjal_phase1_note'] = 'Complete Quests in {location:Mount Hyjal} until you get {quest:29389}. This questline will start the Invasion Phase.'
 L['hyjal_phase2_note'] = 'To advance into Phase 2 you need 10 {currency:416} for {quest:29198}.\n\nYou earn {currency:416} from daily quests.'
 L['hyjal_phase3_note'] = 'To advance into Phase 3 you need 15 {currency:416} for {quest:29201}.\n\nYou earn {currency:416} from daily quests.'
-L['hyjal_phase4_note'] = [[Phase 4 is split into 2 parts.
+L['hyjal_phase4_note'] = [[
+Phase 4 is split into 2 parts.
+
 To advance into Phase 4a you need 150 {currency:416} for {quest:29181}.
 To advance into Phase 4b you need 150 {currency:416} for {quest:29214}.
 
-You earn {currency:416} from daily quests.]]
+You earn {currency:416} from daily quests.
+]]
 L['hyjal_phase5_note'] = 'To advance into Phase 5 you need to complete {quest:29215} and {quest:29182}.' -- review
 
 L['portal_molten_front'] = 'Portal to the Molten Front'
 
-L['spider_hill_note'] = [[Reach the highest point in the {location:Widow's Clutch}.
+L['spider_hill_note'] = [[
+Reach the highest point in the {location:Widow's Clutch}.
+
 To reach the top you have to aggro the {npc:52981} without killing them. They will cast {spell:97959} and pull you up.
-
 Recommended methods for max level characters:
-{item:46725}
 
+{item:46725}
 Demon Hunter: {spell:185245}
 Druid: {spell:2908}
 Hunter: {spell:1513}
@@ -42,21 +46,30 @@ Rogue: {spell:36554}
 Shaman: {spell:52870}
 Warrior: {spell:355}
 ]]
-L['fiery_lords_note'] = 'Only one {npc:Lord of Cinders} is up at a time, after killing it the next will spawn immediately.'
-L['have_we_met_note'] = [[An elite group of fighters will assist you at the daily quest as you arrive in {location:Sethria's Roost}.
 
-Use the emote {emote:/wave} to wave to the required NPC.]]
-L['ludicrous_speed_note'] = [[While on the quest {daily:29147}, obtain 65 stacks of {spell:100957}.
+L['fiery_lords_note'] = 'Only one {title:Lord of Cinders} is up at a time, after killing it the next will spawn immediately.'
+L['have_we_met_note'] = [[
+An elite group of fighters will assist you at the daily quest as you arrive in {location:Sethria's Roost}.
+
+Use the emote {emote:/wave} to wave to the required NPC.
+]]
+
+L['ludicrous_speed_note'] = [[
+While on the quest {daily:29147}, obtain 65 stacks of {spell:100957}.
 
 {npc:52594} gives 15 stacks
 {npc:52596} gives 5 stacks
-{npc:52595} gives 1 stack]]
+{npc:52595} gives 1 stack
+]]
+
 L['angry_little_squirrel_note'] = 'Pull an enemy to the tree and an {npc:52195} will bonk on its head.'
 L['hyjal_bear_cub_note'] = 'While on the quest {daily:29161}, throw a {npc:52688} on a {npc:52795}' -- review
 L['child_of_tortolla_note'] = 'While on the quest {daily:29101}, instead of punting a turtle into the water, punt it at a {npc:52219}.' -- review
-L['ready_for_raiding_2_note'] = 'Slay the following {npc:Lieutenants of Flame} in {location:Ragnaros\' Reach} without getting hit by their special attacks.'
+L['ready_for_raiding_2_note'] = 'Slay the following {title:Lieutenant of Flame} in {location:Ragnaros\' Reach} without getting hit by their special attacks.'
 L['flawless_victory_note'] = 'Solo kill a {npc:52552} without taking any damage from {spell:97243} or {spell:96688}.'
 L['gang_war_note'] = 'Win a duel in {location:Sethria\'s Roost} while on the quest {daily:29128}.'
-L['death_from_above_note'] = [[Bomb the {npc:<Firelord>} while you are on the quest {daily:29290}.
+L['death_from_above_note'] = [[
+Bomb the {title:<Firelord>} while you are on the quest {daily:29290}.
 
-{note:Only 3 {npc:Firelord}s are active at a time. To complete the achievement faster dont turn in the quest and come back tomorrow.}]]
+{note:Only 3 {title:Firelord}s are active at a time. To complete the achievement faster dont turn in the quest and come back tomorrow.}
+]]
