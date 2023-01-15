4809f78d6e2b0424a94b5fa0d352b2df1afc5616
Adavak <adavak@adavak.com>
2023-01-15 21:03:55 +0800

    Update zhCN.lua

--- plugins/04_Cataclysm/localization/zhCN.lua
+++ plugins/04_Cataclysm/localization/zhCN.lua
@@ -14,24 +14,28 @@ L['hyjal_phase4a'] = '阶段4A - 猛禽德鲁伊区域'
 L['hyjal_phase4b'] = '阶段4B - 暗影守望者区域'
 L['hyjal_phase5'] = '阶段5 - 复苏之地'
 
-L['hyjal_phase1_note'] = '在 {location:海加尔山} 完成任务直到 {quest:29389} 开始进攻阶段。'
+L['hyjal_phase1_note'] = '在 {location:海加尔山} 完成任务直到获得 {quest:29389}，此任务线将开启进攻阶段。'
 L['hyjal_phase2_note'] = '要进入阶段2，需要10个 {currency:416} 才能完成 {quest:29198}。\n\n可以通过日常任务赚取 {currency:416}。'
 L['hyjal_phase3_note'] = '要进入阶段3，需要15个 {currency:416} 才能完成 {quest:29201}。\n\n可以通过日常任务赚取 {currency:416}。'
-L['hyjal_phase4_note'] = [[阶段4分为2个部分。
+L['hyjal_phase4_note'] = [[
+阶段4分为2个部分。
+
 要进入阶段4A，需要150个 {currency:416} 给 {quest:29181}。
 要进入阶段4B，需要150个 {currency:416} 给 {quest:29214}。
 
-通过日常任务赚取 {currency:416}。]]
-L['hyjal_phase5_note'] = nil
+通过日常任务赚取 {currency:416}。
+]]
+L['hyjal_phase5_note'] = '要进入阶段5，需要完成 {quest:29215} 和 {quest:29182}。' -- review
 
 L['portal_molten_front'] = '熔火前线传送门'
 
-L['spider_hill_note'] = [[到达 {location:寡妇之巢} 的最高点。
+L['spider_hill_note'] = [[
+到达 {location:寡妇之巢} 的最高点。
+
 要到达顶部，必须在不杀死 {npc:52981} 的情况下激怒他们。他们会施放 {spell:97959} 并把你拉起来。
-
 满级角色推荐方法：
-{item:46725}
 
+{item:46725}
 恶魔猎手：{spell:185245}
 德鲁伊：{spell:2908}
 猎人：{spell:1513}
@@ -42,19 +46,30 @@ L['spider_hill_note'] = [[到达 {location:寡妇之巢} 的最高点。
 萨满祭司：{spell:52870}
 战士：{spell:355}
 ]]
-L['fiery_lords_note'] = '一次只有一个 {npc:53267} 出现，杀死它后下一个会立即出现。'
-L['have_we_met_note'] = [[当到达 {location:塞西莉亚的栖地}时，一群精英战士将协助完成日常任务。
 
-使用表情 {emote:/招手}，{emote:/wave} 向所需的 NPC 挥手。]]
-L['ludicrous_speed_note'] = [[在任务 {daily:29147} 中，获得65层 {spell:100957}。
+L['fiery_lords_note'] = '一次只有一个 {npc:53267} 出现，杀死它后下一个会立即出现。'
+L['have_we_met_note'] = [[
+当到达 {location:塞西莉亚的栖地}时，一群精英战士将协助完成日常任务。
+
+使用表情 {emote:/招手}，{emote:/wave} 向所需的 NPC 挥手。
+]]
+
+L['ludicrous_speed_note'] = [[
+在任务 {daily:29147} 中，获得65层 {spell:100957}。
 
 {npc:52594} 给予15层
 {npc:52596} 给予5层
-{npc:52595} 给予1层]]
+{npc:52595} 给予1层
+]]
+
 L['angry_little_squirrel_note'] = '将一个敌人拉到树上，一个 {npc:52195} 会撞到它的头上。'
 L['hyjal_bear_cub_note'] = '在任务 {daily:29161} 时，向 {npc:52795} 投掷 {npc:52688}。' -- review
 L['child_of_tortolla_note'] = '在任务 {daily:29101} 时，不要将乌龟踢入水中，而是将其踢向 {npc:52219}。' -- review
 L['ready_for_raiding_2_note'] = '在 {location:拉格纳罗斯的领域} 击败下列 {npc:烈焰副官}，并且不被他们的特殊攻击命中。'
 L['flawless_victory_note'] = '单独击杀一个 {npc:52552} 而且没有受到 {spell:97243} 或 {spell:96688} 的任何伤害。'
-L['gang_war_note'] = nil
-L['death_from_above_note'] = nil
+L['gang_war_note'] = '在 {location:塞西莉亚的栖地} 的 {daily:29128} 任务中赢得决斗。'
+L['death_from_above_note'] = [[
+当 {daily:29290} 时轰炸 {title:<火焰领主>}。
+
+{note:一次只有3个 {title:火焰领主} 处于激活状态。为了更快地完成成就，请不要交任务，明天再来。}
+]]
