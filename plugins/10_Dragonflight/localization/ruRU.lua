e801f86798b284cf57dd14619550fa6b4791a8a5
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2023-01-24 07:46:32 +0300

    Update ruRU.lua

--- plugins/10_Dragonflight/localization/ruRU.lua
+++ plugins/10_Dragonflight/localization/ruRU.lua
@@ -91,7 +91,7 @@ L['signal_transmitter_note'] = '{note:Требуется 10 очков в Мас
 L['options_icons_signal_transmitter'] = 'Драконий генератор червоточин'
 L['options_icons_signal_transmitter_desc'] = 'Отображение местоположения {object:Отключенных передатчиков} для {item:198156}.'
 
-L['spawns_periodically'] = '{note:Появляется на час каждые 2 часа.}'
+L['spawns_periodically'] = '{note:Возможные появления каждые 30 минут, синхронизированные по всем игровым мирам. (Пример: 9:00, 17:30)}'
 L['spawns_at_night'] = '{note:Появляется только ночью. (После 18:30 по серверному времени)}'
 
 L['elemental_storm'] = 'Буря Стихии'
