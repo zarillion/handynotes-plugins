3452c3309d5e550bcf03c868bce12953127431e0
Wyldclaw <wyldclawthornbringer@gmail.com>
2023-02-26 20:45:16 -0700

    Initial commit

--- core/templates.lua
+++ core/templates.lua
@@ -249,7 +249,7 @@ function WorldMapOptionsButtonMixin:InitializeDropDown(level)
             end
         elseif L_UIDROPDOWNMENU_MENU_VALUE == 'rewards' then
             for i, type in ipairs({
-                'mount', 'pet', 'toy', 'transmog', 'all_transmog'
+                'mount', 'pet', 'recipe', 'toy', 'transmog', 'all_transmog'
             }) do
                 LibDD:UIDropDownMenu_AddButton({
                     text = L['options_' .. type .. '_rewards'],
