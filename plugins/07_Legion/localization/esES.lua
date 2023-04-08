d211f70fa5852ed66731e6370f2382f92ec7a67e
Krovikan <krovikan@gmail.com>
2023-04-09 00:50:31 +0200

    Updated Portals + add tags Legion esES

--- plugins/07_Legion/localization/esES.lua
+++ plugins/07_Legion/localization/esES.lua
@@ -162,8 +162,8 @@ L['options_icons_eredar_war_supplies_desc'] = 'Muestra dónde pueden estar los {
 -------------------------------------------------------------------------------
 --------------------------------- STORMHEIM -----------------------------------
 -------------------------------------------------------------------------------
-L['to_stormheim'] = nil
-L['to_helheim'] = nil
+L['to_stormheim'] = 'Portal a {location:Tormenheim}'
+L['to_helheim'] = 'Portal a {location:Helheim}'
 
 L['trapper_jarrun_note'] = 'Reúne tus defensas, mortal.'
 L['robert_craig_note'] = '¡Azúzalas!'
