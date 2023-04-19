70d58eb6df19b5dcbe4376caa801871c4efcbb7d
Ioney <dominik.hier@gmail.com>
2023-04-20 00:40:41 +0200

    add path to Lurgan, add some scoutpacks

--- plugins/10_Dragonflight/zones/ohnahran_plains.lua
+++ plugins/10_Dragonflight/zones/ohnahran_plains.lua
@@ -644,13 +644,16 @@ map.nodes[43105078] = Rare({
     pois = {POI({43724823})}
 }) -- Web-Queen Ashkaz
 
-map.nodes[35803600] = Rare(
+map.nodes[34823454] = Rare(
     { -- is all over the place on wowhead, took a random one,
         id = 201540,
         quest = 74546,
         rewards = {
             Transmog({item = 203674, slot = L['plate']}), -- Brutal Tramplers
             DC.CliffsideWylderdrake.ManedTail, DC.RenewedProtoDrake.GrayHair
+        },
+        pois = {
+            Path({33793438, 34303449, 34823454, 35333464, 35833478, 36323500})
         }
     }) -- Lurgan
 
@@ -1278,16 +1281,25 @@ map.nodes[60567702] = Scoutpack()
 map.nodes[60952000] = Scoutpack()
 map.nodes[61301817] = Scoutpack()
 map.nodes[61781881] = Scoutpack()
+map.nodes[62922121] = Scoutpack()
 map.nodes[63423235] = Scoutpack()
 map.nodes[64028081] = Scoutpack()
 map.nodes[65021064] = Scoutpack()
 map.nodes[66798258] = Scoutpack()
+map.nodes[71356836] = Scoutpack()
+map.nodes[73353879] = Scoutpack()
 map.nodes[73618656] = Scoutpack()
 map.nodes[78736935] = Scoutpack()
+map.nodes[79432374] = Scoutpack()
+map.nodes[79923683] = Scoutpack({note = L['in_small_cave']})
+map.nodes[83663235] = Scoutpack({note = L['in_small_cave']})
+map.nodes[83812914] = Scoutpack()
+map.nodes[84194551] = Scoutpack()
 map.nodes[84685647] = Scoutpack()
 map.nodes[86084606] = Scoutpack()
 map.nodes[86323937] = Scoutpack()
 map.nodes[86483680] = Scoutpack()
+map.nodes[87256074] = Scoutpack()
 map.nodes[91393390] = Scoutpack()
 
 -------------------------------------------------------------------------------
