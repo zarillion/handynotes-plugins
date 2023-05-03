cf41b204ad550627c886c543e4054432ebd6410a
Krovikan <krovikan@gmail.com>
2023-05-03 12:40:37 +0200

    Trying to fix Lint DF esES

--- plugins/10_Dragonflight/localization/esES.lua
+++ plugins/10_Dragonflight/localization/esES.lua
@@ -738,7 +738,7 @@ L['brulsef_the_stronk_note'] = 'Despoja tus recompensas del {object:Cofre de Eno
 L['ancient_zaqali_chest_note'] = 'Usa el {object:Magma Embotellado} cercano para abrir el cofre.'
 L['blazing_shadowflame_chest_note'] = '{bug:The shadowflame is too strong to bear.}' -- DEV NOTE - Do not translate
 L['crystal_encased_chest_note'] = 'Interactúa con el {object:Cristal de Armonización} azul {dot:Blue} y con el {object:Cristal de Armonización} rojo {dot:Red} para abrir el cofre.'
-L['old_trunk_note'] = 'To get {item:204323} and open {object:Old Trunk} you need to find {npc:204277} 5 times.' -- 'Para conseguir la {item:204323} y abrir el {object:Old Trunk} necesitas encontrar 5 veces {npc:204277}.'
+L['old_trunk_note'] = 'To get {item:204323} and open {object:Old Trunk} you need to find {npc:204277} 5 times.' -- Para conseguir la {item:204323} y abrir el {object:Old Trunk} necesitas encontrar 5 veces {npc:204277}.
 L['well_chewed_chest_note'] = 'La {item:202869} {dot:Green} está escondida debajo de {npc:199962} dentro de la cueva.'
 
 L['molten_hoard_label'] = 'Tesoro de Magma'
