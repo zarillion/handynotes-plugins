6fdc798b18c87e2641d7fb0fab4f6f83507bed07
Krovikan <krovikan@gmail.com>
2023-05-12 00:22:14 +0200

    Updated zaralek_rotation DF esES

--- plugins/10_Dragonflight/localization/esES.lua
+++ plugins/10_Dragonflight/localization/esES.lua
@@ -788,5 +788,5 @@ L['seething_cache_treasure_note'] = 'Para poder ver el {object:Alijo Hirviente} 
 L['chest_of_the_flights_treasure_note'] = 'Para abrir el cofre necesitarás hacer click en las {object:Gema Potenciada} en el orden {note:Rojo > Negro > Azul > Amarillo > Verde}.'
 L['curious_top_hat_note'] = 'Interactúa con la {npc:205010} mientras tengas el buff {spell:410288} para recibir {item:205021}. Si no tienes el buff y te acercas, huirá.'
 
-L['zaralek_rotation_active'] = nil
-L['zaralek_rotation_inactive'] = nil
+L['zaralek_rotation_active'] = ns.color.Blue('Este Raro puedo aparecer hoy.')
+L['zaralek_rotation_inactive'] = ns.color.Orange('Este Raro no aparecerá hoy, vuelve mañana.')
