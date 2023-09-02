c613110c8834d8082a5196f20b51218e6ba24f42
Krovikan <krovikan@gmail.com>
2023-09-02 22:23:06 +0200

    Updated emote esES

--- plugins/10_Dragonflight/localization/esES.lua
+++ plugins/10_Dragonflight/localization/esES.lua
@@ -877,7 +877,7 @@ L['soa_day02_gryffin_note_b'] = 'Despoja 1x {item:207812} matando {npc:191451} d
 
 L['soa_day02_shakey_note_a'] = 'Habla con {npc:198586} para conseguir {item:207816}.'
 L['soa_day02_shakey_note_b'] = 'Paga la cuenta del bar de {npc:198586} %s del {location:Tesoro del Dragón} en {location:Valdrakken}.'
-L['soa_day02_shakey_note_c'] = 'Entra en el {location:Tesoro del Dragón} a través de la entrada secreta tecleando en el chat {emote:/bow} en la {object:Estatua Extraña} en la parte trasera de {location:El Carnero Asado}.'
+L['soa_day02_shakey_note_c'] = 'Entra en el {location:Tesoro del Dragón} a través de la entrada secreta tecleando en el chat la emoción {emote:/arrodillarse} en la {object:Estatua Extraña} en la parte trasera de {location:El Carnero Asado}.'
 
 L['options_icons_secrets_of_azeroth'] = 'Secretos de Azeroth'
 L['options_icons_secrets_of_azeroth_desc'] = 'Muestra dónde están las pistas para el logro {location:Secrets of Azeroth}.'
