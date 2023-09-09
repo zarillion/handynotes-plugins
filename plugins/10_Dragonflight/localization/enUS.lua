f709606682b850c382e3f44671eb64fb41180f3e
Lightuky <47103897+Lightuky@users.noreply.github.com>
2023-09-09 23:02:55 +0200

    Update enUS.lua - npc ID + object name - DF

--- plugins/10_Dragonflight/localization/enUS.lua
+++ plugins/10_Dragonflight/localization/enUS.lua
@@ -937,7 +937,7 @@ L['soa_mjj_part2_note'] = [[
 
 {note:You will need at least 4 people around you for this part.}
 
-In {location:Irontree woods}, you will find a giant {npc:Water Elemental} and an engine next to use.
+In {location:Irontree woods}, you will find a giant {npc:210417} and a {object:Mimiron's Booster Part} next to use.
 You can either use the engine to mount on the elemental and use its ability {spell:423412} to suck people in, or run close to the elemental (but not close enough to get on its aoe, as it knocks you back) so the person piloting it can suck you in.
 
 Once the elemental sucks 4 people in, it will explode and drop {item:209781}! Anyone in the area can loot it.]]
