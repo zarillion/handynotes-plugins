4cef741016e9b91b00e148ab145f9a732a67c362
Ioney <dominik.hier@gmail.com>
2023-09-25 02:21:02 +0200

    Dreamfruit

--- plugins/10_Dragonflight/zones/emerald_dream.lua
+++ plugins/10_Dragonflight/zones/emerald_dream.lua
@@ -753,22 +753,49 @@ map.nodes[51265990] = Dreamfruit({
             id = 19310,
             criteria = {
                 -- 62910, -- Basket of Explosive Acorns
-                -- 62911, -- Bubble Blowgun
-                -- 62912, -- Dream Gatherer
+                62911, -- Bubble Blowgun
+                62912, -- Dream Gatherer
                 62913, -- Feral Overflow
                 -- 62914, -- Flower Trail
                 62915, -- Lifesurger
                 -- 62916, -- Memory of L'ghorek
                 -- 62917, -- Memory of Ursoc
                 -- 62918, -- Mighty Roar
-                -- 62919, -- Moonkin Jump
+                62919, -- Moonkin Jump
                 62920, -- Moonkin Mayhem
                 -- 62921, -- Potent Lullaby
-                -- 62922, -- Putrid Propulsion
+                62922, -- Putrid Propulsion
                 62923 -- Sack of Talontreats
                 -- 62924, -- Star Shower
                 -- 62925, -- The Superb Loom
                 -- 62926 -- Wake Up!
+            }
+        })
+    }
+})
+
+map.nodes[52847357] = Dreamfruit({
+    rewards = {
+        Achievement({
+            id = 19310,
+            criteria = {
+                -- 62910, -- Basket of Explosive Acorns
+                -- 62911, -- Bubble Blowgun
+                62912, -- Dream Gatherer
+                -- 62913, -- Feral Overflow
+                62914, -- Flower Trail
+                -- 62915, -- Lifesurger
+                -- 62916, -- Memory of L'ghorek
+                62917, -- Memory of Ursoc
+                62918, -- Mighty Roar
+                -- 62919, -- Moonkin Jump
+                -- 62920, -- Moonkin Mayhem
+                -- 62921, -- Potent Lullaby
+                -- 62922, -- Putrid Propulsion
+                -- 62923, -- Sack of Talontreats
+                62924, -- Star Shower
+                62925, -- The Superb Loom
+                62926 -- Wake Up!
             }
         })
     }
