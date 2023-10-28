06f24b5032cf0f4fc5fb4e21b2f4544e7f9bfae3
pepedressingroom <119722554+pepedressingroom@users.noreply.github.com>
2023-10-28 21:57:17 +0800

    Update zhTW.lua
    Add mount: Ochre Dreamtalon
    Add Hunter Pet: Thorn Beasts

--- plugins/10_Dragonflight/localization/zhTW.lua
+++ plugins/10_Dragonflight/localization/zhTW.lua
@@ -954,6 +954,26 @@ L['options_icons_somnut_desc'] = nil
 L['improvised_leafbed_note'] = nil
 L['kalandu_note'] = nil
 
+L['Ochre_note'] = nil
+L['Ochre_note_Stage1'] = nil
+L['Ochre_note_Stage2'] = nil
+L['Ochre_note_Stage3'] = nil
+L['Ochre_note_Stage4'] = nil
+L['Ochre_note_Stage5'] = nil
+
+L['thorn_beast_stag'] = nil
+L['thorn_beast_saber'] = nil
+L['thorn_beast_bear'] = nil
+L['thornbeast_note'] = nil
+L['athainne_note'] = nil
+L['ulfar_note'] = nil
+L['thorn_beast_step_2'] = nil
+L['thorn_beast_step_3'] = nil
+L['thorn_beast_step_4'] = nil
+L['thorn_stag_note'] = nil
+L['thorn_saber_note'] = nil
+L['thorn_bear_note'] = nil
+
 -------------------------------------------------------------------------------
 ----------------------------- SECRETS OF AZEROTH ------------------------------
 -------------------------------------------------------------------------------
