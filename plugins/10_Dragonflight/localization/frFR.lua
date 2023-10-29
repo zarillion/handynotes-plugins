043311db742348bbee28cca0a97872ec2592cfb5
pepedressingroom <119722554+pepedressingroom@users.noreply.github.com>
2023-10-29 12:36:56 +0800

    Update frFR.lua
    https://github.com/zarillion/handynotes-plugins/pull/373#issuecomment-1783874261

--- plugins/10_Dragonflight/localization/frFR.lua
+++ plugins/10_Dragonflight/localization/frFR.lua
@@ -956,12 +956,12 @@ L['options_icons_somnut_desc'] = 'Afficher les emplacements possibles des {objec
 L['improvised_leafbed_note'] = 'Terminez la suite de quêtes commençant par {quest:77896} pour recevoir le {item:210864} de la part de {npc:210164}.'
 L['kalandu_note'] = 'Terminez la suite de quêtes commençant par {quest:77948} pour recevoir {item:210633} de la part de {npc:210196}.'
 
-L['Ochre_note'] = nil
-L['Ochre_note_Stage1'] = nil
-L['Ochre_note_Stage2'] = nil
-L['Ochre_note_Stage3'] = nil
-L['Ochre_note_Stage4'] = nil
-L['Ochre_note_Stage5'] = nil
+L['ochre_note'] = nil
+L['ochre_note_stage1'] = nil
+L['ochre_note_stage2'] = nil
+L['ochre_note_stage3'] = nil
+L['ochre_note_stage4'] = nil
+L['ochre_note_stage5'] = nil
 
 L['thorn_beast_stag'] = nil
 L['thorn_beast_saber'] = nil
