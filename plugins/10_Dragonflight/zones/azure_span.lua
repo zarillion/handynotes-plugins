a1e01141501086ee0f1262bf964adb49ac308e81
wyldclaw <wyldclawthornbringer@gmail.com>
2024-01-31 11:26:50 -0600

    Fix mount loot for The Big Dig

--- plugins/10_Dragonflight/zones/azure_span.lua
+++ plugins/10_Dragonflight/zones/azure_span.lua
@@ -2230,16 +2230,14 @@ local Eadweard = Class('Eadweard', Collectible, {
                 65421 -- Just One More Thing
             }
         }), -- The Archives Called, You Answered
+        Mount({item = 212645, id = 2038}), -- Clayscale Hornstrider
         Transmog({item = 212692, slot = L['cosmetic']}), -- Excavator's Dusky Fedora
         Transmog({item = 212638, slot = L['cosmetic']}), -- Excavator's Work Shirt
         Transmog({item = 212793, slot = L['cosmetic']}), -- Excavator's Pack of Findings
         Transmog({item = 212640, slot = L['cosmetic']}), -- Excavator's Trusty Satchel
         Transmog({item = 212641, slot = L['cosmetic']}), -- Excavator's Rugged Pants
         Transmog({item = 212639, slot = L['cosmetic']}), -- Excavator's Glovelettes
-        Transmog({item = 212642, slot = L['cosmetic']}), -- Excavator's Boots
-        Spacer(), --
-        Section('{item:213177}'), --
-        Mount({item = 212645, id = 2038}) -- Clayscale Hornstrider
+        Transmog({item = 212642, slot = L['cosmetic']}), -- Excavator's Boots    
     }
 }) -- Eadweard Dalyngrigge
 
