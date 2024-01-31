99130f3e507ede364b1689f7b7d9a6ed327a5f41
Krovikan <krovikan@gmail.com>
2024-01-31 12:14:36 +0100

    Some fixes enUS

--- plugins/10_Dragonflight/localization/enUS.lua
+++ plugins/10_Dragonflight/localization/enUS.lua
@@ -183,11 +183,11 @@ L['options_icons_rich_soil_desc'] = 'Display locations for {object:Rich Soil}.'
 L['information_stuffed_clue'] = 'Information-Stuffed Clue'
 L['clued_in_note'] = 'The {npc:210079} spawn during {note:the Big Dig} event and the 3 world quests {wq:Research: ...}.'
 L['options_icons_clued_in'] = '{achievement:19787}'
-L['options_icons_clued_in_desc'] = 'Display {npc:210079} locations for {achievement:19787}.'
+L['options_icons_clued_in_desc'] = 'Display {npc:210079} locations for {achievement:19787} achievement.'
 
 L['goggle_wobble_note'] = '{emote:/dance} with {npc:207763} during the intro quest line or the 3 world quests {wq:Technoscrying: ...} while wearing {item:202247}.'
 L['options_icons_goggle_wobble'] = '{achievement:19791}'
-L['options_icons_goggle_wobble_desc'] = 'Display {npc:207763} locations for {achievement:19791}.'
+L['options_icons_goggle_wobble_desc'] = 'Display {npc:207763} locations for {achievement:19791} achievement.'
 
 -------------------------------------------------------------------------------
 ------------------------------- THE AZURE SPAN --------------------------------
@@ -440,9 +440,9 @@ L['naszuro_caldera_of_the_menders'] = 'On top of the tower.'
 
 L['sun_bleached_vase'] = 'Sun-Bleached Vase'
 L['untranslated_tome'] = 'Untranslated Tome'
-L['untranslated_tome_note'] = 'In the building with the entrance below the bridge'
+L['untranslated_tome_note'] = 'In the building with the entrance below the bridge.'
 L['mysterious_boot'] = 'Mysterious Boot'
-L['mysterious_boot_note'] = 'Upper floor'
+L['mysterious_boot_note'] = 'Upper floor.'
 
 -------------------------------------------------------------------------------
 ------------------------------ OHN'AHRAN PLAINS -------------------------------
