7c231d5fcea2d4e5036d13d866b5abf43669056b
Krovikan <krovikan@gmail.com>
2024-06-23 20:04:43 +0200

    Updated Ringing Deeps TWW esES

--- plugins/11_TheWarWithin/localization/esES.lua
+++ plugins/11_TheWarWithin/localization/esES.lua
@@ -33,8 +33,8 @@ L['turtles_thanks_3_note'] = 'Alimenta a {npc:223338} en {location:Dornogal} y h
 -------------------------------- RINGING DEEPS --------------------------------
 -------------------------------------------------------------------------------
 
-L['forgotten_treasure_note'] = nil
-L['kaja_cola_machine_note'] = nil
+L['forgotten_treasure_note'] = 'Abre los {object:Tesoro Enterrado} cercanos para conseguir la {item:217960}.'
+L['kaja_cola_machine_note'] = 'Compra bebidas en el orden siguiente: Bluesberry Blast > Orange O-pocalypse > Oyster Outbreak > Mangoro Madness.' -- Faltan las bebidas
 
 L['options_icons_not_so_quick_fix'] = '{achievement:40473}'
 L['options_icons_not_so_quick_fix_desc'] = 'Muestra dónde están las consolas del logro {achievement:40473}.'
