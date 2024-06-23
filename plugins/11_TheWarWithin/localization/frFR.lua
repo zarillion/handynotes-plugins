718f52d66d610a0186e3847618ee740ac7069e30
Lightuky <47103897+Lightuky@users.noreply.github.com>
2024-06-23 13:36:23 +0200

    Update frFR.lua - some notes - TWW

--- plugins/11_TheWarWithin/localization/frFR.lua
+++ plugins/11_TheWarWithin/localization/frFR.lua
@@ -11,26 +11,26 @@ L['skyriding_glyph'] = 'Glyphe de vol dynamique'
 L['options_icons_skyriding_glyph'] = 'Glyphes de vol dynamique'
 L['options_icons_skyriding_glyph_desc'] = 'Afficher les emplacements des glyphes de vol dynamique.'
 
-L['options_icons_khaz_algar_lore_hunter'] = nil
-L['options_icons_khaz_algar_lore_hunter_desc'] = nil
+L['options_icons_khaz_algar_lore_hunter'] = '{achievement:40762}'
+L['options_icons_khaz_algar_lore_hunter_desc'] = 'Afficher les emplacements des objets d\'histoire pour le haut-fait {achievement:40762}.'
 
 -------------------------------------------------------------------------------
 -------------------------------- ISLE OF DORN ---------------------------------
 -------------------------------------------------------------------------------
 
-L['magical_treasure_chest_note'] = nil
-L['mosswool_flower_note'] = nil
-L['mushroom_cap_note'] = nil
-L['mysterious_orb_note'] = nil
-L['thaks_treasure_note'] = nil
-L['trees_treasure_note'] = nil
+L['magical_treasure_chest_note'] = 'Poussez {npc:223104} dans l\'eau puis récupérez 5x {npc:223159} à proximité.'
+L['mosswool_flower_note'] = 'Cliquez sur la {npc:222956} et suivez-la.'
+L['mushroom_cap_note'] = 'Ramassez un {object:Chapeau de Boscracine} dans la forêt voisine et ramenez-le à {npc:222894}.'
+L['mysterious_orb_note'] = 'Ramenez la {object:Perle élémentaire} à l\'{npc:222847}.'
+L['thaks_treasure_note'] = 'Parlez à {npc:223227} et suivez-le.'
+L['trees_treasure_note'] = 'Parlez à {npc:222940} pour obtenir une {item:224185}. Guidez 6x {npc:224548} {dot:Green} à travers l\'{location:Île de Dorn}. Si vous avez ramassé tous les crabes, retournez dans la grotte pour parler à nouveau avec {npc:222940}.'
 
 -------------------------------------------------------------------------------
 -------------------------------- RINGING DEEPS --------------------------------
 -------------------------------------------------------------------------------
 
-L['options_icons_not_so_quick_fix'] = nil
-L['options_icons_not_so_quick_fix_desc'] = nil
+L['options_icons_not_so_quick_fix'] = '{achievement:40473}'
+L['options_icons_not_so_quick_fix_desc'] = 'Afficher les emplacements des consoles pour le haut-fait {achievement:40473}.'
 
 L['not_so_quick_fix_note'] = nil
 L['water_console_location'] = nil
@@ -40,8 +40,8 @@ L['obsidian_console_location'] = nil
 L['lost_console_location'] = nil
 L['earthen_console_location'] = nil
 
-L['options_icons_notable_machines'] = nil
-L['options_icons_notable_machines_desc'] = nil
+L['options_icons_notable_machines'] = '{achievement:40628}'
+L['options_icons_notable_machines_desc'] = 'Afficher les emplacements des notes pour le haut-fait {achievement:40628}.'
 
 L['notable_machines_note'] = nil
 L['fragment_I_location'] = nil
@@ -51,8 +51,8 @@ L['fragment_IV_location'] = nil
 L['fragment_V_location'] = nil
 L['fragment_VI_location'] = nil
 
-L['options_icons_rocked_to_sleep'] = nil
-L['options_icons_rocked_to_sleep_desc'] = nil
+L['options_icons_rocked_to_sleep'] = '{achievement:40504}'
+L['options_icons_rocked_to_sleep_desc'] = 'Afficher les emplacements des plaques pour le haut-fait {achievement:40504}.'
 
 L['rocked_to_sleep_note'] = nil
 L['attwogaz_location'] = nil
@@ -70,8 +70,8 @@ L['sathilga_location'] = nil
 --------------------------------- HALLOWFALL ----------------------------------
 -------------------------------------------------------------------------------
 
-L['options_icons_biblio_archivist'] = nil
-L['options_icons_biblio_archivist_desc'] = nil
+L['options_icons_biblio_archivist'] = '{achievement:40622}'
+L['options_icons_biblio_archivist_desc'] = 'Afficher les emplacements des livres pour le haut-fait {achievement:40622}.'
 
 L['biblio_archivist_note'] = nil
 L['biblio_book_01_location'] = nil
@@ -85,8 +85,8 @@ L['biblio_book_08_location'] = nil
 L['biblio_book_09_location'] = nil
 L['biblio_book_10_location'] = nil
 
-L['options_icons_lost_and_found'] = nil
-L['options_icons_lost_and_found_desc'] = nil
+L['options_icons_lost_and_found'] = '{achievement:40618}'
+L['options_icons_lost_and_found_desc'] = 'Afficher les emplacements des souvenirs perdus pour le haut-fait {achievement:40618}.'
 
 L['lost_and_found_note'] = nil
 L['broken_bracelet_location'] = nil
@@ -99,8 +99,8 @@ L['ivory_tinderbox_location'] = nil
 L['dented_spear_location'] = nil
 L['filigreed_cleric_location'] = nil
 
-L['options_icons_missing_lynx'] = nil
-L['options_icons_missing_lynx_desc'] = nil
+L['options_icons_missing_lynx'] = '{achievement:40625}'
+L['options_icons_missing_lynx_desc'] = 'Afficher les emplacements des lynx pour le haut-fait {achievement:40625}.'
 
 L['missing_lynx_note'] = nil
 L['magpie_location'] = nil
@@ -122,8 +122,8 @@ L['gobbo_location'] = nil
 ---------------------------------- AZJ-KAHET ----------------------------------
 -------------------------------------------------------------------------------
 
-L['options_icons_itsy_bitsy_spider'] = nil
-L['options_icons_itsy_bitsy_spider_desc'] = nil
+L['options_icons_itsy_bitsy_spider'] = '{achievement:40624}'
+L['options_icons_itsy_bitsy_spider_desc'] = 'Afficher les emplacements des tisse-rats pour le haut-fait {achievement:40624}.'
 
 L['itsy_bitsy_spider_note'] = nil
 L['webster_location'] = nil
