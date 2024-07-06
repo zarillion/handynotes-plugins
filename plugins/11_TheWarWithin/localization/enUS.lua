d00b2a7570506fca5bf05ed81ba0ad7f1b57a71b
Krovikan <krovikan@gmail.com>
2024-07-06 22:54:53 +0200

    Fixed gnawbles_ruby_vendor TWW enUS

--- plugins/11_TheWarWithin/localization/enUS.lua
+++ plugins/11_TheWarWithin/localization/enUS.lua
@@ -76,9 +76,9 @@ L['sathilga_location'] = 'PH'
 
 L['gnawbles_ruby_vendor_note'] = [[Gather {item:212493} from {object:Disturbed Earth} and bring it to {npc:225166}.
 
-Once you've done 10 contributions, or contributed a total of 50 {item:212493}, the NPC next to {npc:225166}, {npc:216164}, will offer the quest {quest:82957}, which will give you 1 {item:224642}.
+Once you've done 10 contributions or contributed a total of 50x {item:212493}, the NPC next to {npc:225166}, {npc:216164}, will offer the quest {quest:82957} which will give you 1x {item:224642}.
 
-Each item costs 1 {item:224642}.]]
+Each item costs 1x {item:224642}.]]
 
 -------------------------------------------------------------------------------
 --------------------------------- HALLOWFALL ----------------------------------
