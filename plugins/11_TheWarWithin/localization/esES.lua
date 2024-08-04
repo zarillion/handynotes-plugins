63cb65caa791e7c3210b994c12f93cec157ad259
Krovikan <krovikan@gmail.com>
2024-08-04 22:56:44 +0200

    Updated Rares TWW esES

--- plugins/11_TheWarWithin/localization/esES.lua
+++ plugins/11_TheWarWithin/localization/esES.lua
@@ -45,38 +45,38 @@ L['options_icons_not_so_quick_fix'] = '{achievement:40473}'
 L['options_icons_not_so_quick_fix_desc'] = 'Muestra dónde están las consolas del logro {achievement:40473}.'
 
 L['not_so_quick_fix_note'] = 'Repara la consolas terránea estropeada.'
-L['water_console_location'] = nil
-L['abyssal_console_location'] = nil
-L['taelloch_console_location'] = nil
+L['water_console_location'] = 'Junto a las escaleras.'
+L['abyssal_console_location'] = 'En una alcoba.'
+L['taelloch_console_location'] = 'Entre los barriles de un puente.'
 L['obsidian_console_location'] = nil
 L['lost_console_location'] = nil
-L['earthen_console_location'] = nil
+L['earthen_console_location'] = 'En una alcoba.'
 
 L['options_icons_notable_machines'] = '{achievement:40628}'
 L['options_icons_notable_machines_desc'] = 'Muestra dónde están las notas del logro {achievement:40628}.'
 
 L['notable_machines_note'] = 'Lee la nota.'
-L['fragment_I_location'] = nil
-L['fragment_II_location'] = nil
-L['fragment_III_location'] = nil
-L['fragment_IV_location'] = nil
-L['fragment_V_location'] = nil
-L['fragment_VI_location'] = nil
+L['fragment_I_location'] = 'En el suelo.'
+L['fragment_II_location'] = 'En una cornisa.'
+L['fragment_III_location'] = 'Encima del edificio.'
+L['fragment_IV_location'] = 'En lo alto de la torre de madera.'
+L['fragment_V_location'] = 'En el arco en lo alto de las escaleras.'
+L['fragment_VI_location'] = 'En el suelo, al lado de la farola.'
 
 L['options_icons_rocked_to_sleep'] = '{achievement:40504}'
 L['options_icons_rocked_to_sleep_desc'] = 'Muestra dónde están las placas del logro {achievement:40504}.'
 
 L['rocked_to_sleep_note'] = 'Lee la placa del terráneo inerte.'
-L['attwogaz_location'] = nil
-L['halthaz_location'] = nil
-L['krattdaz_location'] = nil
-L['uisgaz_location'] = nil
-L['venedaz_location'] = nil
-L['merunth_location'] = nil
-L['varerko_location'] = nil
-L['alfritha_location'] = nil
-L['gundrig_location'] = nil
-L['sathilga_location'] = nil
+L['attwogaz_location'] = 'En una cornisa.'
+L['halthaz_location'] = 'En una cornisa en la base del pilar.'
+L['krattdaz_location'] = 'En una cornisa entre dos cascadas.'
+L['uisgaz_location'] = 'En una cornisa.'
+L['venedaz_location'] = 'En una plataforma junto a la tubería.'
+L['merunth_location'] = 'En la tubería encima de las escaleras.'
+L['varerko_location'] = 'En una cornisa.'
+L['alfritha_location'] = 'Sentado al borde del acantilado.'
+L['gundrig_location'] = 'Sentado encima de una cornisa.'
+L['sathilga_location'] = 'En una cornisa cerca de un edificio de máquinas mineras de tierra.'
 
 L['trungal_note'] = 'Mata a {npc:220615} que aparece alrededor de la entrada y abajo en la cueva para hacer aparecer a Trungal.'
 L['disturbed_earthgorger_note'] = 'Usa el hechizo de acción extra para hacer {spell:437003} en el suelo 3 veces para hacerlo aparecer.'
@@ -127,7 +127,7 @@ L['biblo_book_07_location'] = 'Dentro del edificio en la mesa de detrás de {npc
 L['biblo_book_08_location'] = 'Dentro de las ruinas.'
 L['biblo_book_09_location'] = 'Dentro de la gran carpa detrás de la mesa de guerra.'
 L['biblo_book_10_location'] = 'En las habitaciones del capitán a bordo de {location:El Rompealbas}.'
-L['biblo_book_11_location'] = nil
+L['biblo_book_11_location'] = 'Dentro del edificio sobre una mesa.'
 
 L['options_icons_lost_and_found'] = '{achievement:40618}'
 L['options_icons_lost_and_found_desc'] = 'Muestra dónde están todos los recuerdos del logro {achievement:40618}.'
@@ -146,21 +146,21 @@ L['filigreed_cleric_location'] = nil
 L['options_icons_missing_lynx'] = '{achievement:40625}'
 L['options_icons_missing_lynx_desc'] = 'Muestra dónde están los linces del logro {achievement:40625}.'
 
-L['missing_lynx_note'] = nil
-L['magpie_location'] = nil
-L['evan_location'] = nil
-L['emery_location'] = nil
-L['jinx_location'] = nil
-L['moog_location'] = nil
-L['iggy_location'] = nil
-L['nightclaw_location'] = nil
-L['purrlock_location'] = nil
-L['shadowpouncer_location'] = nil
-L['miral_murder_mittens_location'] = nil
-L['fuzzy_location'] = nil
-L['furball_location'] = nil
-L['dander_location'] = nil
-L['gobbo_location'] = nil
+L['missing_lynx_note'] = 'Acaricia al gato guerrero feroz.'
+L['magpie_location'] = 'En el suelo junto a la bandera.'
+L['evan_location'] = 'Dentro del edificio.'
+L['emery_location'] = 'Dentro del edificio.'
+L['jinx_location'] = 'Dentro del edificio.'
+L['moog_location'] = 'Dentro del edificio.'
+L['iggy_location'] = 'Dentro del edificio.'
+L['nightclaw_location'] = 'Enciende la {object:Llave Ardiente Inferior} cercana. Requiere 3x {item:206350}.'
+L['purrlock_location'] = 'Enciende la {object:Floración de la Llave Ardiente Inferior} cercana. Requiere 20x {item:206350}.'
+L['shadowpouncer_location'] = 'Enciende la {object:Floración de la Llave Ardiente Inferior} cercana. Requiere 20x {item:206350}.'
+L['miral_murder_mittens_location'] = 'Afuera.'
+L['fuzzy_location'] = 'En el suelo cerca de la {object:Llave Ardiente Inferior}.'
+L['furball_location'] = 'En el edificio en ruinas.'
+L['dander_location'] = 'Afuera.'
+L['gobbo_location'] = 'Dentro del edificio encima de la cama.'
 
 L['croakit_note'] = [[Para atacar a {npc:214757} primero tienes que pescar 10x {item:211474} del agua cercana.
 
