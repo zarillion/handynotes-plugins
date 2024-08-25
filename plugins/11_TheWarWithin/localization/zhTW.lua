494b266fa213bcc4d546f815da3188d930b5367b
ChihChung Ma <mcc@mccma.cc>
2024-08-25 10:46:15 +0800

    update zhTW

--- plugins/11_TheWarWithin/localization/zhTW.lua
+++ plugins/11_TheWarWithin/localization/zhTW.lua
@@ -6,43 +6,43 @@ if not L then return end
 --------------------------------- KHAZ ALGAR ----------------------------------
 -------------------------------------------------------------------------------
 
-L['skyriding_glyph'] = nil
-L['options_icons_skyriding_glyph'] = nil
-L['options_icons_skyriding_glyph_desc'] = nil
+L['skyriding_glyph'] = '天空騎術雕紋'
+L['options_icons_skyriding_glyph'] = '天空騎術雕紋'
+L['options_icons_skyriding_glyph_desc'] = '顯示所有天空騎術雕紋的位置.'
 
-L['options_icons_khaz_algar_lore_hunter'] = nil
-L['options_icons_khaz_algar_lore_hunter_desc'] = nil
+L['options_icons_khaz_algar_lore_hunter'] = '{achievement:40762}'
+L['options_icons_khaz_algar_lore_hunter_desc'] = '顯示成就 {achievement:40762} 所需的知識位置.'
 
 -------------------------------------------------------------------------------
 -------------------------------- ISLE OF DORN ---------------------------------
 -------------------------------------------------------------------------------
 
-L['alunira_note'] = nil
-L['violet_hold_prisoner'] = nil
+L['alunira_note'] = '收集 10x {item:224025} 並組合成 {item:224026} 來移除她的 {spell:451570}.'
+L['violet_hold_prisoner'] = '紫羅蘭堡囚犯'
 
 L['elemental_geode_label'] = nil
-L['magical_treasure_chest_note'] = nil
-L['mosswool_flower_note'] = nil
-L['mushroom_cap_note'] = nil
+L['magical_treasure_chest_note'] = '把 {npc:223104} 推回水裡, 接著在附近收集 5x{npc:223159} 餵給他.'
+L['mosswool_flower_note'] = '點擊 {npc:222956} 並跟著牠.'
+L['mushroom_cap_note'] = '在附近平原蒐集 {object:叢根傘菇} 並帶回來給 {npc:222894}.'
 L['mysterious_orb_note'] = nil
-L['thaks_treasure_note'] = nil
-L['trees_treasure_note'] = nil
+L['thaks_treasure_note'] = '和 {npc:223227} 說話並跟著他.'
+L['trees_treasure_note'] = '和 {npc:222940} 說話來取得 {item:224185}. 引導多恩島上的 6x {npc:224548} ({dot:Green}) 回到 {npc:222940}. 當你引導所有的螃蟹回去後, 回到山洞內和 {npc:222940} 說話.'
 L['turtles_thanks_1_note'] = nil
 L['turtles_thanks_2_note'] = nil
 L['turtles_thanks_3_note'] = nil
 
-L['options_icons_flat_earthen'] = nil
-L['options_icons_flat_earthen_desc'] = nil
+L['options_icons_flat_earthen'] = '{achievement:40606}'
+L['options_icons_flat_earthen_desc'] = '顯示成就 {achievement:40606} 的位置.'
 
 -------------------------------------------------------------------------------
 -------------------------------- RINGING DEEPS --------------------------------
 -------------------------------------------------------------------------------
 
-L['forgotten_treasure_note'] = nil
-L['kaja_cola_machine_note'] = nil
+L['forgotten_treasure_note'] = '開啟附近的 {object:地底藏寶} 來取得 {item:217960}.'
+L['kaja_cola_machine_note'] = '以下列順序購買飲料 {item:223741} > {item:223743} > {item:223744} > {item:223742}.'
 
-L['options_icons_not_so_quick_fix'] = nil
-L['options_icons_not_so_quick_fix_desc'] = nil
+L['options_icons_not_so_quick_fix'] = '{achievement:40473}'
+L['options_icons_not_so_quick_fix_desc'] = '顯示成就 {achievement:40473} 所需的控制臺位置.'
 
 L['not_so_quick_fix_note'] = nil
 L['water_console_location'] = nil
@@ -52,8 +52,8 @@ L['obsidian_console_location'] = nil
 L['lost_console_location'] = nil
 L['earthen_console_location'] = nil
 
-L['options_icons_notable_machines'] = nil
-L['options_icons_notable_machines_desc'] = nil
+L['options_icons_notable_machines'] = '{achievement:40628}'
+L['options_icons_notable_machines_desc'] = '顯示成就 {achievement:40628} 所需的筆記位置.'
 
 L['notable_machines_note'] = nil
 L['fragment_I_location'] = nil
@@ -63,8 +63,8 @@ L['fragment_IV_location'] = nil
 L['fragment_V_location'] = nil
 L['fragment_VI_location'] = nil
 
-L['options_icons_rocked_to_sleep'] = nil
-L['options_icons_rocked_to_sleep_desc'] = nil
+L['options_icons_rocked_to_sleep'] = '{achievement:40504}'
+L['options_icons_rocked_to_sleep_desc'] = '顯示成就 {achievement:40504} 顯示成就 {achievement:40504} 所需的名牌位置.'
 
 L['rocked_to_sleep_note'] = nil
 L['attwogaz_location'] = nil
@@ -78,14 +78,14 @@ L['alfritha_location'] = nil
 L['gundrig_location'] = nil
 L['sathilga_location'] = nil
 
-L['trungal_note'] = nil
+L['trungal_note'] = '殺掉從山洞入口到內部的 {npc:220615} 來刷新.'
 L['disturbed_earthgorger_note'] = nil
 L['deepflayer_broodmother_note'] = nil
-L['lurker_note'] = nil
+L['lurker_note'] = '{note:需要5個玩家以召喚}\n\n在10秒內啟動5個 {dot:Red}{object:不顯眼的控制桿} 來召喚稀有.\n當成功啟動時你會看到區域性的廣播.'
 
 L['gnawbles_ruby_vendor_note'] = nil
-L['options_icons_disturbed_earth'] = nil
-L['options_icons_disturbed_earth_desc'] = nil
+L['options_icons_disturbed_earth'] = '受打擾的土地'
+L['options_icons_disturbed_earth_desc'] = '顯示 {object:受打擾的土地} 的位置.'
 
 L['options_icons_gobblin_with_glublurp'] = '{achievement:40614}'
 L['options_icons_gobblin_with_glublurp_desc'] = nil
@@ -108,18 +108,18 @@ L['ragefin_necrostaff_location'] = nil
 L['jewel_of_the_cliffs_location'] = nil
 L['lost_necklace_note'] = nil
 L['priory_satchel_location'] = nil
-L['smugglers_treasure_note'] = nil
-L['smugglers_treasure_location'] = nil
+L['smugglers_treasure_note'] = '從懸崖底下的 ({dot:Blue}) {npc:226025} 處拾取需要的 {item:225335}.'
+L['smugglers_treasure_location'] = '在懸崖高處的石頭間.'
 L['coral_fused_clam'] = nil
 L['coral_fused_clam_note'] = nil
 
-L['options_icons_biblo_archivist'] = nil
-L['options_icons_biblo_archivist_desc'] = nil
+L['options_icons_biblo_archivist'] = '{achievement:40622}'
+L['options_icons_biblo_archivist_desc'] = '顯示成就 {achievement:40622} 所需的書本位置.'
 
 L['biblo_book_01_location'] = nil
 L['biblo_book_02_location'] = nil
-L['biblo_book_03_location'] = nil
-L['biblo_book_04_location'] = nil
+L['biblo_book_03_location'] = '在都是 {npc:217606} 的獸欄內.'
+L['biblo_book_04_location'] = '在橋的正中間.'
 L['biblo_book_05_location'] = nil
 L['biblo_book_06_location'] = nil
 L['biblo_book_07_location'] = nil
@@ -128,8 +128,8 @@ L['biblo_book_09_location'] = nil
 L['biblo_book_10_location'] = nil
 L['biblo_book_11_location'] = nil
 
-L['options_icons_lost_and_found'] = nil
-L['options_icons_lost_and_found_desc'] = nil
+L['options_icons_lost_and_found'] = '{achievement:40618}'
+L['options_icons_lost_and_found_desc'] = '顯示成就 {achievement:40618} 所需的紀念物位置.'
 
 L['lost_and_found_note'] = nil
 L['broken_bracelet_location'] = nil
@@ -142,10 +142,10 @@ L['ivory_tinderbox_location'] = nil
 L['dented_spear_location'] = nil
 L['filigreed_cleric_location'] = nil
 
-L['options_icons_missing_lynx'] = nil
-L['options_icons_missing_lynx_desc'] = nil
+L['options_icons_missing_lynx'] = '{achievement:40625}'
+L['options_icons_missing_lynx_desc'] = '顯示成就 {achievement:40625} 所需的戰貓位置'
 
-L['missing_lynx_note'] = nil
+L['missing_lynx_note'] = '撫摸兇狠的戰貓.'
 L['magpie_location'] = nil
 L['nightclaw_location'] = nil
 L['purrlock_location'] = nil
@@ -161,7 +161,7 @@ L['deathtide_note'] = nil
 L['murkshade_note'] = nil
 
 L['options_icons_mereldar_menace'] = '{achievement:40151}'
-L['options_icons_mereldar_menace_desc'] = nil
+L['options_icons_mereldar_menace_desc'] = '顯示成就 {achievement:40151} 所需的目標位置.'
 
 L['mereldar_menace_note'] = nil
 L['light_and_flame_location'] = nil
@@ -177,10 +177,10 @@ L['beacon_of_hope_note'] = nil
 ---------------------------------- AZJ-KAHET ----------------------------------
 -------------------------------------------------------------------------------
 
-L['options_icons_itsy_bitsy_spider'] = nil
-L['options_icons_itsy_bitsy_spider_desc'] = nil
+L['options_icons_itsy_bitsy_spider'] = '{achievement:40624}'
+L['options_icons_itsy_bitsy_spider_desc'] = '顯示成就 {achievement:40624} 所需的小織蛛位置.'
 
-L['itsy_bitsy_spider_note'] = nil
+L['itsy_bitsy_spider_note'] = '向小織蛛揮手 ({emote:/wave}).'
 L['webster_location'] = nil
 L['spindle_location'] = nil
 L['swift_location'] = nil
@@ -190,7 +190,7 @@ L['scampering_weave_rat_location'] = nil
 L['generals_scouting_shadecaster_location'] = nil
 
 L['options_icons_smelling_history'] = '{achievement:40542}'
-L['options_icons_smelling_history_desc'] = nil
+L['options_icons_smelling_history_desc'] = '顯示成就 {achievement:40542} 所需的書籍位置.'
 
 L['smelling_history_note'] = nil
 L['smelling_history_1_location'] = nil
@@ -207,10 +207,10 @@ L['smelling_history_11_location'] = nil
 L['smelling_history_12_location'] = nil
 
 L['options_icons_skittershaw_spin'] = '{achievement:40727}'
-L['options_icons_skittershaw_spin_desc'] = nil
+L['options_icons_skittershaw_spin_desc'] = '顯示成就 {achievement:40727} 飛掠拉車路線位置.'
 L['skittershaw_spin_note'] = nil
 
-L['memory_cache_note'] = nil
+L['memory_cache_note'] = '從附近 {spell:420847} from a nearby ({dot:Blue}) {object:Extractor Storage}. After kill the {npc:223908} to get the {item:223870} to open the {object:記憶寶箱}.'
 L['niffen_stash_note'] = '在橋的下面。'
 L['trapped_trove_note'] = nil
 
