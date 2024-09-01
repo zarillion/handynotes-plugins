0a8392d7ee725f32bf5a3d7b6f0e65b405c4931d
Krovikan <krovikan@gmail.com>
2024-09-01 23:56:45 +0200

    Updated Faifhtul Dog with tags esES

--- plugins/11_TheWarWithin/localization/esES.lua
+++ plugins/11_TheWarWithin/localization/esES.lua
@@ -38,12 +38,12 @@ L['turtles_thanks_2_note'] = 'Entrega 1x {item:222533} (puede ser pescado de ban
 L['turtles_thanks_3_note'] = 'Alimenta a {npc:223338} en {location:Dornogal} y habla con ella para revelar el tesoro.'
 L['web_wrapped_axe_note'] = 'En el primer piso.\n\n{note:Reaparece cada 1-2 horas.}'
 L['faithful_dog_note'] = [[
-1. Encuentra al {npc:59533} en las coordenadas 42.40 50.00 en {map:376} de {map:424} y completa {quest:30526}.
-2. Construye el {object:Jardín de Hierbas} en tu {area:7490} en {map:572}.
-3. Encuentra un {item:147420} en {map:619} {map:627} y habla con {npc:87553} en tu {area:7490} {object:Jardín de Hierbas}.
-El {npc:87553} estará ahora en {location:Magical Menagerie} en {map:627}.
-4. Interactúa con el {object:Cuenco de Perro Semienterrado} en {map:2248} y habla con {npc:87553} para conseguir la mascota.
-]] -- REVIEW!!!
+1. Encuentra al {npc:59533} en las coordenadas 42.40 50.00 en el {locations:Valle de los Cuatro Vientos} en {location:Pandaria} y completa la misión {quest:30526}.
+2. Construye el {object:Jardín de Hierbas} en tu {location:Ciudadela} en {location:Draenor}.
+3. Encuentra un {item:147420} en {location:Dalaran} ({location:Islas Abruptas}) y habla con {npc:87553} en el {location:Jardín de Hierbas} de tu {location:Ciudadela}.
+El {npc:87553} estará ahora en {location:Monturas Exóticas} en {location:Dalaran}.
+4. Interactúa con el {object:Cuenco de Perro Semienterrado} en {location:Isla de Dorn} y habla con {npc:87553} para conseguir la mascota.
+]]
 L['cendvin_note'] = 'Consigue 900x {item:225557} en {location:Foldeceniza} de los enemigos élite para comprar la montura {item:223153} a {npc:226205}.'
 
 L['options_icons_flat_earthen'] = '{achievement:40606}'
