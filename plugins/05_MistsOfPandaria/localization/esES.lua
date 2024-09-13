e7bcdd7c2cba18910c689a379061d61140fb727c
Krovikan <krovikan@gmail.com>
2024-09-13 17:35:39 +0200

    Fixes MoP esES

--- plugins/05_MistsOfPandaria/localization/esES.lua
+++ plugins/05_MistsOfPandaria/localization/esES.lua
@@ -6,16 +6,16 @@ if not L then return end
 -------------------------------- TIMELESS ISLE --------------------------------
 -------------------------------------------------------------------------------
 
-L['cavern_of_lost_spirits'] = 'Dentro de la Caverna de los {location:Espíritus Perdidos}.'
+L['cavern_of_lost_spirits'] = 'Dentro de la caverna {location:Espíritus Perdidos}.'
 L['looted_twice'] = 'Nunca has matado a este raro en este personaje. Hoy se puede despojar una segunda vez.'
-L['neverending_spritewood'] = 'Cristal de Duende Interminable'
-L['neverending_spritewood_note'] = 'Tras romper el {object:Cristal de Duende Interminable}, ¡mata a todos los {npc:71824} que puedas mientras tengas el debuff {spell:144052}!'
+L['neverending_spritewood'] = 'Cristal de duende interminable'
+L['neverending_spritewood_note'] = 'Tras romper el {object:Cristal de duende interminable}, ¡mata a todos los {npc:71824} que puedas mientras tengas el debuff {spell:144052}!'
 L['zarhym_note'] = 'Puedes hablar una vez al día con {npc:71876} para entrar al {spell:144145} e intentar encontrar su cuerpo.'
 
 L['archiereus_note'] = 'Compra un {item:103684} del {npc:73306} para invocar al {npc:73174}.'
-L['chelon_note'] = 'Haz click en el {object:Caparazón Vacío Llamativo} para hacer aparecer a {npc:72045}.'
+L['chelon_note'] = 'Haz click en el {object:Caparazón vacío llamativo} para hacer aparecer a {npc:72045}.'
 L['cranegnasher_note'] = 'Atrae a una {npc:72095} desde el sur hasta ponerla encima del cadáver de la {npc:73297}.'
-L['dread_ship_note'] = 'Despoja el {item:104115} de {npc:73279} y úsalo en la {object:Lápida Maldita} para invocar al {npc:73281}.'
+L['dread_ship_note'] = 'Despoja el {item:104115} de {npc:73279} y úsalo en la {object:Lápida maldita} para invocar al {npc:73281}.'
 L['emerald_gander_note'] = 'Mata a las {npc:72762} que hay alrededor de la {location:Corte Celestial} hasta que aparezca el {npc:73158}.'
 L['evermaw_note'] = 'Nada en la dirección de las agujas del reloj alrededor de toda la isla.'
 L['great_turtle_furyshell_note'] = 'Mata {npc:72764} o {npc:72763} hasta que aparezca la {npc:73161}.'
@@ -37,19 +37,19 @@ Si no puedes encontrar un artefacto, puedes usar la {item:70161} (u otro juguete
 ]]
 L['zhugon_note'] = 'Cuando el evento de la cerveza esté activo, mata varios {npc:71908} y rompe barriles hasta que aparezca {npc:71919}.'
 
-L['blazing_chest'] = 'Cofre Llameante'
-L['moss_covered_chest'] = 'Cofre Cubierto de Musgo'
-L['skull_covered_chest'] = 'Cofre Adornado de Calaveras'
-L['smoldering_chest'] = 'Cofre Humeante'
-L['sturdy_chest'] = 'Cofre Robusto'
+L['blazing_chest'] = 'Cofre llameante'
+L['moss_covered_chest'] = 'Cofre cubierto de musgo'
+L['skull_covered_chest'] = 'Cofre adornado de calaveras'
+L['smoldering_chest'] = 'Cofre humeante'
+L['sturdy_chest'] = 'Cofre robusto'
 L['sturdy_chest_note'] = 'Golpea a un {npc:73531} para ser llevado aquí.'
 
 L['gleaming_treasure_satchel_note'] = 'Camina por las cuerdas del barco y salta al poste donde está colgando la cartera.'
 L['gleaming_treasure_chest_note'] = 'Salta por los pilares hasta llegar al tesoro.'
 L['mist_covered_treasure_chest_note'] = [[
-{note:Requiere haber despojado antes el {object:Cofre del Tesoro Reluciente} y el {object:Cofre del Tesoro Atado con Cuerda}.}
+{note:Requiere haber despojado antes el {object:Cofre del tesoro reluciente} y el {object:Cofre del tesoro atado con cuerda}.}
 
-Haz click en la {object:Estatua de la Grulla Reluciente} para volar hasta el tesoro.
+Haz click en la {object:Estatua de la Grulla reluciente} para volar hasta el tesoro.
 ]]
 L['ropebound_treasure_chest_note'] = 'Camina por las cuerdas hasta llegar al tesoro.'
 L['sunken_treasure_note'] = 'Mata élites en el barco hundido hasta conseguir la llave.'
@@ -61,11 +61,11 @@ L['sunken_treasure_note'] = 'Mata élites en el barco hundido hasta conseguir la
 L['zandalari_warbringer_note'] = [[
 Puede ser despojado varias veces al día.
 
-El color de la montura del {npc:69842} determinará el color de la montura que se despoje.
+El color de la montura del {npc:69842} determinará el color de la montura que se despoja.
 ]]
 L['zandalari_warbringer_killed'] = 'Ha muerto el {npc:69842}.'
 
-L['squirrels_note'] = 'Debes teclear la emoción {emote:/amor} en las alimañas que no sean mascotas de combate.'
+L['squirrels_note'] = 'Debes teclear {emote:/amor} en las alimañas que no sean mascotas de combate.'
 L['options_icons_squirrels'] = '{achievement:6350}'
 L['options_icons_squirrels_desc'] = 'Muestra dónde están las alimañas para el logro {achievement:6350}.'
 
@@ -77,65 +77,65 @@ L['options_icons_lorewalker_desc'] = 'Muestra dónde están los sitios para el l
 -------------------------------------------------------------------------------
 
 L['ancient_pick'] = 'El {item:85777} está dentro de la {location:Cantera Verdemar} en el nivel inferior.'
-L['ships_locker'] = 'Baúl del Barco'
-L['ships_locker_note'] = 'En el barco hundido. Contiene unos 96 de Oro.'
-L['chest_of_supplies'] = 'Cofre de Suministros'
-L['chest_of_supplies_note'] = 'Contiene unos 10 de Oro.'
-L['offering_of_rememberance'] = 'Ofrenda de Recuerdo'
-L['offering_of_rememberance_note'] = 'Contiene unos 30 de Oro.'
-L['stash_of_gems'] = 'Alijo de Gemas'
-L['stash_of_gems_note'] = 'Contiene unos 7 de Oro y Gemas.'
+L['ships_locker'] = 'Baúl del barco'
+L['ships_locker_note'] = 'En el barco hundido. Contiene unos 96 de oro.'
+L['chest_of_supplies'] = 'Cofre de suministros'
+L['chest_of_supplies_note'] = 'Contiene unos 10 de oro.'
+L['offering_of_rememberance'] = 'Ofrenda de recuerdo'
+L['offering_of_rememberance_note'] = 'Contiene unos 30 de oro.'
+L['stash_of_gems'] = 'Alijo de gemas'
+L['stash_of_gems_note'] = 'Contiene unos 7 de oro y gemas.'
 
 -------------------------------------------------------------------------------
 -------------------------------- Krasarang Wilds ------------------------------
 -------------------------------------------------------------------------------
 
-L['equipment_locker'] = 'Armario de Equipo'
+L['equipment_locker'] = 'Armario de equipo'
 
 -------------------------------------------------------------------------------
 ------------------------------- The Veiled Stair ------------------------------
 -------------------------------------------------------------------------------
 
-L['forgotten_lockbox'] = 'Arcón Olvidado'
-L['forgotten_lockbox_note'] = 'En el segundo piso de la taverna.\nContiene unos 10 de Oro.'
+L['forgotten_lockbox'] = 'Arcón olvidado'
+L['forgotten_lockbox_note'] = 'En el segundo piso de la taberna.\nContiene unos 10 de oro.'
 
 -------------------------------------------------------------------------------
 -------------------------- Vale of Eternal Blossoms ---------------------------
 -------------------------------------------------------------------------------
 
 L['guolai_halls'] = 'En las {location:Salas de Guo-Lai}.'
-L['guolai_cache'] = 'Encuentra una {item:87779} y abre un {object:Alijo de Guo-Lai Antiguo}.'
+L['guolai_cache'] = 'Encuentra una {item:87779} y abre un {object:Alijo de Guo-Lai antiguo}.'
 
 -------------------------------------------------------------------------------
 ------------------------------- Kun Lai Summit --------------------------------
 -------------------------------------------------------------------------------
 
-L['lost_adventurers_belongings'] = 'Pertenencias del Aventurero Perdido'
-L['lost_adventurers_belongings_note'] = 'Contiene unos 97 de Oro.'
-L['momos_treasure_chest'] = 'Cofre del Tesoro de Mo-Mo'
-L['momos_treasure_chest_note'] = 'Contiene unos 10 de Oro.'
-L['hozen_treasure_cache'] = 'Alijo de Tesoro Hozen'
-L['hozen_treasure_cache_note'] = 'Contiene unos 99 de Oro.'
-L['rikktiks_tick_remover'] = 'Cofre Diminuto de Rikktik'
-L['stolen_sprite_treasure'] = 'Tesoro de Duende Robado'
-L['stolen_sprite_treasure_note'] = 'Contiene unos 104 de Oro.'
-L['sturdy_yaungol_spear'] = 'Alijo de Armas Yaungol'
-L['sprites_cloth_chest'] = 'Cofre de Tela de Duende'
+L['lost_adventurers_belongings'] = 'Pertenencias del aventurero perdido'
+L['lost_adventurers_belongings_note'] = 'Contiene unos 97 de oro.'
+L['momos_treasure_chest'] = 'Cofre del tesoro de Mo-Mo'
+L['momos_treasure_chest_note'] = 'Contiene unos 10 de oro.'
+L['hozen_treasure_cache'] = 'Alijo de tesoro hozen'
+L['hozen_treasure_cache_note'] = 'Contiene unos 99 de oro.'
+L['rikktiks_tick_remover'] = 'Cofre diminuto de Rikktik'
+L['stolen_sprite_treasure'] = 'Tesoro de duende robado'
+L['stolen_sprite_treasure_note'] = 'Contiene unos 104 de oro.'
+L['sturdy_yaungol_spear'] = 'Alijo de armas yaungol'
+L['sprites_cloth_chest'] = 'Cofre de tela de duende'
 
 -------------------------------------------------------------------------------
 ------------------------------- Townlong Steppes ------------------------------
 -------------------------------------------------------------------------------
 
-L['abandoned_crate_of_goods'] = 'Cajón de Bienes Abandonado'
-L['abandoned_crate_of_goods_note'] = 'Contiene unos 103 de Oro.'
+L['abandoned_crate_of_goods'] = 'Cajón de bienes abandonado'
+L['abandoned_crate_of_goods_note'] = 'Contiene unos 103 de oro.'
 
 -------------------------------------------------------------------------------
 ----------------------------- Valley of Four Winds ----------------------------
 -------------------------------------------------------------------------------
 
-L['cache_of_pilfered_goods'] = 'Alijo de Bienes Trincados'
-L['virmen_treasure_cache'] = 'Alijo del Tesoro Mur'
-L['mysterious_fruit_pile'] = 'Montón de Fruta Misteriosa'
+L['cache_of_pilfered_goods'] = 'Alijo de bienes trincados'
+L['virmen_treasure_cache'] = 'Alijo del tesoro mur'
+L['mysterious_fruit_pile'] = 'Montón de fruta misteriosa'
 
 -------------------------------------------------------------------------------
 ------------------------------- Isle of Thunder -------------------------------
@@ -146,6 +146,6 @@ L['options_icons_kroshik_desc'] = 'Muestra dónde están los sitios para el logr
 
 L['iot_portal'] = 'Portal'
 L['ritualstone_needed'] = 'Necesitas 3 {item:94221} para invocar al raro.'
-L['kroshik_bow'] = '{emote:/reverenciar}'
-L['kroshik_adult'] = 'Corre alrededor del lago.\n{emote:/reverenciar}'
-L['kroshik_baby'] = 'Corre alrededor de la zona.\n{emote:/reverenciar}'
+L['kroshik_bow'] = 'Teclea {emote:/reverenciar}.'
+L['kroshik_adult'] = 'Corre alrededor del lago.\nTeclea {emote:/reverenciar}.'
+L['kroshik_baby'] = 'Corre alrededor de la zona.\nTeclea {emote:/reverenciar}.'
