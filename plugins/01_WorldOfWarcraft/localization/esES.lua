ce8060d91bdac273ae0cc94ac1177b6cfbc31430
Krovikan <krovikan@gmail.com>
2024-09-13 18:11:42 +0200

    Fixes Vanilla esES

--- plugins/01_WorldOfWarcraft/localization/esES.lua
+++ plugins/01_WorldOfWarcraft/localization/esES.lua
@@ -6,15 +6,15 @@ if not L then return end
 ----------------------------------- COMMON ------------------------------------
 -------------------------------------------------------------------------------
 
-L['anniversary_note'] = '{note:¡Sólo disponible durante el eventuo anual de aniversario!}'
+L['anniversary_note'] = '{note:¡Sólo está disponible durante el evento anual de aniversario!}'
 
 L['summer_only'] = 'Aparece sólo en Verano. (Del 21 de Junio al 22 de Septiembre)'
 L['options_icons_safari'] = 'Safari de Kalimdor / Reinos del Este'
-L['options_icons_safari_desc'] = 'Muestra dónde están  las mascotas de combate para los logros {achievement:6585} y {achievement:6586}.'
+L['options_icons_safari_desc'] = 'Muestra dónde están las mascotas de combate para los logros {achievement:6585} y {achievement:6586}.'
 
 L['options_icons_crazyforcats'] = '{achievement:8397}'
-L['options_icons_crazyforcats_desc'] = 'Muestra dónde están  las mascotas de combate para el logro {achievement:8397}.'
-L['cfc_blacktabby'] = 'La {item:8491} se puede despojar de varios enemigos en las {location:Laderas de Trabalomas}.\n\nLos enemigos recomendados son el {npc:49116} (sólo para la Horda), el {npc:48017} o el {npc:2248}.\n\n{note:Posibilidades de caer de un 0.1%.}'
+L['options_icons_crazyforcats_desc'] = 'Muestra dónde están las mascotas de combate para el logro {achievement:8397}.'
+L['cfc_blacktabby'] = 'La {item:8491} se puede despojar de varios enemigos en las {location:Laderas de Trabalomas}.\n\nLos enemigos recomendados son el {npc:49116} (sólo para la Horda), el {npc:48017} o el {npc:2248}.\n\n{note:Posibilidades de caer de un 0,1%.}'
 
 -------------------------------------------------------------------------------
 ----------------------------- ZUL'GURUB (10.0.7) ------------------------------
@@ -24,8 +24,8 @@ L['zg_unlock_note_01'] = '1. {dot:Blue} Coge la {item:203735} de la habitación 
 L['zg_unlock_note_02'] = '2. Derrota al menos a dos Bosses para poder iniciar el combate contra {npc:52148}.\n\nLa {npc:52059} puede soltar la {item:68824}\nEl {npc:52151} puede soltar el {item:68823}.'
 L['zg_unlock_note_03'] = '3. {dot:Red} Comienza el combate contra {npc:52148} y reduce su vida a 1 HP. Te enviará al {spell:96689}.\n\nRegresa a la habitación ahora llena de {npc:52624} en la que tendrás que coger la {item:203736}.'
 L['zg_unlock_note_04'] = '4. Combina la {item:203735} y la {item:203736} para crear la {item:203737} y comenzar la misión {quest:74576}.'
-L['zg_unlock_note_05'] = '5. {quest:74576} se devuelve a {npc:143138} en {location:Intercambio Yojamba} en {location:Dazar\'alor}.'
-L['zg_unlock_note_06'] = '6. Una vez se ha completado {quest:74576}, {npc:143138} estará disponible en la entrada de {location:Zul\'Gurub} y comenzará a vender patrones a cambio de joyas.'
+L['zg_unlock_note_05'] = '5. La misión {quest:74576} se devuelve a {npc:143138} en {location:Intercambio Yojamba} de {location:Dazar\'alor}.'
+L['zg_unlock_note_06'] = '6. Una vez se ha completado la misión {quest:74576}, {npc:143138} estará disponible en la entrada de {location:Zul\'Gurub} y comenzará a vender patrones a cambio de joyas.'
 
 L['zg_zandalari_bijous_note_01'] = 'Las joyas las pueden soltar cualquier enemigo de {location:Zul\'Gurub} con 1 garantizada por cada Boss.'
 L['zg_zandalari_bijous_note_02'] = 'Los Bosses pueden soltar una {item:203774} que contienen 4 joyas.'
@@ -36,25 +36,25 @@ L['zg_fishing_note_02'] = 'Los {object:Alijo} pueden contener varias joyas, mate
 L['zg_fishing_note_03'] = '{note:El {item:203743} necesitará alguna forma de {spell:1804} para abrirlo.}'
 
 L['zg_brazier_of_madness_note'] = 'Está cerca del altar bajo la {object:Tablilla de Locura} en {location:El Extremo de la Locura}.'
-L['zg_gurubashi_mojo_madness_note'] = 'Los Alquimistas con 300 puntos en {object:Alquimia Clásica} pueden interacturar con la {object:Tablilla de Locura} en {location:El Extremo de la Locura} para aprender la receta de la {item:19931}.'
+L['zg_gurubashi_mojo_madness_note'] = 'Los Alquimistas con 300 puntos en {object:Alquimia clásica} pueden interacturar con la {object:Tablilla de Locura} en {location:El Extremo de la Locura} para aprender la receta de la {item:19931}.'
 
 L['zg_offering_note'] = 'Usa {item:19931} junto al {item:203757} para recibir {spell:401721}. Ahora podrás interactuar con los montones de calaveras alrededor de {location:Zul\'Gurub}.\n\nEl montón acepta:'
 
-L['offering_of_fangs_label'] = 'Ofrenda de Colmillos'
+L['offering_of_fangs_label'] = 'Ofrenda de colmillos'
 L['offering_of_fangs_location'] = 'Encima del altar de {location:El Serpenteo}.'
-L['offering_of_blood_label'] = 'Ofrenda de Sangre'
+L['offering_of_blood_label'] = 'Ofrenda de sangre'
 L['offering_of_blood_location'] = 'Cerca del borde de la arena entre dos postes en los {location:Dominios de Mandokir}.'
-L['offering_of_claws_label'] = 'Ofrenda de Zarpas'
+L['offering_of_claws_label'] = 'Ofrenda de zarpas'
 L['offering_of_claws_location'] = 'Dentro de las profundidades del {location:Templo de Bethekk} en la parte trasera de la habitación.'
-L['offering_of_mojo_label'] = 'Ofrenda de Mojo'
+L['offering_of_mojo_label'] = 'Ofrenda de mojo'
 L['offering_of_mojo_location'] = 'Junto al caldero verde en {location:El Altar Demoníaco}.'
 
 L['zg_vendor_note_01'] = 'Intercambias varias joyas y {item:203914} por recetas y transfiguraciones.'
-L['zg_vendor_note_02'] = '{note:Las indumentarias de clase sólo están disponibles para clases que ya estaban en el juego durante los tiempos del {location:Zul\'Gurub} original. Las indumentarias de Conjunto de Armadura las puede usar cualquiera.}'
+L['zg_vendor_note_02'] = '{note:Las indumentarias de clase sólo están disponibles para clases que ya estaban en el juego durante los tiempos del {location:Zul\'Gurub} original. Las indumentarias de Conjunto de armadura las puede usar cualquiera.}'
 L['zg_vendor_note_03'] = 'Las {item:203914} se pueden conseguir reuniendo varias monedas de los enemigos de {location:Zul\'Gurub}.'
 
 L['zg_class_ensembles'] = 'Indumentaria de clase de Zul\'Gurub'
-L['zg_armor_ensembles'] = 'Indumentaria de Conjunto de Armadura de Zul\'Gurub'
+L['zg_armor_ensembles'] = 'Indumentaria de Conjunto de armadura de Zul\'Gurub'
 
 L['options_icons_secrets_of_zulgurub'] = 'Secretos de Zul\'Gurub'
-L['options_icons_secrets_of_zulgurub_desc'] = 'Muestra localizaciones importantes para los {location:Secretos of Zul\'Gurub}.'
+L['options_icons_secrets_of_zulgurub_desc'] = 'Muestra localizaciones importantes para los {location:Secretos de Zul\'Gurub}.'
