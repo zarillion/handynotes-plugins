1f3c407879e15b5306f47d13c976cdfadafc683a
wyldclaw <wyldclawthornbringer@gmail.com>
2024-11-23 02:36:42 -0600

    add 3rd orb

--- plugins/11_TheWarWithin/zones/ratts_revenge.lua
+++ plugins/11_TheWarWithin/zones/ratts_revenge.lua
@@ -17,6 +17,8 @@ local Gray = ns.status.Gray
 local Green = ns.status.Green
 local Red = ns.status.Red
 
+local ItemStatus = ns.tooltip.ItemStatus
+
 -------------------------------------------------------------------------------
 
 -- INERT PECULIAR KEY
@@ -39,6 +41,9 @@ local maldraxxus = ns.maps[1536] or Map({id = 1536, settings = false})
 
 -- 2 O'CLOCK ORB - PRAY
 local valeOfEternalBlossoms = ns.maps[390] or Map({id = 390, settings = false})
+
+-- 3 O'CLOCK ORB - HATE
+local stormwind = ns.maps[84] or Map({id = 84, settings = false})
 
 -------------------------------------------------------------------------------
 
@@ -125,29 +130,6 @@ deadwindPass.nodes[46766907] = RattsRevenge({
 }) -- Karazhan Catacombs
 
 -------------------------------------------------------------------------------
---------------------------- KARAZHAN CATACOMB ORBS ----------------------------
--------------------------------------------------------------------------------
-
-local Orb = Class('Orb', RattsRevenge)
-
-function Orb.getters:rlabel()
-    local completed = C_QuestLog.IsQuestFlaggedCompleted(self.quest[1])
-    return completed and Green(L['completed']) or Red(L['incomplete'])
-end
-
-karazhanCatacombs.nodes[50001500] = Orb({
-    label = L['love_orb_label'],
-    note = L['love_orb_note'] .. '\n\n' .. L['love_orb_locations'],
-    quest = 84676 -- hidden
-}) -- Orb 1
-
-karazhanCatacombs.nodes[50001800] = Orb({
-    label = L['pray_orb_label'],
-    note = L['pray_orb_note'],
-    quest = 84677 -- hidden
-}) -- Orb 2
-
--------------------------------------------------------------------------------
 ---------------------------- 1 O'CLOCK ORB - LOVE -----------------------------
 -------------------------------------------------------------------------------
 
@@ -195,3 +177,174 @@ valeOfEternalBlossoms.nodes[83183011] = RattsRevenge({
     quest = 84677, -- hidden
     rewards = {Item({item = 53156, bag = true})} -- Key of Shadows
 }) -- Ny'alotha Obelisk
+
+-------------------------------------------------------------------------------
+---------------------------- 3 O'CLOCK ORB - HATE -----------------------------
+-------------------------------------------------------------------------------
+
+local CODE_MACHINES = {
+    [1] = {
+        coordinates = 49518030,
+        label = L['decryption_machine_label'],
+        note = L['code_machine_note_1'],
+        quest = 84757 -- hidden
+    },
+    [2] = {
+        coordinates = 49826455,
+        label = L['decryption_console_label'],
+        note = L['code_machine_note_2'],
+        quest = 84768 -- hidden
+    },
+    [3] = {
+        coordinates = 56096311,
+        label = L['decryption_console_label'],
+        note = L['code_machine_note_3'],
+        quest = 84769 -- hidden
+    },
+    [4] = {
+        coordinates = 64954877,
+        label = L['rubensteins_console'],
+        note = L['code_machine_note_4'],
+        quest = 84766 -- hidden
+    },
+    [5] = {
+        coordinates = 70445463,
+        label = L['decryption_console_label'],
+        note = L['code_machine_note_5'],
+        quest = 84772 -- hidden
+    },
+    [6] = {
+        coordinates = 66191582,
+        label = L['decryption_console_label'],
+        note = L['code_machine_note_6'],
+        quest = 84770 -- hidden
+    },
+    [7] = {
+        coordinates = 42837101,
+        label = L['decryption_console_label'],
+        note = L['code_machine_note_7'],
+        quest = 84758 -- hidden
+    },
+    [8] = {
+        coordinates = 67978284,
+        label = L['decryption_console_label'],
+        note = L['code_machine_note_8'],
+        quest = 84771 -- hidden
+    }
+}
+
+local HATE_QUESTS = {
+    84757, 84768, 84769, 84766, 84772, 84770, 84758, 84771, 84786
+}
+
+karazhanCatacombs.nodes[51187830] = RattsRevenge({
+    label = L['astral_soup_label'],
+    note = L['astral_rewards_note'],
+    quest = HATE_QUESTS, -- hidden
+    rewards = {Item({item = 228965, bag = true})}, -- Astral Key
+    rlabel = Gray('1/2')
+}) -- Astral Soup
+
+karazhanCatacombs.nodes[48187996] = RattsRevenge({
+    label = L['astral_chest_label'],
+    note = L['astral_rewards_note'],
+    quest = HATE_QUESTS, -- hidden
+    rewards = {Item({item = 228966, bag = true})}, -- Starry-Eyed Goggles
+    requires = ns.requirement.Item(228965), -- Astral Key
+    rlabel = Gray('2/2')
+}) -- Astral Chest
+
+local PieceOfHate = Class('PieceOfHate', Item, {item = 228967})
+
+function PieceOfHate:GetStatus()
+    local count = C_Item.GetItemCount(self.item, true)
+    return count >= 9 and Green(count .. '/9') or Red(count .. '/9')
+end
+
+for num, machine in ipairs(CODE_MACHINES) do
+    karazhanCatacombs.nodes[machine.coordinates] = RattsRevenge({
+        icon = 'peg_gn',
+        label = machine.label,
+        note = machine.note,
+        quest = machine.quest,
+        requires = ns.requirement.Spell(463749), -- Starry-Eyed Goggles
+        rewards = {PieceOfHate()},
+        rlabel = Gray(num .. '/9')
+    })
+end
+
+local SlotMachine = Class('SlotMachine', RattsRevenge, {
+    icon = 'peg_gn',
+    label = L['slot_machine_label'],
+    quest = 84786, -- hidden
+    requires = ns.requirement.Spell(463749), -- Starry-Eyed Goggles
+    rewards = {PieceOfHate()}, -- Piece of Hate
+    rlabel = Gray('9/9')
+}) -- Slot Machine
+
+function SlotMachine.getters:note()
+    local note = L['slot_machine_note_a']
+    note = note .. L['slot_machine_note_b']
+    note = note .. ItemStatus(138384, 1, '{item:138384}', false) .. '\n' -- Lucky Charm
+    note = note .. ItemStatus(138382, 1, '{item:138382}', false) .. '\n' -- Lucky Rat's Tooth
+    note = note .. ItemStatus(138385, 1, '{item:138385}', false) .. '\n\n' -- Lucky Shirt
+    note = note .. L['slot_machine_note_c']
+    note = note .. ItemStatus(200265, 1, '{item:200265}', false) .. '\n' -- Lucky Dragon's Claw
+    note = note .. ItemStatus(202046, 1, '{item:202046}', false) .. '\n\n' -- Lucky Tortollan Charm
+    note = note .. L['slot_machine_note_d']
+    note = note .. ItemStatus(198400, 1, '{item:198400}:', false) .. '\n\n' -- Lucky Horsehoe
+    note = note .. L['slot_machine_note_e']
+    return note
+end
+
+karazhanCatacombs.nodes[63792841] = SlotMachine()
+
+stormwind.nodes[28692781] = RattsRevenge({
+    label = '{npc:108533}',
+    note = L['slot_machine_vendor_note'],
+    quest = 84786, -- hidden
+    rewards = {
+        Item({item = 138384, bag = true}), -- Lucky Charm
+        Item({item = 138382, bag = true}), -- Lucky Rat's Tooth
+        Item({item = 138385, bag = true}) -- Lucky Shirt
+    }
+}) -- Lenny "Fingers" McCoy
+
+dornogal.nodes[62555095] = RattsRevenge({
+    label = '{npc:219197}',
+    note = L['slot_machine_vendor_note'],
+    quest = 84786, -- hidden
+    rewards = {
+        Item({item = 200265, bag = true}), -- Lucky Dragon's Claw
+        Item({item = 202046, bag = true}) -- Lucky Tortollan Charm
+    }
+}) -- Griftah
+
+-------------------------------------------------------------------------------
+--------------------------- KARAZHAN CATACOMB ORBS ----------------------------
+-------------------------------------------------------------------------------
+
+local Orb = Class('Orb', RattsRevenge, {icon = 'peg_yw'})
+
+function Orb.getters:rlabel()
+    local completed = C_QuestLog.IsQuestFlaggedCompleted(self.quest[1])
+    return completed and Green(L['completed']) or Red(L['incomplete'])
+end
+
+karazhanCatacombs.nodes[50001500] = Orb({
+    label = L['love_orb_label'],
+    note = L['love_orb_note'] .. '\n\n' .. L['love_orb_locations'],
+    quest = 84676 -- hidden
+}) -- Orb 1
+
+karazhanCatacombs.nodes[50001900] = Orb({
+    label = L['pray_orb_label'],
+    note = L['pray_orb_note'],
+    quest = 84677 -- hidden
+}) -- Orb 2
+
+karazhanCatacombs.nodes[50002300] = Orb({
+    label = L['hate_orb_label'],
+    note = L['astral_rewards_note'],
+    quest = HATE_QUESTS -- hidden
+}) -- Orb 3
