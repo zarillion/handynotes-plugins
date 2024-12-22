63de7d7fc0066c79b1dcc8ef8403f845e9cd25ca
wyldclaw <wyldclawthornbringer@gmail.com>
2024-12-21 23:06:50 -0600

    initial commit

---
+++ plugins/11_TheWarWithin/zones/undermine.lua
@@ -0,0 +1,41 @@
+-------------------------------------------------------------------------------
+---------------------------------- NAMESPACE ----------------------------------
+-------------------------------------------------------------------------------
+local ADDON_NAME, ns = ...
+local Class = ns.Class
+-- local L = ns.locale
+local Map = ns.Map
+
+local Collectible = ns.node.Collectible
+local Rare = ns.node.Rare
+local Treasure = ns.node.Treasure
+-- local Vendor = ns.node.Vendor
+
+local Achievement = ns.reward.Achievement
+-- local Item = ns.reward.Item
+-- local Mount = ns.reward.Mount
+-- local Pet = ns.reward.Pet
+-- local Toy = ns.reward.Toy
+-- local Transmog = ns.reward.Transmog
+
+-- local Circle = ns.poi.Circle
+-- local Entrance = ns.poi.Entrance
+-- local Path = ns.poi.Path
+-- local POI = ns.poi.POI
+
+-- local QuestStatus = ns.tooltip.QuestStatus
+
+-------------------------------------------------------------------------------
+
+local map = Map({id = 9999, patch = 111000, settings = true}) -- Undermine
+
+-------------------------------------------------------------------------------
+------------------------------------ RARES ------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+---------------------------------- TREASURES ----------------------------------
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-------------------------------- MISCELLEANOUS --------------------------------
+-------------------------------------------------------------------------------
