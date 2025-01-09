9852f6ba6b5381cf73ba254cac098af9cb1cf12a
wyldclaw <wyldclawthornbringer@gmail.com>
2025-01-08 19:36:55 -0600

    clean up locals

--- plugins/11_TheWarWithin/zones/undermine.lua
+++ plugins/11_TheWarWithin/zones/undermine.lua
@@ -7,23 +7,20 @@ local L = ns.locale
 local Map = ns.Map
 
 local Collectible = ns.node.Collectible
-local Rare = ns.node.Rare
+-- local Rare = ns.node.Rare
 local Treasure = ns.node.Treasure
--- local Vendor = ns.node.Vendor
 
 local Achievement = ns.reward.Achievement
 local Item = ns.reward.Item
--- local Mount = ns.reward.Mount
--- local Pet = ns.reward.Pet
--- local Toy = ns.reward.Toy
--- local Transmog = ns.reward.Transmog
+local Pet = ns.reward.Pet
+local Toy = ns.reward.Toy
+local Transmog = ns.reward.Transmog
 
--- local Circle = ns.poi.Circle
 local Entrance = ns.poi.Entrance
--- local Path = ns.poi.Path
--- local POI = ns.poi.POI
+local Path = ns.poi.Path
+local POI = ns.poi.POI
 
--- local QuestStatus = ns.tooltip.QuestStatus
+local ItemStatus = ns.tooltip.ItemStatus
 
 -------------------------------------------------------------------------------
 
