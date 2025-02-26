39e4ef512e782f78a3dab408e256ab8cdddb60ec
Krovikan <krovikan@gmail.com>
2025-02-26 13:13:37 +0100

    Updated Timeless Chest MP esES

--- plugins/05_MistsOfPandaria/localization/esES.lua
+++ plugins/05_MistsOfPandaria/localization/esES.lua
@@ -44,8 +44,8 @@ L['skull_covered_chest'] = 'Cofre adornado de calaveras'
 L['smoldering_chest'] = 'Cofre humeante'
 L['sturdy_chest'] = 'Cofre robusto'
 L['sturdy_chest_note'] = 'Golpea a un {npc:73531} para ser llevado aquí.'
-L['timeless_chest'] = nil
-L['timeless_chest_note'] = nil
+L['timeless_chest'] = 'Cofre intemporal'
+L['timeless_chest_note'] = 'Puedes comprar la llave al {npc:72007} por 500x {currency:777}.'
 
 L['gleaming_treasure_satchel_note'] = 'Camina por las cuerdas del barco y salta al poste donde está colgando la cartera.'
 L['gleaming_treasure_chest_note'] = 'Salta por los pilares hasta llegar al tesoro.'
