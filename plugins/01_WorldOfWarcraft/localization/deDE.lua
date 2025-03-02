3899d723b76ba59c1fa13301911847f2a3a7caa4
Dathwada <dathwada@gmail.com>
2025-03-02 20:14:43 +0100

    Fix loading of deDE

--- plugins/01_WorldOfWarcraft/localization/deDE.lua
+++ plugins/01_WorldOfWarcraft/localization/deDE.lua
@@ -1,5 +1,5 @@
 local ADDON_NAME, ns = ...
-local L = ns.NewLocale('enUS')
+local L = ns.NewLocale('deDE')
 if not L then return end
 -- German translation by Dathwada EU-Eredar
 -------------------------------------------------------------------------------
