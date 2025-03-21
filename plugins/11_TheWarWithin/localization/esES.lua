77801f2fd79cb3d490d565a908c6b1ef193562f6
Krovikan <krovikan@gmail.com>
2025-03-21 19:25:10 +0100

    Updated 3 Hunter pets esES

--- plugins/11_TheWarWithin/localization/esES.lua
+++ plugins/11_TheWarWithin/localization/esES.lua
@@ -636,6 +636,6 @@ L['undermine_clam_chowder_label'] = 'La guía de la auténtica cocina de Minahon
 L['undermine_clam_chowder_location'] = 'Encima de un estante detrás de {npc:226728} en el callejón de la {location:Estación de Vapor}.'
 L['undermine_clam_chowder_note'] = '1. Para conseguir esta receta, primero debes saber {item:16767} que se puede comprar en dos vendedores {object:Clásicos} por %s.\n\n{npc:54232} de {location:Bahía del Botín} en {location:El Cabo de Tuercespina}\n\n{npc:48060} de {location:El Diodo} en las {location:Tierras Inhóspitas}\n\n2. Una vez que sepas la vieja receta, abre {object:La guía de la auténtica cocina de Minahonda} y lee la última página. Anota la receta para conseguir la {item:235800}.'
 
-L['blazefeather_peacock_note'] = nil
-L['radioactive_subject_note'] = nil
-L['george_the_big_pinch_note'] = nil
+L['blazefeather_peacock_note'] = 'Después de completar la serie de misiones {quest:83421}, habla con {npc:226555} para pagar %s por invocar un {npc:226561} entrenable.'
+L['radioactive_subject_note'] = 'Usa las tuberías cercanas y, combinando {item:237276}, {item:109076}, {item:231099} o cualquier otro truco de rompecabezas, salta al techo del edificio de los {location:Laboratorios Bonvapor} en {location:Estación de Vapor}.'
+L['george_the_big_pinch_note'] = 'Haz click rápidamente en los 5 {npc:238189} para conseguir {spell:1219679} que te permitirá domesticar {npc:233938}.'
