d196f553639f11794a02be84cc002c38b4a21c88
Krovikan <krovikan@gmail.com>
2025-08-10 11:45:33 +0200

    Updated Phase-Lost-and-Found esES

--- plugins/11_TheWarWithin/localization/esES.lua
+++ plugins/11_TheWarWithin/localization/esES.lua
@@ -660,16 +660,16 @@ L['options_icons_karesh_lore_hunter'] = '{achievement:60890}'
 L['options_icons_karesh_lore_hunter_desc'] = 'Muestra dónde están los objetos históricos del logro {achievement:60890}.'
 
 L['options_icons_phase_lost_and_found'] = '{achievement:61017}'
-L['options_icons_phase_lost_and_found_desc'] = nil
-L['phaseorb_label'] = nil
-L['phaseorb_note'] = nil
+L['options_icons_phase_lost_and_found_desc'] = 'Muestra dónde están los Orbes de buceo de fase para el logro {achievement:61017}.'
+L['phaseorb_label'] = 'Orbe de buceo de fase'
+L['phaseorb_note'] = 'Pasa a través del Orbe de buceo de fase para tener una probabilidad de conseguir una de las transfiguraciones.'
 
 L['options_icons_purrkins'] = '{achievement:42729}'
 L['options_icons_purrkins_desc'] = 'Muestra dónde están los ronroneadores para el logro {achievement:42729}.'
 L['purrkins_note'] = 'Teclea {emote:/acariciar} en el ronroneador.'
-L['kareshire_sublabel'] = 'Cambia de fases'
+L['kareshire_sublabel'] = 'En fase o fuera de fase'
 L['in_a_tree'] = 'Encima del árbol.'
-L['little_ms_phaser_sublabel'] = 'Cambia de fases'
+L['little_ms_phaser_sublabel'] = 'En fase o fuera de fase'
 
 L['mailroom_distribution_note'] = 'Interactúa con el carro de fuera y abre la puerta de la sala de correo. Párate en los círculos y usa el botón de acción adicional. El tesoro aparecerá cerca de la puerta.'
 L['gift_of_the_brothers_note'] = 'Interactúa con el {object:Farol chisporroteante} y habla con {npc:233881}. Después ves a buscar a {npc:234112} ({dot:Red}), {npc:234113} ({dot:Yellow}) y {npc:234075} ({dot:Green}). Por último, vuelve a {npc:233881} en el {object:Farol chisporroteante}.'
