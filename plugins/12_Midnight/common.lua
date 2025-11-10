3c9f4e095781459cdc81c1ab9674741a7b84c73b
Ioney <dominik.hier@gmail.com>
2025-11-10 12:37:10 +0100

    beta prep. map ids from wago.tools

---
+++ plugins/12_Midnight/common.lua
@@ -0,0 +1,122 @@
+-------------------------------------------------------------------------------
+---------------------------------- NAMESPACE ----------------------------------
+-------------------------------------------------------------------------------
+local ADDON_NAME, ns = ...
+local L = ns.locale
+
+local Class = ns.Class
+local Group = ns.Group
+
+-- local Collectible = ns.node.Collectible
+
+-- local Achievement = ns.reward.Achievement
+
+-------------------------------------------------------------------------------
+
+ns.expansion = 12
+
+-------------------------------------------------------------------------------
+----------------------------------- GROUPS ------------------------------------
+-------------------------------------------------------------------------------
+
+ns.groups.DELVE_REWARDS = Group('delve_rewards', 4203076, {
+    defaults = ns.GROUP_HIDDEN,
+    type = ns.group_types.EXPANSION,
+    HasEnabledNodes = function() return true end
+})
+
+ns.groups.PROFESSION_TREASURES = Group('profession_treasures', 4620676, {
+    defaults = ns.GROUP_HIDDEN,
+    type = ns.group_types.EXPANSION
+})
+
+ns.groups.SKYRIDING_GLYPH = Group('skyriding_glyph', 4728198, {
+    defaults = ns.GROUP_HIDDEN,
+    type = ns.group_types.EXPANSION
+})
+
+-------------------------------------------------------------------------------
+
+-- ns.groups.SAFARI = Group('safari', 4048818, {
+--     defaults = ns.GROUP_HIDDEN,
+--     type = ns.group_types.ACHIEVEMENT,
+--     achievement = 40194
+-- })
+
+-------------------------------------------------------------------------------
+------------------------------- SKYRIDING GLYPH -------------------------------
+-------------------------------------------------------------------------------
+
+local SkyridingGlyph = Class('SkyridingGlyph', Collectible, {
+    icon = 4728198,
+    label = L['skyriding_glyph'],
+    group = ns.groups.SKYRIDING_GLYPH
+})
+
+ns.node.SkyridingGlyph = SkyridingGlyph
+
+-------------------------------------------------------------------------------
+----------------------------- PROFESSION TREASURES ----------------------------
+-------------------------------------------------------------------------------
+
+-- local ProfessionMaster = Class('ProfessionMaster', ns.node.NPC, {
+--     scale = 0.9,
+--     group = ns.groups.PROFESSION_TREASURES
+-- })
+
+-- function ProfessionMaster:IsEnabled()
+--     if not ns.PlayerHasProfession(self.skillID) then return false end
+--     return ns.node.NPC.IsEnabled(self)
+-- end
+
+-- local ProfessionTreasure = Class('ProfessionTreasure', ns.node.Item, {
+--     scale = 0.9,
+--     group = ns.groups.PROFESSION_TREASURES
+-- })
+
+-- function ProfessionTreasure:IsEnabled()
+--     if not ns.PlayerHasProfession(self.skillID) then return false end
+--     return ns.node.Item.IsEnabled(self)
+-- end
+
+-- ns.node.ProfessionMasters = {}
+-- ns.node.ProfessionTreasures = {}
+
+-- local PM = ns.node.ProfessionMasters
+-- local PT = ns.node.ProfessionTreasures
+
+-- for _, profession in pairs(ns.professions) do
+--     if profession.variantID ~= nil then
+--         local name = profession.name
+--         local icon = profession.icon
+--         local skillID = profession.skillID
+--         local variantID = profession.variantID[11]
+
+--         PM[name] = Class(name .. 'Master', ProfessionMaster, {
+--             icon = icon,
+--             skillID = skillID,
+--             requires = ns.requirement.Profession(skillID, variantID, 1)
+--         })
+
+--         PT[name] = Class(name .. 'Treasure', ProfessionTreasure, {
+--             icon = icon,
+--             skillID = skillID,
+--             requires = ns.requirement.Profession(skillID, variantID, 1)
+--         })
+--     end
+-- end
+
+-------------------------------------------------------------------------------
+------------------------------- MIDNIGHT SAFARI -------------------------------
+-------------------------------------------------------------------------------
+
+-- local Safari = Class('Safari', Collectible,
+--     {icon = 'paw_g', group = ns.groups.SAFARI}) -- Midnight Safari
+
+-- ns.node.Safari = Safari
+
+-- map.nodes[0000] = Safari({
+--     id = 222071,
+--     rewards = {Achievement({id = nil, criteria = nil}), Pet({id = nil})}
+--     -- pois = {POI({0000})}
+-- }) -- Critter Name
