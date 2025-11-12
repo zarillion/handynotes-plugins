0dbe941fc3a58dd32275b1cf07e4e86ac6c8eadd
Krovikan <krovikan@gmail.com>
2025-11-12 11:41:39 +0100

    Update esES.lua

--- plugins/06_WarlordsOfDraenor/localization/esES.lua
+++ plugins/06_WarlordsOfDraenor/localization/esES.lua
@@ -1,7 +1,7 @@
 local ADDON_NAME, ns = ...
 local L = ns.NewLocale('esES')
 if not L then return end
--- Spanish Translation by (EU) Krovikan-Minahonda
+-- Spanish Translation by Krovikan EU-Minahonda
 -------------------------------------------------------------------------------
 -------------------------------- ACROSS ZONES ---------------------------------
 -------------------------------------------------------------------------------
