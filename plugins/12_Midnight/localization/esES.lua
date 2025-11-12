e655b1f0019a079dbeba4e7b61c539748b1f24bd
Krovikan <krovikan@gmail.com>
2025-11-12 11:33:27 +0100

    Updated esES.lua

--- plugins/12_Midnight/localization/esES.lua
+++ plugins/12_Midnight/localization/esES.lua
@@ -1,4 +1,4 @@
 local ADDON_NAME, ns = ...
 local L = ns.NewLocale('esES')
 if not L then return end
--- Spanish Translation by (EU) Krovikan-Minahonda
+-- Spanish Translation by Krovikan EU-Minahonda
