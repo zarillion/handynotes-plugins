e4b19ef924947f04578c2b373c247cf6d5a354b3
Krovikan <krovikan@gmail.com>
2025-11-16 11:49:03 +0100

    Updated Midnight esES

--- plugins/12_Midnight/localization/esES.lua
+++ plugins/12_Midnight/localization/esES.lua
@@ -2,7 +2,6 @@ local ADDON_NAME, ns = ...
 local L = ns.NewLocale('esES')
 if not L then return end
 -- Spanish Translation by Krovikan EU-Minahonda
-
 -------------------------------------------------------------------------------
 ------------------------------------ COMMON -----------------------------------
 -------------------------------------------------------------------------------
@@ -11,20 +10,20 @@ L['skyriding_glyph'] = 'Glifo de Surcacielos'
 L['options_icons_skyriding_glyph'] = 'Glifos de Surcacielos'
 L['options_icons_skyriding_glyph_desc'] = 'Muestra dónde están los glifos de surcacielos.'
 
-L['midnight_telescope'] = nil
-L['telescopes_placed'] = nil
+L['midnight_telescope'] = 'Telescopio'
+L['telescopes_placed'] = 'Telescopios puestos'
 L['options_icons_telescope'] = '{achievement:62057}'
-L['options_icons_telescope_desc'] = nil
+L['options_icons_telescope_desc'] = 'Muestra dónde están los 10 telescopios para el logro {achievement:62057}.'
 
 L['options_icons_midnight_lore_hunter'] = '{achievement:62104}'
-L['options_icons_midnight_lore_hunter_desc'] = nil
+L['options_icons_midnight_lore_hunter_desc'] = 'Muestra dónde están los objetos de conocimiento para el logro {achievement:62104}.'
 
 -------------------------------------------------------------------------------
 -------------------------------- EVERSONG WOODS --------------------------------
 -------------------------------------------------------------------------------
 
-L['eversong_woods_stone_vat_of_wine_note'] = nil
-L['eversong_woods_stone_vat_of_wine_location'] = nil
+L['eversong_woods_stone_vat_of_wine_note'] = 'Requiere 10x {item:256232} y 1x {item:256397} de {npc:251405}.\n\nPisa las uvas y luego añade la levadura.'
+L['eversong_woods_stone_vat_of_wine_location'] = 'En una plataforma voladora.'
 
 -------------------------------------------------------------------------------
 ----------------------------------- HARANDAR -----------------------------------
