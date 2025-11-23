a41541d93ae744d5cea05bf986812b04cfe61d84
Ioney <dominik.hier@gmail.com>
2025-11-24 00:08:30 +0100

    added arcantina

---
+++ plugins/12_Midnight/zones/arcantina.lua
@@ -0,0 +1,36 @@
+-------------------------------------------------------------------------------
+---------------------------------- NAMESPACE ----------------------------------
+-------------------------------------------------------------------------------
+local ADDON_NAME, ns = ...
+local Map = ns.Map
+
+local Item = ns.node.Item
+
+-------------------------------------------------------------------------------
+
+local map = Map({id = 2541, settings = true})
+
+-------------------------------------------------------------------------------
+
+map.nodes[55264393] = Item({icon = 134301, id = 213202, quest = 79550}) -- Explorer Pepe
+
+map.nodes[44966051] = Item({icon = 134721, id = 6662, quest = 92349}) -- Giant Growth Potion
+map.nodes[28433959] = Item({icon = 134721, id = 6662, quest = 92350}) -- Giant Growth Potion
+map.nodes[18534417] = Item({icon = 134721, id = 6662, quest = 92351}) -- Giant Growth Potion
+
+map.nodes[61315823] = Item({icon = 134757, id = 124640, quest = 92352}) -- Inky Black Potion
+map.nodes[39688083] = Item({icon = 134757, id = 124640, quest = 92553}) -- Inky Black Potion
+map.nodes[36575489] = Item({icon = 134757, id = 124640, quest = 92354}) -- Inky Black Potion
+
+map.nodes[36688028] = Item({icon = 134301, id = 40195, quest = 92355}) -- Pygmy Oil
+map.nodes[33621257] = Item({icon = 134301, id = 40195, quest = 92356}) -- Pygmy Oil
+map.nodes[74746281] = Item({icon = 134301, id = 40195, quest = 32957}) -- Pygmy Oil
+
+map.nodes[54494478] = Item({icon = 1033182, id = 112384, quest = 92358}) -- Reflecting Prism
+map.nodes[42224258] = Item({icon = 1033182, id = 112384, quest = 92359}) -- Reflecting Prism
+map.nodes[79124579] = Item({icon = 1033182, id = 112384, quest = 92360}) -- Reflecting Prism
+
+map.nodes[35467845] = Item({icon = 134302, id = 6657, quest = 92361}) -- Savory Deviate Delight
+map.nodes[35343007] = Item({icon = 134302, id = 6657, quest = 92362}) -- Savory Deviate Delight
+map.nodes[68055216] = Item({icon = 134302, id = 6657, quest = 92363}) -- Savory Deviate Delight
+
