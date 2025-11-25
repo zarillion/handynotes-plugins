b3c505a01c540c902dd129a30eec29fd6de61cd5
Lightuky <47103897+Lightuky@users.noreply.github.com>
2025-11-25 19:51:56 +0100

    Update frFR.lua - Phoenix/Safebox - MN

--- plugins/12_Midnight/localization/frFR.lua
+++ plugins/12_Midnight/localization/frFR.lua
@@ -25,8 +25,8 @@ L['options_icons_midnight_lore_hunter_desc'] = 'Afficher les emplacements des ob
 
 L['eversong_woods_stone_vat_of_wine_note'] = 'Requiert 10x {item:256232} et 1x {item:256397} acheté auprès de {npc:251405}.\n\nPiétinez les Grappes de raisin puis ajoutez la Levure.'
 L['on_flying_platform'] = 'Sur une plate-forme volante.'
-L['gift_of_the_phoenix_note'] = nil
-L['triple_locked_safebox_note'] = nil
+L['gift_of_the_phoenix_note'] = 'Attrapez 5x Cendres de phénix tombant au sol après avoir interagi avec le {object:Réceptacle de Haut-Soleil}.'
+L['triple_locked_safebox_note'] = 'Requiert 3x clés de coffre-fort cachées. Prenez la {object:Torche ardente} à côté du coffre pour les faire apparaître.'
 
 -------------------------------------------------------------------------------
 ----------------------------------- HARANDAR -----------------------------------
