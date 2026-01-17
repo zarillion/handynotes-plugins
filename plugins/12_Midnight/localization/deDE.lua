9fdd85cd458fa83900d4fe56112eed6514ca8f73
Dathwada <dathwada@gmail.com>
2026-01-17 13:54:40 +0100

    Update deDE.lua

--- plugins/12_Midnight/localization/deDE.lua
+++ plugins/12_Midnight/localization/deDE.lua
@@ -26,52 +26,52 @@ L['options_icons_profession_treasures_desc'] = 'Zeigt die Positionen der Schätz
 -------------------------------------------------------------------------------
 
 L['options_icons_ever_painting'] = '{achievement:62185}'
-L['options_icons_ever_painting_desc'] = nil
+L['options_icons_ever_painting_desc'] = 'Zeigt die Positionen der Staffeleien für den Erfolg {achievement:62185} an.'
 L['options_icons_runestone_rush'] = '{achievement:61961}'
-L['options_icons_runestone_rush_desc'] = nil
+L['options_icons_runestone_rush_desc'] = 'Zeigt die Positionen der Runensteine für den Erfolg {achievement:61961} an.'
 
 L['eversong_woods_stone_vat_of_wine_note'] = 'Benötigt 10 {item:256232} und {item:256397} von {npc:251405}.\n\nStampfe die Trauben und gib dann die Hefe hinzu.'
 L['on_flying_platform'] = 'Auf einer fliegenden Plattform.'
-L['gift_of_the_phoenix_note'] = nil
-L['triple_locked_safebox_note'] = nil
-L['incomplete_book_of_sonnets'] = nil
+L['gift_of_the_phoenix_note'] = 'Klicke den Schatz an und fange 5 mal eine Glut, die der erschienene {npc:255241} fallen lässt.'
+L['triple_locked_safebox_note'] = 'Nehme die {object:Brennende Fackel} neben der Truhe, um die drei benötigten Truhenschlüssel sehen zu können. Sammle die Schlüssel ein, um die Truhe öffnen zu können.'
+L['incomplete_book_of_sonnets'] = 'Unvollständiges Buch der Sonnete'
 
 -------------------------------------------------------------------------------
 ----------------------------------- HARANDAR -----------------------------------
 -------------------------------------------------------------------------------
 
-L['glowing_moth'] = nil
-L['options_icons_glowing_moth'] = nil
-L['options_icons_glowing_moth_desc'] = nil
+L['glowing_moth'] = 'Leuchtende Motte'
+L['options_icons_glowing_moth'] = 'Leuchtende Motte'
+L['options_icons_glowing_moth_desc'] = 'Zeigt die Positionen der leuchtenden Motten für den Erfolg {achievement:61052} an.'
 
-L['sporespawned_cache_note'] = nil
-L['fungal_mallet'] = nil
-L['impenatrably_sealed_gourd_note'] = nil
+L['sporespawned_cache_note'] = 'Interagiere mit dem {dot:Red}{object:Pilzhammer} in Fungara Village, um den Stärkungszauber {spell:1266347} zu erhalten, und benutze ihn, um den {object:Myzelgong} zu läuten. Der Schatz erscheint daneben.'
+L['fungal_mallet'] = 'Pilzhammer'
+L['impenatrably_sealed_gourd_note'] = 'Sammle die {item:260251} vom nahegelegenen {object:Baumelnder Krug} und die {item:260250} vom nahegelegenen {object:Hängendes Fläschen}. Interagiere mit {object:Robuste Vase} und vermische die Flüssigkeiten, um die {item:260266} zu erhalten. Interagiere mit dem Schatz, um die Belohnung zu erhalten.'
 
 L['options_icons_more_than_just_thier_roots'] = '{achievement:62188}'
-L['options_icons_more_than_just_thier_roots_desc'] = 'Zeigt die Positionen der NPCs für den Erfolg {achievement:62188}.'
-L['more_than_just_thier_roots_note'] = nil
+L['options_icons_more_than_just_thier_roots_desc'] = 'Zeigt die Positionen der NPCs für den Erfolg {achievement:62188} an.'
+L['more_than_just_thier_roots_note'] = 'Spreche mit dem jeweiligen NPC an den unten aufgeführten Orten.'
 
 -------------------------------------------------------------------------------
 ---------------------------------- VOIDSTORM ----------------------------------
 -------------------------------------------------------------------------------
 
-L['bloody_sack_note'] = 'Sammle {object:Dripping Meat} von nahegelegenen Knochenhaufen, um die {object:Forgotten Oubliette} zu füttern.' -- TODO: object translation
-L['malignant_chest_note'] = 'Aktiviere die {object:Malignant Nodes} in der Höhle, um die Truhe zu öffnen.' -- TODO: object translation
-L['malignant_node'] = nil
+L['bloody_sack_note'] = 'Interagiere mit der {object:Vergessenen Oubliette} und sammle dann {object:Triefendes Fleisch} von nahegelegenen Knochenhaufen, um die {object:Vergessene Oubliette} zu füttern.'
+L['malignant_chest_note'] = 'Aktiviere die {object:Heimtückischer Knoten} in der Höhle, um die Truhe zu öffnen.'
+L['malignant_node'] = 'Heimtückischer Knoten'
 
 -------------------------------------------------------------------------------
 ---------------------------------- ZUL'AMAN ----------------------------------
 -------------------------------------------------------------------------------
 
 L['options_icons_frog_princess'] = '{achievement:62201}'
-L['options_icons_frog_princess_desc'] = nil
+L['options_icons_frog_princess_desc'] = 'Zeige die Positionen der Frösche für den Erfolg {achievement:62201} an.'
 
 L['options_icons_song_seeker'] = '{achievement:61455}'
-L['options_icons_song_seeker_desc'] = nil
+L['options_icons_song_seeker_desc'] = 'Zeige die Positionen der Liedsucher für den Erfolg {achievement:61455} an.'
 
 L['options_icons_spiritpaw_marathon'] = '{achievement:62202}'
-L['options_icons_spiritpaw_marathon_desc'] = nil
+L['options_icons_spiritpaw_marathon_desc'] = 'Zeige die Positionen der Läufer für den Erfolg {achievement:62202} an.'
 L['spiritpaw_marathon_note'] = 'Sprich mit {npc:258938}.'
 
 -------------------------------------------------------------------------------
