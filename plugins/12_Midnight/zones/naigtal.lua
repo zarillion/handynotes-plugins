cc37b60dadd099d07cd64192276f2e0040c0eab5
Adavak <adavak@adavak.com>
2026-06-20 12:27:09 +0800

    Add initial Naigtal and Val zones for 12.0.7
    Add zone files for two new Lingering Shadows zones:
    
    Naigtal (UiMap 3075):
    - 8 rares with coordinates/NPC IDs/transmog drops
    - Warbringer Thal'kuur (Heroic Slugger only)
    - Voidwarped Sporebat (no achievement tracking)
    - 3 Squirming Mollusk treasure spawns
    - Ancient Crypt Reliquary and Hal'hadar Pocket-Storage (commented, coords unknown)
    
    Val (UiMap 3047):
    - 4 known rares with coordinates
    - 6 rares with NPC IDs only (commented, coords unknown)
    - Dominaar Storage Vessel (commented, random spawn)
    
    Achievements covered: Showdown Slugger (Naigtal 62883, Val 62881)
    and Heroic Slugger (63348) with criteria IDs from DB2.

---
+++ plugins/12_Midnight/zones/naigtal.lua
@@ -0,0 +1,179 @@
+-------------------------------------------------------------------------------
+---------------------------------- NAMESPACE ----------------------------------
+-------------------------------------------------------------------------------
+local ADDON_NAME, ns = ...
+local L = ns.locale
+local Map = ns.Map
+
+local Rare = ns.node.Rare
+local RareElite = ns.node.RareElite
+local Treasure = ns.node.Treasure
+local Achievement = ns.reward.Achievement
+local Transmog = ns.reward.Transmog
+
+-------------------------------------------------------------------------------
+
+local map = Map({id = 3075, settings = true}) -- Naigtal
+
+-------------------------------------------------------------------------------
+------------------------------------ RARES ------------------------------------
+-------------------------------------------------------------------------------
+
+map.nodes[28806290] = Rare({
+    id = 264569,
+    quest = 96316,
+    rewards = {
+        Achievement({id = 62883, criteria = 114009}), -- Showdown Slugger: Naigtal
+        Achievement({id = 63348, criteria = 115253}), -- Heroic Slugger
+        Transmog({item = 275167, type = L['plate']}), -- Intact Construct Plates
+        Transmog({item = 275159, type = L['mail']}), -- Construct Manipulator Bracers
+        Transmog({item = 275151, type = L['leather']}), -- Bands of Draenic Mourning
+        Transmog({item = 275143, type = L['cloth']}), -- Draeni Ceremonial Cuffs
+    }
+}) -- Auredar's Chassis <The Vacant Vigilant>
+
+map.nodes[44205100] = Rare({
+    id = 263950,
+    quest = 96206,
+    rewards = {
+        Achievement({id = 62883, criteria = 114006}), -- Showdown Slugger: Naigtal
+        Achievement({id = 63348, criteria = 115250}), -- Heroic Slugger
+        Transmog({item = 275162, type = L['plate']}), -- Spore-Shined Gloves
+        Transmog({item = 275154, type = L['mail']}), -- Swamp Trekker's Grips
+        Transmog({item = 275146, type = L['leather']}), -- Spore-Membrane Gloves
+        Transmog({item = 275138, type = L['cloth']}), -- Spore-Handler's Handwraps
+    }
+}) -- Broxion <Contagious Anomaly>
+
+map.nodes[53805160] = RareElite({
+    id = 264571,
+    quest = 96317,
+    rewards = {
+        Achievement({id = 62883, criteria = 114011}), -- Showdown Slugger: Naigtal
+        Achievement({id = 63348, criteria = 115255}), -- Heroic Slugger
+        Transmog({item = 275164, type = L['plate']}), -- Indomitable Mechanized Legplates
+        Transmog({item = 275156, type = L['mail']}), -- Gear-Linked Leggings
+        Transmog({item = 275148, type = L['leather']}), -- Reaver's Padded Trousers
+        Transmog({item = 275140, type = L['cloth']}), -- Leggings of Tainted Stuffing
+    }
+}) -- Indomitable Mk XII <Blazing Reaver>
+
+map.nodes[37606180] = Rare({
+    id = 263947,
+    quest = 96205,
+    rewards = {
+        Achievement({id = 62883, criteria = 114005}), -- Showdown Slugger: Naigtal
+        Achievement({id = 63348, criteria = 115248}), -- Heroic Slugger
+        Transmog({item = 275163, type = L['plate']}), -- Uarn's Reinforced Headplate
+        Transmog({item = 275155, type = L['mail']}), -- Interminable Fungal Helmet
+        Transmog({item = 275147, type = L['leather']}), -- Bioluminescent Swamp Mask
+        Transmog({item = 275139, type = L['cloth']}), -- Crown of Fungal Spores
+    }
+}) -- Interminable Uarn <The Undying Stalk>
+
+map.nodes[67706290] = Rare({
+    id = 263955,
+    quest = 96208,
+    rewards = {
+        Achievement({id = 62883, criteria = 114008}), -- Showdown Slugger: Naigtal
+        Achievement({id = 63348, criteria = 115252}), -- Heroic Slugger
+    }
+}) -- Lomelith
+
+map.nodes[77703800] = Rare({
+    id = 263954,
+    quest = 96207,
+    rewards = {
+        Achievement({id = 62883, criteria = 114007}), -- Showdown Slugger: Naigtal
+        Achievement({id = 63348, criteria = 115251}), -- Heroic Slugger
+        Transmog({item = 275161, type = L['plate']}), -- Swamp-Resistant Stompers
+        Transmog({item = 275153, type = L['mail']}), -- Flickering Scale Sabatons
+        Transmog({item = 275145, type = L['leather']}), -- Swalewing Skin Footpads
+        Transmog({item = 275137, type = L['cloth']}), -- Flickering Soft-Steppers
+    }
+}) -- Swalewing Matriarch
+
+map.nodes[56106140] = Rare({
+    id = 264576,
+    quest = 96320,
+    rewards = {
+        Achievement({id = 62883, criteria = 114012}), -- Showdown Slugger: Naigtal
+        Achievement({id = 63348, criteria = 115256}), -- Heroic Slugger
+        Transmog({item = 275160, type = L['plate']}), -- Slaipaan's Carapace
+        Transmog({item = 275152, type = L['mail']}), -- Burrower's Linked Hauberk
+        Transmog({item = 275144, type = L['leather']}), -- Leafy-Hide Coat
+        Transmog({item = 275136, type = L['cloth']}), -- Leaf-Woven Blouse
+    }
+}) -- Slaipaan
+
+map.nodes[56106140] = Rare({
+    id = 267422,
+    quest = 97014,
+    rewards = {
+        Achievement({id = 63348, criteria = 115343}), -- Heroic Slugger
+        Transmog({item = 275165, type = L['plate']}), -- Petrified Mushroom Shoulderplates
+        Transmog({item = 275157, type = L['mail']}), -- Swampwalker's Spaulders
+        Transmog({item = 275149, type = L['leather']}), -- Fungal Draped Epaulets
+        Transmog({item = 275141, type = L['cloth']}), -- Spore-Speckled Shoulderpads
+    }
+}) -- Warbringer Thal'kuur
+
+map.nodes[70307640] = Rare({
+    id = 264574,
+    quest = 96319,
+    rewards = {
+        Achievement({id = 62883, criteria = 114010}), -- Showdown Slugger: Naigtal
+        Achievement({id = 63348, criteria = 115254}), -- Heroic Slugger
+    }
+}) -- Warp Agent Xi'grivr
+
+map.nodes[48804740] = Rare({
+    id = 265698,
+    quest = 96566,
+    rewards = {
+        Transmog({item = 275166, type = L['plate']}), -- Voidwarped Greatbelt
+        Transmog({item = 275158, type = L['mail']}), -- Voidwarped Scale Girdle
+        Transmog({item = 275150, type = L['leather']}), -- Sporebat Leather Belt
+        Transmog({item = 275142, type = L['cloth']}), -- Voidwarped Sash
+    }
+}) -- Voidwarped Sporebat
+
+-------------------------------------------------------------------------------
+------------------------------- TREASURES ------------------------------------
+-------------------------------------------------------------------------------
+
+--[=[
+map.nodes[00000000] = Treasure({
+    -- Spawns randomly in Vilaldoun cave
+    rewards = {
+        Transmog({item = 276292, type = L['offhand']}), -- Ancient Anchorite's Beacon
+        Transmog({item = 276303, type = L['1h_sword']}), -- Blessed Blade of Taalvilor
+        Transmog({item = 276285, type = L['1h_sword']}), -- Elonath's Defender's Sword
+        Transmog({item = 276302, type = L['1h_mace']}), -- Taalvilor Crystalsmith's Mace
+    }
+}) -- Ancient Crypt Reliquary
+]=]
+
+--[=[
+map.nodes[00000000] = Treasure({
+    -- Spawns randomly throughout the zone
+}) -- Hal'hadar Pocket-Storage
+]=]
+
+map.nodes[26304710] = Treasure({
+    rewards = {
+        Transmog({item = 276296, type = L['leather']}), -- Soltian Wrangler's Grip
+    }
+}) -- Squirming Mollusk
+
+map.nodes[76205070] = Treasure({
+    rewards = {
+        Transmog({item = 276296, type = L['leather']}), -- Soltian Wrangler's Grip
+    }
+}) -- Squirming Mollusk
+
+map.nodes[78406880] = Treasure({
+    rewards = {
+        Transmog({item = 276296, type = L['leather']}), -- Soltian Wrangler's Grip
+    }
+}) -- Squirming Mollusk
---
+++ plugins/12_Midnight/zones/val.lua
@@ -0,0 +1,120 @@
+-------------------------------------------------------------------------------
+---------------------------------- NAMESPACE ----------------------------------
+-------------------------------------------------------------------------------
+local ADDON_NAME, ns = ...
+local Map = ns.Map
+
+local Rare = ns.node.Rare
+local RareElite = ns.node.RareElite
+local Treasure = ns.node.Treasure
+local Achievement = ns.reward.Achievement
+
+-------------------------------------------------------------------------------
+
+local map = Map({id = 3047, settings = true}) -- Val
+
+-------------------------------------------------------------------------------
+------------------------------------ RARES ------------------------------------
+-------------------------------------------------------------------------------
+
+map.nodes[44605280] = Rare({
+    id = 264866,
+    rewards = {
+        Achievement({id = 62881, criteria = 114000}), -- Showdown Slugger: Val
+        Achievement({id = 63348, criteria = 115262}), -- Heroic Slugger
+    }
+}) -- Krilkan
+
+map.nodes[49707920] = Rare({
+    id = 264865,
+    rewards = {
+        Achievement({id = 62881, criteria = 113998}), -- Showdown Slugger: Val
+        Achievement({id = 63348, criteria = 115260}), -- Heroic Slugger
+    }
+}) -- Mercilus
+
+map.nodes[55206560] = Rare({
+    id = 261965,
+    rewards = {
+        Achievement({id = 62881, criteria = 113995}), -- Showdown Slugger: Val
+        Achievement({id = 63348, criteria = 115257}), -- Heroic Slugger
+    }
+}) -- Sleet-Rune
+
+map.nodes[29007380] = Rare({
+    id = 264864,
+    rewards = {
+        Achievement({id = 62881, criteria = 113999}), -- Showdown Slugger: Val
+        Achievement({id = 63348, criteria = 115261}), -- Heroic Slugger
+    }
+}) -- Xirah
+
+--[=[
+map.nodes[00000000] = RareElite({
+    id = 261716,
+    rewards = {
+        Achievement({id = 62881, criteria = 113997}), -- Showdown Slugger: Val
+        Achievement({id = 63348, criteria = 115259}), -- Heroic Slugger
+    }
+}) -- Glacial Broodmother
+]=]
+
+--[=[
+map.nodes[00000000] = Rare({
+    id = 264868,
+    rewards = {
+        Achievement({id = 62881, criteria = 114001}), -- Showdown Slugger: Val
+        Achievement({id = 63348, criteria = 115263}), -- Heroic Slugger
+    }
+}) -- Opprimius
+]=]
+
+--[=[
+map.nodes[00000000] = Rare({
+    id = 264870,
+    rewards = {
+        Achievement({id = 62881, criteria = 114003}), -- Showdown Slugger: Val
+        Achievement({id = 63348, criteria = 115265}), -- Heroic Slugger
+    }
+}) -- The Horror Below
+]=]
+
+--[=[
+map.nodes[00000000] = Rare({
+    id = 262421,
+    rewards = {
+        Achievement({id = 62881, criteria = 113996}), -- Showdown Slugger: Val
+        Achievement({id = 63348, criteria = 115258}), -- Heroic Slugger
+    }
+}) -- Atomus
+]=]
+
+--[=[
+map.nodes[00000000] = Rare({
+    id = 264869,
+    rewards = {
+        Achievement({id = 62881, criteria = 114002}), -- Showdown Slugger: Val
+        Achievement({id = 63348, criteria = 115264}), -- Heroic Slugger
+    }
+}) -- Nelgothar
+]=]
+
+--[=[
+map.nodes[00000000] = RareElite({
+    id = 265269,
+    rewards = {
+        Achievement({id = 62881, criteria = 114004}), -- Showdown Slugger: Val
+        Achievement({id = 63348, criteria = 115266}), -- Heroic Slugger
+    }
+}) -- Shadowguard Destroyer
+]=]
+
+-------------------------------------------------------------------------------
+------------------------------- TREASURES ------------------------------------
+-------------------------------------------------------------------------------
+
+--[=[
+map.nodes[00000000] = Treasure({
+    -- Spawns randomly throughout the zone
+}) -- Dominaar Storage Vessel (random spawn)
+]=]
