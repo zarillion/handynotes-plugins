5d457a87ed9c0b68d756f0669616a62a41e716c9
Adavak <adavak@adavak.com>
2026-09-06 02:31:19 +0800

    Disable Arcantina daily objects only, keep Pepe and Share a Drink

--- plugins/12_Midnight/zones/arcantina.lua
+++ plugins/12_Midnight/zones/arcantina.lua
@@ -4,7 +4,7 @@
 local ADDON_NAME, ns = ...
 local Map = ns.Map
 local L = ns.locale
-local Class = ns.Class
+-- local Class = ns.Class
 
 local Item = ns.node.Item
 
@@ -13,29 +13,29 @@ local Item = ns.node.Item
 local map = Map({id = 2541, settings = true})
 
 -------------------------------------------------------------------------------
-local CantinaItem = Class('CantinaItem', Item, {ignore_bags = true})
+-- local CantinaItem = Class('CantinaItem', Item, {ignore_bags = true})
 
 map.nodes[55264393] = Item({icon = 134301, id = 213202, quest = 79550}) -- Explorer Pepe
 
-map.nodes[44966051] = CantinaItem({icon = 134721, id = 6662, quest = 92349}) -- Giant Growth Potion
-map.nodes[28433959] = CantinaItem({icon = 134721, id = 6662, quest = 92350}) -- Giant Growth Potion
-map.nodes[18534417] = CantinaItem({icon = 134721, id = 6662, quest = 92351}) -- Giant Growth Potion
+-- map.nodes[44966051] = CantinaItem({icon = 134721, id = 6662, quest = 92349}) -- Giant Growth Potion
+-- map.nodes[28433959] = CantinaItem({icon = 134721, id = 6662, quest = 92350}) -- Giant Growth Potion
+-- map.nodes[18534417] = CantinaItem({icon = 134721, id = 6662, quest = 92351}) -- Giant Growth Potion
 
-map.nodes[61315823] = CantinaItem({icon = 134757, id = 124640, quest = 92352}) -- Inky Black Potion
-map.nodes[39688083] = CantinaItem({icon = 134757, id = 124640, quest = 92353}) -- Inky Black Potion
-map.nodes[36575489] = CantinaItem({icon = 134757, id = 124640, quest = 92354}) -- Inky Black Potion
+-- map.nodes[61315823] = CantinaItem({icon = 134757, id = 124640, quest = 92352}) -- Inky Black Potion
+-- map.nodes[39688083] = CantinaItem({icon = 134757, id = 124640, quest = 92353}) -- Inky Black Potion
+-- map.nodes[36575489] = CantinaItem({icon = 134757, id = 124640, quest = 92354}) -- Inky Black Potion
 
-map.nodes[61875110] = CantinaItem({icon = 134301, id = 40195, quest = 92355}) -- Pygmy Oil
-map.nodes[33621257] = CantinaItem({icon = 134301, id = 40195, quest = 92356}) -- Pygmy Oil
-map.nodes[74746281] = CantinaItem({icon = 134301, id = 40195, quest = 92357}) -- Pygmy Oil
+-- map.nodes[61875110] = CantinaItem({icon = 134301, id = 40195, quest = 92355}) -- Pygmy Oil
+-- map.nodes[33621257] = CantinaItem({icon = 134301, id = 40195, quest = 92356}) -- Pygmy Oil
+-- map.nodes[74746281] = CantinaItem({icon = 134301, id = 40195, quest = 92357}) -- Pygmy Oil
 
-map.nodes[54494478] = CantinaItem({icon = 1033182, id = 112384, quest = 92358}) -- Reflecting Prism
-map.nodes[42224258] = CantinaItem({icon = 1033182, id = 112384, quest = 92359}) -- Reflecting Prism
-map.nodes[79124579] = CantinaItem({icon = 1033182, id = 112384, quest = 92360}) -- Reflecting Prism
+-- map.nodes[54494478] = CantinaItem({icon = 1033182, id = 112384, quest = 92358}) -- Reflecting Prism
+-- map.nodes[42224258] = CantinaItem({icon = 1033182, id = 112384, quest = 92359}) -- Reflecting Prism
+-- map.nodes[79124579] = CantinaItem({icon = 1033182, id = 112384, quest = 92360}) -- Reflecting Prism
 
-map.nodes[55807130] = CantinaItem({icon = 134302, id = 6657, quest = 92361}) -- Savory Deviate Delight
-map.nodes[35343007] = CantinaItem({icon = 134302, id = 6657, quest = 92362}) -- Savory Deviate Delight
-map.nodes[68055216] = CantinaItem({icon = 134302, id = 6657, quest = 92363}) -- Savory Deviate Delight
+-- map.nodes[55807130] = CantinaItem({icon = 134302, id = 6657, quest = 92361}) -- Savory Deviate Delight
+-- map.nodes[35343007] = CantinaItem({icon = 134302, id = 6657, quest = 92362}) -- Savory Deviate Delight
+-- map.nodes[68055216] = CantinaItem({icon = 134302, id = 6657, quest = 92363}) -- Savory Deviate Delight
 
 -------------------------------------------------------------------------------
 
