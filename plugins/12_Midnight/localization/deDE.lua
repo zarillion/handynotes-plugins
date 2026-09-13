a927ea873b097b0ea8d88c152064ab04d0ec362c
Ioney <dominik.hier@gmail.com>
2026-09-14 00:25:59 +0200

    Update deDE.lua
    Added missing german translations for Midnight

--- plugins/12_Midnight/localization/deDE.lua
+++ plugins/12_Midnight/localization/deDE.lua
@@ -1,7 +1,7 @@
 local ADDON_NAME, ns = ...
 local L = ns.NewLocale('deDE')
 if not L then return end
--- German translation by Dathwada EU-Eredar
+-- German translation by Dathwada EU-Eredar and Ioney EU-Blackrock
 
 -------------------------------------------------------------------------------
 ------------------------------------ COMMON -----------------------------------
@@ -67,28 +67,28 @@ L['impenatrably_sealed_gourd_note'] = 'Sammle die {item:260251} vom nahegelegene
 L['options_icons_more_than_just_thier_roots'] = '{achievement:62188}'
 L['options_icons_more_than_just_thier_roots_desc'] = 'Zeigt die Positionen der NPCs für den Erfolg {achievement:62188} an.'
 L['more_than_just_thier_roots_note'] = 'Spreche mit dem jeweiligen NPC an den unten aufgeführten Orten.'
-L['chonon_note'] = nil
-L['funnid_note'] = nil
-L['kawayn_note'] = nil
+L['chonon_note'] = 'Oben in den Ästen des Baumes.'
+L['funnid_note'] = 'Hoch oben in den Ästen des Weltenbaums.'
+L['kawayn_note'] = 'Oben auf dem großen Baumstamm.'
 
-L['altar_of_innocence'] = nil
-L['altar_of_vigor'] = nil
-L['altar_of_wisdom'] = nil
-L['altar_of_innocence_note'] = nil
-L['altar_of_vigor_note'] = nil
-L['altar_of_wisdom_note'] = nil
+L['altar_of_innocence'] = 'Altar der Unschuld'
+L['altar_of_vigor'] = 'Altar des Elans'
+L['altar_of_wisdom'] = 'Altar der Weisheit'
+L['altar_of_innocence_note'] = 'Gib {item:256882} an den {npc:254030} zurück, nachdem du mit dem {object:Altar der Unschuld} interagiert hast.\n\nErledige die Aufgaben für die verbleibenden Altäre, um den Schatz {object:Geschenk des Kreislaufs} in {location:Der Höhle} zu erschaffen.'
+L['altar_of_vigor_note'] = 'Gib {item:257024} an den {npc:254104} zurück, nachdem du mit dem {object:Altar des Elans} interagiert hast.\n\nErledige die Aufgaben für die verbleibenden Altäre, um den Schatz {object:Geschenk des Kreislaufs} in {location:Der Höhle} zu erschaffen.'
+L['altar_of_wisdom_note'] = 'Gib {item:257054} an den {npc:254116} zurück, nachdem du mit dem {object:Altar der Weisheit} interagiert hast.\n\nErledige die Aufgaben für die verbleibenden Altäre, um den Schatz {object:Geschenk des Kreislaufs} in {location:Der Höhle} zu erschaffen.'
 
 -------------------------------------------------------------------------------
 ---------------------------------- VOIDSTORM ----------------------------------
 -------------------------------------------------------------------------------
 
-L['void_shielded_tomb_note'] = nil
+L['void_shielded_tomb_note'] = 'Trink den {object:Trank der Dissoziation} auf dem Tisch in der Nähe, laufe dann zum gegenüberliegenden Gebäude, nimm {item:251519} an dich und benutze ihn, um die Truhe zu öffnen.\n\n' .. ns.color.Red('Benötigt Level 90')
 L['bloody_sack_note'] = 'Interagiere mit der {object:Vergessenen Oubliette} und sammle dann {object:Triefendes Fleisch} von nahegelegenen Knochenhaufen, um die {object:Vergessene Oubliette} zu füttern.'
 L['malignant_chest_note'] = 'Aktiviere die {object:Heimtückischer Knoten} in der Höhle, um die Truhe zu öffnen.'
 L['malignant_node'] = 'Heimtückischer Knoten'
-L['exaliburn_note'] = nil
+L['exaliburn_note'] = 'Trink den in der Nähe liegenden {object:Trank der unbestrittenen Stärke} und zieh dann Exaliburn heraus.'
 L['voidhoarders_corpse'] = 'Leiche des Leerenhorters'
-L['blackcore_note'] = nil
+L['blackcore_note'] = 'Töte {npc:248462} oder {npc:248483} innerhalb des markierten Bereichs, um 3x {item:248680} zu erhalten. Interagiere anschließend mit der {object:Singularitätslinse}, um den seltene Gegner zu beschwören.'
 
 -------------------------------------------------------------------------------
 ----------------------------------- ZUL'AMAN ----------------------------------
@@ -105,84 +105,85 @@ L['options_icons_spiritpaw_marathon_desc'] = 'Zeige die Positionen der Läufer f
 L['spiritpaw_marathon_note'] = 'Sprich mit {npc:258938}.'
 
 L['options_icons_gnome_alone'] = '{achievement:62200}'
-L['options_icons_gnome_alone_desc'] = nil
+L['options_icons_gnome_alone_desc'] = 'Zeigt die Positionen der Schriftstücke für den Erfolg {achievement:62200} an.'
 
 L['abandoned_ritual_skull'] = 'Zurückgelassener Ritualschädel'
 
 L['options_icons_put_a_pin_in_it'] = '{achievement:62199}'
-L['options_icons_put_a_pin_in_it_desc'] = nil
-L['kalika_note'] = nil
-L['songseeker_ikaja_note'] = nil
+L['options_icons_put_a_pin_in_it_desc'] = 'Zeigt die Positionen für den Erfolg {achievement:62199} an.'
+L['kalika_note'] = 'Sprich mit {npc:258884} und interagiere anschließend mit dem {object:Vergessenen Knopf} hinter der nahegelegenen {object:Jade-Statue}.'
+L['songseeker_ikaja_note'] = 'Oben auf dem Tempel'
 
 -------------------------------------------------------------------------------
 ----------------------------------- NAIGTAL -----------------------------------
 -------------------------------------------------------------------------------
 
-L['sleepy_mandrake_note'] = nil
-L['partially_digested_redcap_note'] = nil
-L['ancient_crypt_reliquary'] = nil
-L['squirming_mollusk'] = nil
-L['spiked_shell'] = nil
+L['sleepy_mandrake_note'] = 'Finde 5 einzigartige Rotkappen-Pilze und gib sie der {npc:267910} in der Grotte des Schläfers. Jeder Pilz hat einen anderen Fundort in der Zone.'
+L['partially_digested_redcap_note'] = 'Wird von {npc:264340}, {npc:264315} und ähnlichen seltenen Gegnern in dieser Zone fallen gelassen.'
+L['ancient_crypt_reliquary'] = 'Uralte Kryptareliquie'
+L['squirming_mollusk'] = 'Zuckende Molluske'
+L['spiked_shell'] = 'Stachelpanzer'
 
 -------------------------------------------------------------------------------
 ------------------------------------- VAL -------------------------------------
 -------------------------------------------------------------------------------
 
-L['enchanted_hilt'] = nil
-L['enchanted_hilt_note'] = nil
+L['enchanted_hilt'] = 'Verzauberter Griff'
+L['enchanted_hilt_note'] = 'Nur im heroischen Modus.\n\nKlicke auf den {Objekt:Verzaubertnr Griff}, um den {spell:1300397} zu erhalten, und besiege anschließend den Weltboss {npc:261072} oder seltene Gegner aus {achievement:62881} in dieser Zone zweimal. Kehre zum Griff zurück, um den Schatz zu erhalten.'
 
 -------------------------------------------------------------------------------
 ---------------------------------- ARCANTINA ----------------------------------
 -------------------------------------------------------------------------------
 
 L['share_a_drink_note'] = 'Kaufe {item:251039} von {npc:250495} in der Arcantina und teile ihn mit allen Rassen.'
-L['highly_decorated_note'] = nil
+L['highly_decorated_note'] = 'Schließe die Quests zu den wechselnden Relikten ab und sammle anschließend deren Dekorationen ein, um sie in der Arcantina zu platzieren.'
 
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 --------------------------------- RITUAL SITES --------------------------------
 -------------------------------------------------------------------------------
-L['options_icons_ritual_site'] = nil
-L['options_icons_ritual_site_desc'] = nil
 
-L['ritual_site_broken_throne'] = nil
-L['ritual_site_daggerspine_point'] = nil
+L['options_icons_ritual_site'] = 'Ritualstätten'
+L['options_icons_ritual_site_desc'] = 'Zeige {location:Ritualstätten} Belohnungend in Tooltips.'
 
-L['broken_throne_entry_note'] = nil
-L['daggerspine_point_entry_note'] = nil
+L['ritual_site_broken_throne'] = 'Ritualstätte: Zerschlagener Thron'
+L['ritual_site_daggerspine_point'] = 'Ritualstätte: Säbelflossenspitze'
 
-L['chewed_meat'] = nil
-L['chewed_meat_note'] = nil
+L['broken_throne_entry_note'] = 'Der Zerschlagene Thron, auch bekannt als Atal\'Kaldan, ist eine Amani ruine die von der Zwielichtklinge übernommen wurde. Klicke auf {npc:260103} die Ritualstätte zu betreten.\n\nWechselt wöchentlich mit {location:Ritualstätte: Säbelflossenspitze}.'
+L['daggerspine_point_entry_note'] = 'Die Säbelflossenspitze ist eine Insel, die von den Säbelflossen Naga besetzt ist und früher Insel Shalandis hieß. Klicke auf {npc:260103} die Ritualstätte zu betreten.\n\nWechselt wöchentlich mit {location:Ritualstätte: Zerschlagener Thron}.'
 
-L['ritual_circle'] = nil
-L['ritual_circle_note'] = nil
+L['chewed_meat'] = 'Angekautes Fleisch'
+L['chewed_meat_note'] = 'Beschwöre {spell:1286634} beim {object:Angekauten Fleisch}. Besiege {npc:263381}. Sobald er wieder friedlich ist, füttere ihm 5x {item:242639}.'
 
-L['updraft'] = nil
-L['void_tainted_nest'] = nil
-L['void_tainted_nest_note'] = nil
+L['ritual_circle'] = 'Ritualkreis'
+L['ritual_circle_note'] = 'Platziere die {item:271999} und beginne das Ritual um {npc:263527} zu beschwören.'
 
-L['chubs_note'] = nil
+L['updraft'] = 'Aufwind'
+L['void_tainted_nest'] = 'Leerenbesudeltes Nest'
+L['void_tainted_nest_note'] = 'Du musst auf {spell:1286606} reiten um den Aufwind zu sehen. Gehe in den Aufwind um zum Nest zu fliegen.'
 
-L['rustling_fern'] = nil
-L['rustling_bush'] = nil
-L['rustling_fern_note'] = nil
-L['rustling_bush_note'] = nil
+L['chubs_note'] = 'Kaufe 1x {item:242639} von {npc:263355}.'
+
+L['rustling_fern'] = 'Raschelnder Farn'
+L['rustling_bush'] = 'Raschelndes Gebüsch'
+L['rustling_fern_note'] = 'Zu finden auf Stufe 3 oder höher. Klicke auf den {object:Raschelnden Farn} bis das Kätzchen erscheint (8-13 Klicks). Nur 1-2 pro Lauf verfügbar.\nDas Haustier darf noch nicht freigeschalten sein.'
+L['rustling_bush_note'] = 'Zu finden auf Stufe 3 oder höher. Klicke auf das {object:Raschelnde Gebüsch} bis das Kätzchen erscheint (8-13 Klicks). Nur 1-2 pro Lauf verfügbar.\nDas Haustier darf noch nicht freigeschalten sein.'
 
 L['soggy_nest'] = nil
-L['soggy_nest_note'] = nil
+L['soggy_nest_note'] = 'Benötigt {item:272128} um {npc:263917} anzulocken.\n{item:272128} wird auch von Naga in der Zone fallen gelassen.'
 
-L['washed_up_kelp'] = nil
-L['washed_up_kelp_note'] = nil
+L['washed_up_kelp'] = 'Angespülter Seetang'
+L['washed_up_kelp_note'] = 'Durchsuche {object:Angespülten Seetang}, um mit etwas Glück {npc:263617} anzulocken. Zu finden an mehreren Orten in der Zone.'
 
-L['floating_egg_note'] = nil
+L['floating_egg_note'] = 'Das {npc:263805} treibt den Fluss hinunter. Schnapp es dir auf seinem Weg.'
+
+-------------------------------------------------------------------------------
 ------------------------------------ DELVES -----------------------------------
 -------------------------------------------------------------------------------
 
 L['sturdy_chest'] = 'Robuste Truhe'
 L['sturdy_chest_suffix'] = 'robuste Truhen geöffnet'
 
-L['gulf_of_memory_chest_note'] = nil
+L['gulf_of_memory_chest_note'] = 'Steigen Sie die Wurzeln des Baumes hinauf, um den Schatz zu erreichen.'
 
 -------------------------------------------------------------------------------
 --------------------------------- COILED ISLES --------------------------------
@@ -193,12 +194,12 @@ L['sunken_divers_chest_note'] = 'Tötet den nahegelegenen {npc:263081} und plün
 L['vulzahn_smuggled_treasure_note'] = '1. Holt {item:271791} von {dot:Blue}{npc:253837}\n\n2. Gebt {item:271791} an {dot:Red}{npc:262204}, um {item:271788} zu erhalten\n\n3. Gebt {item:271788} an {dot:Green}{npc:263265}, um {item:271792} zu erhalten und den Schatz freizuschalten'
 L['grave_of_someone_forgotten_note'] = '1. Sprecht mit {dot:Red}{npc:263242}.\n\n2. Sprecht mit {dot:Green}{npc:263243}.\n\n3. Sprecht mit {dot:Blue}{npc:263241}.\n\n4. Kehrt zum Grab zurueck und plundert es.'
 L['profane_ritual_spoils_note'] = 'Blickt zur Statue und klickt die {npc:263187} in dieser Reihenfolge an: oben rechts, oben links, unten rechts, unten links, um den Schatz zu öffnen.'
-L['abandoned_amani_privateers_cache_note'] = nil
-L['grisly_cod_pool'] = nil
-L['waterlogged_crate'] = nil
-L['broken_urn'] = nil
-L['brine_crusted_chest_note'] = nil
-L['bubbling_clam'] = nil
+L['abandoned_amani_privateers_cache_note'] = '1. Fische im {dot:Blue}{object:Schauerlichen Dorschteich}, um einen {item:265525} zu erhalten und füttere ihn anschließend an {npc:258076} unter Wasser.\n\n2. Folge dem Delfin, um die {item:265610} aus der {dot:Red}{object:Durchnässten Kiste} und die {item:265603} aus der {dot:Green}{object:Zerbrochenen Urne} zu sammeln.\n\n3. Klicke mit der rechten Maustaste auf eine Hälfte, um sie zum {item:265602} zu kombinieren und den Schatz freizuschalten.\n\n{Hinweis: Bleibe die ganze Zeit unter Wasser. Wenn du auftauchst, schwimmt der Delfin davon.}'
+L['grisly_cod_pool'] = 'Schauerlicher Dorschteich'
+L['waterlogged_crate'] = 'Durchnässte Kiste'
+L['broken_urn'] = 'Zerbrochene Urne'
+L['brine_crusted_chest_note'] = '1. Öffne eine {dot:Blue}{object:Blubbernde Muschel}, um eine {item:271815} zu erhalten.\n\n2. Gehe zu {dot:Red}{npc:263347} und lege die Perle auf den Pfeil auf dem Boden. Sie wird sie nehmen und den {item:271881} fallen lassen.\n\n3. Hebe den Schlüssel auf und öffne den Schatz.\n\n{note:Wenn sich eine Muschel nicht öffnen lässt, hast du wahrscheinlich bereits eine Perle in deiner Tasche.}'
+L['bubbling_clam'] = 'Blubbernde Muschel'
 
 L['options_icons_coiled_isle_safari'] = '{achievement:62492}'
 L['options_icons_coiled_isle_safari_desc'] = 'Zeigt die Positionen der Kampfhaustiere für den Erfolg {achievement:62492} an.'
@@ -213,6 +214,6 @@ L['options_icons_the_honored_dead'] = '{achievement:63610}'
 L['options_icons_the_honored_dead_desc'] = 'Zeigt die Positionen für den {achievement:63610} Erfolg an.'
 
 L['options_icons_mysterious_mixing'] = '{achievement:63432}'
-L['options_icons_mysterious_mixing_desc'] = nil
-L['mysterious_mixing_note'] = nil
-L['curse_surge_note'] = 'Cursed-Surge-Ereignisse wechseln alle 45 Minuten.\n\n{note:Wenn du es verpasst hast, gruppiere dich realmübergreifend — auf anderen Realms läuft das Ereignis möglicherweise noch.}'
+L['options_icons_mysterious_mixing_desc'] = 'Zeigt die Positionen für den {achievement:63432} Erfolg an.'
+L['mysterious_mixing_note'] = 'Schließe die tägliche Quest {quest:97016} für {npc:263327} ab, um Opfergaben herzustellen:\n\n{Hinweis: Die Zutaten erhältst du aus der {object: Beschädigten Kanope}, dem {object: Giftgeronnenen Schmuckstück} und der {object: Singenden Muschel}, die auf der Insel zu finden sind, oder du kannst sie im Auktionshaus kaufen.}'
+L['curse_surge_note'] = 'Fluchwellen-Ereignisse wechseln alle 45 Minuten.\n\n{note:Wenn du es verpasst hast, gruppiere dich realmübergreifend — auf anderen Realms läuft das Ereignis möglicherweise noch.}'
